9850739a130fd7af1320c9399e0ec5e72858b433
Olivier FAURE <couteaubleu@gmail.com>
2021-10-02 16:06:41 +0200

    WIP: Add test files from Druid

---
+++ widget-cruncher/src/testing/helper_widgets.rs
@@ -0,0 +1,328 @@
+// Copyright 2020 The Druid Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Helper types for test writing.
+//!
+//! This includes tools for making throwaway widgets more easily.
+//!
+//! Note: Some of these types are undocumented. They're meant to help maintainers of Druid and
+//! people trying to build a framework on top of Druid (like crochet), not to be user-facing.
+
+#![allow(missing_docs)]
+
+use smallvec::SmallVec;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+use crate::*;
+
+pub type EventFn<S> = dyn FnMut(&mut S, &mut EventCtx, &Event, &Env);
+pub type LifeCycleFn<S> = dyn FnMut(&mut S, &mut LifeCycleCtx, &LifeCycle, &Env);
+pub type LayoutFn<S> = dyn FnMut(&mut S, &mut LayoutCtx, &BoxConstraints, &Env) -> Size;
+pub type PaintFn<S> = dyn FnMut(&mut S, &mut PaintCtx, &Env);
+pub type ChildrenFn<S> = dyn Fn(&S) -> SmallVec<[&dyn AsWidgetPod; 16]>;
+pub type ChildrenMutFn<S> = dyn FnMut(&mut S) -> SmallVec<[&mut dyn AsWidgetPod; 16]>;
+
+pub const REPLACE_CHILD: Selector = Selector::new("druid-test.replace-child");
+
+/// A widget that can be constructed from individual functions, builder-style.
+///
+/// This widget is generic over its state, which is passed in at construction time.
+pub struct ModularWidget<S> {
+    state: S,
+    on_event: Option<Box<EventFn<S>>>,
+    lifecycle: Option<Box<LifeCycleFn<S>>>,
+    layout: Option<Box<LayoutFn<S>>>,
+    paint: Option<Box<PaintFn<S>>>,
+    children: Option<Box<ChildrenFn<S>>>,
+    children_mut: Option<Box<ChildrenMutFn<S>>>,
+}
+
+/// A widget that can replace its child on command
+pub struct ReplaceChild {
+    child: WidgetPod<Box<dyn Widget>>,
+    replacer: Box<dyn Fn() -> WidgetPod<Box<dyn Widget>>>,
+}
+
+/// A widget that records each time one of its methods is called.
+///
+/// Make one like this:
+///
+/// ```
+/// # use druid::widget::Label;
+/// # use druid::{WidgetExt, LifeCycle};
+/// use druid::tests::helpers::{Recording, Record, TestWidgetExt};
+/// use druid::tests::harness::Harness;
+/// let recording = Recording::default();
+/// let widget = Label::new("Hello").padding(4.0).record(&recording);
+///
+/// Harness::create_simple((), widget, |harness| {
+///     harness.send_initial_events();
+///     assert!(matches!(recording.next(), Record::L(LifeCycle::WidgetAdded)));
+/// })
+/// ```
+pub struct Recorder<W> {
+    recording: Recording,
+    child: W,
+}
+
+/// A recording of widget method calls.
+#[derive(Debug, Clone, Default)]
+pub struct Recording(Rc<RefCell<VecDeque<Record>>>);
+
+/// A recording of a method call on a widget.
+///
+/// Each member of the enum corresponds to one of the methods on `Widget`.
+#[derive(Debug, Clone)]
+pub enum Record {
+    /// An `Event`.
+    E(Event),
+    /// A `LifeCycle` event.
+    L(LifeCycle),
+    Layout(Size),
+    Update(Region),
+    Paint,
+    // instead of always returning an Option<Record>, we have a none variant;
+    // this would be code smell elsewhere but here I think it makes the tests
+    // easier to read.
+    None,
+}
+
+/// like WidgetExt but just for this one thing
+pub trait TestWidgetExt: Widget + Sized + 'static {
+    fn record(self, recording: &Recording) -> Recorder<Self> {
+        Recorder {
+            child: self,
+            recording: recording.clone(),
+        }
+    }
+}
+
+impl<W: Widget + 'static> TestWidgetExt for W {}
+
+impl<S> ModularWidget<S> {
+    pub fn new(state: S) -> Self {
+        ModularWidget {
+            state,
+            on_event: None,
+            lifecycle: None,
+            layout: None,
+            paint: None,
+            children: None,
+            children_mut: None,
+        }
+    }
+
+    pub fn event_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut EventCtx, &Event, &Env) + 'static,
+    ) -> Self {
+        self.on_event = Some(Box::new(f));
+        self
+    }
+
+    pub fn lifecycle_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut LifeCycleCtx, &LifeCycle, &Env) + 'static,
+    ) -> Self {
+        self.lifecycle = Some(Box::new(f));
+        self
+    }
+
+    pub fn layout_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut LayoutCtx, &BoxConstraints, &Env) -> Size + 'static,
+    ) -> Self {
+        self.layout = Some(Box::new(f));
+        self
+    }
+
+    pub fn paint_fn(mut self, f: impl FnMut(&mut S, &mut PaintCtx, &Env) + 'static) -> Self {
+        self.paint = Some(Box::new(f));
+        self
+    }
+
+    pub fn children_fns(
+        mut self,
+        children: impl Fn(&S) -> SmallVec<[&dyn AsWidgetPod; 16]> + 'static,
+        children_mut: impl FnMut(&mut S) -> SmallVec<[&mut dyn AsWidgetPod; 16]> + 'static,
+    ) -> Self {
+        self.children = Some(Box::new(children));
+        self.children_mut = Some(Box::new(children_mut));
+        self
+    }
+}
+
+impl<S> Widget for ModularWidget<S> {
+    fn on_event(&mut self, ctx: &mut EventCtx, event: &Event, env: &Env) {
+        if let Some(f) = self.on_event.as_mut() {
+            f(&mut self.state, ctx, event, env)
+        }
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, env: &Env) {
+        if let Some(f) = self.lifecycle.as_mut() {
+            f(&mut self.state, ctx, event, env)
+        }
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, env: &Env) -> Size {
+        let ModularWidget {
+            ref mut state,
+            ref mut layout,
+            ..
+        } = self;
+        layout
+            .as_mut()
+            .map(|f| f(state, ctx, bc, env))
+            .unwrap_or_else(|| Size::new(100., 100.))
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, env: &Env) {
+        if let Some(f) = self.paint.as_mut() {
+            f(&mut self.state, ctx, env)
+        }
+    }
+
+    fn children(&self) -> SmallVec<[&dyn AsWidgetPod; 16]> {
+        if let Some(f) = self.children.as_ref() {
+            f(&self.state)
+        } else {
+            SmallVec::new()
+        }
+    }
+
+    fn children_mut(&mut self) -> SmallVec<[&mut dyn AsWidgetPod; 16]> {
+        if let Some(f) = self.children_mut.as_mut() {
+            f(&mut self.state)
+        } else {
+            SmallVec::new()
+        }
+    }
+}
+
+impl ReplaceChild {
+    pub fn new<W: Widget + 'static>(
+        child: impl Widget + 'static,
+        f: impl Fn() -> W + 'static,
+    ) -> Self {
+        let child = WidgetPod::new(child).boxed();
+        let replacer = Box::new(move || WidgetPod::new(f()).boxed());
+        ReplaceChild { child, replacer }
+    }
+}
+
+impl Widget for ReplaceChild {
+    fn on_event(&mut self, ctx: &mut EventCtx, event: &Event, env: &Env) {
+        // TODO
+        /*
+        if let Event::Command(cmd) = event {
+            if cmd.is(REPLACE_CHILD) {
+                self.child = WidgetPod::new((self.replacer)());
+                ctx.children_changed();
+                return;
+            }
+        }
+        */
+        self.child.on_event(ctx, event, env)
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, env: &Env) {
+        self.child.lifecycle(ctx, event, env)
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, env: &Env) -> Size {
+        self.child.layout(ctx, bc, env)
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, env: &Env) {
+        self.child.paint_raw(ctx, env)
+    }
+
+    fn children(&self) -> SmallVec<[&dyn AsWidgetPod; 16]> {
+        self.child.widget().children()
+    }
+
+    fn children_mut(&mut self) -> SmallVec<[&mut dyn AsWidgetPod; 16]> {
+        self.child.widget_mut().children_mut()
+    }
+}
+
+#[allow(dead_code)]
+impl Recording {
+    pub fn is_empty(&self) -> bool {
+        self.0.borrow().is_empty()
+    }
+
+    #[allow(dead_code)]
+    pub fn len(&self) -> usize {
+        self.0.borrow().len()
+    }
+
+    pub fn clear(&self) {
+        self.0.borrow_mut().clear()
+    }
+
+    /// Returns the next event in the recording, if one exists.
+    ///
+    /// This consumes the event.
+    pub fn next(&self) -> Record {
+        self.0.borrow_mut().pop_front().unwrap_or(Record::None)
+    }
+
+    /// Returns an iterator of events drained from the recording.
+    pub fn drain(&self) -> impl Iterator<Item = Record> {
+        self.0
+            .borrow_mut()
+            .drain(..)
+            .collect::<Vec<_>>()
+            .into_iter()
+    }
+
+    fn push(&self, event: Record) {
+        self.0.borrow_mut().push_back(event)
+    }
+}
+
+impl<W: Widget> Widget for Recorder<W> {
+    fn on_event(&mut self, ctx: &mut EventCtx, event: &Event, env: &Env) {
+        self.recording.push(Record::E(event.clone()));
+        self.child.on_event(ctx, event, env)
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, env: &Env) {
+        self.recording.push(Record::L(event.clone()));
+        self.child.lifecycle(ctx, event, env)
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, env: &Env) -> Size {
+        let size = self.child.layout(ctx, bc, env);
+        self.recording.push(Record::Layout(size));
+        size
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, env: &Env) {
+        self.child.paint(ctx, env);
+        self.recording.push(Record::Paint)
+    }
+
+    fn children(&self) -> SmallVec<[&dyn AsWidgetPod; 16]> {
+        self.child.children()
+    }
+
+    fn children_mut(&mut self) -> SmallVec<[&mut dyn AsWidgetPod; 16]> {
+        self.child.children_mut()
+    }
+}
---
+++ widget-cruncher/src/testing/layout_tests.rs
@@ -0,0 +1,280 @@
+// Copyright 2020 The Druid Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Tests related to layout.
+
+#![allow(unused_imports)]
+
+use float_cmp::approx_eq;
+use test_env_log::test;
+
+use super::*;
+
+#[cfg(FALSE)]
+#[test]
+fn simple_layout() {
+    const BOX_WIDTH: f64 = 200.;
+    const PADDING: f64 = 10.;
+
+    let id_1 = WidgetId::next();
+
+    let widget = Split::columns(Label::new("hi"), Label::new("there"))
+        .fix_size(BOX_WIDTH, BOX_WIDTH)
+        .padding(10.0)
+        .with_id(id_1)
+        .center();
+
+    Harness::create_simple(true, widget, |harness| {
+        harness.send_initial_events();
+        harness.just_layout();
+        let state = harness.get_state(id_1);
+        approx_eq!(
+            f64,
+            state.layout_rect().x0,
+            ((DEFAULT_SIZE.width - BOX_WIDTH) / 2.) - PADDING
+        );
+    })
+}
+
+#[cfg(FALSE)]
+#[test]
+fn row_column() {
+    let [id1, id2, id3, id4, id5, id6] = widget_ids();
+    let widget = Flex::row()
+        .must_fill_main_axis(true)
+        .with_flex_child(
+            Flex::column()
+                .with_flex_child(SizedBox::empty().expand().with_id(id1), 1.0)
+                .with_flex_child(SizedBox::empty().expand().with_id(id2), 1.0),
+            1.0,
+        )
+        .with_flex_child(
+            Flex::column()
+                .with_flex_child(SizedBox::empty().expand().with_id(id3), 1.0)
+                .with_flex_child(SizedBox::empty().expand().with_id(id4), 1.0)
+                .with_flex_child(SizedBox::empty().expand().with_id(id5), 1.0)
+                .with_flex_child(SizedBox::empty().expand().with_id(id6), 1.0)
+                .expand_width(),
+            1.0,
+        );
+
+    Harness::create_simple((), widget, |harness| {
+        harness.send_initial_events();
+        harness.just_layout();
+        let state1 = harness.get_state(id1);
+        assert_eq!(state1.layout_rect().origin(), Point::ZERO);
+        let state2 = harness.get_state(id2);
+        assert_eq!(state2.layout_rect().origin(), Point::new(0., 200.));
+        let state3 = harness.get_state(id3);
+        assert_eq!(state3.layout_rect().origin(), Point::ZERO);
+        let state5 = harness.get_state(id5);
+        assert_eq!(state5.layout_rect().origin(), Point::new(0., 200.));
+    })
+}
+
+#[cfg(FALSE)]
+#[test]
+fn simple_paint_rect() {
+    let [id1, id2] = widget_ids();
+
+    let widget = ModularWidget::<(), ()>::new(())
+        .layout_fn(|_, ctx, bc, _, _| {
+            // this widget paints twenty points above below its layout bounds
+            ctx.set_paint_insets(Insets::uniform_xy(0., 20.));
+            bc.max()
+        })
+        .with_id(id1)
+        .fix_size(100., 100.)
+        .padding(10.0)
+        .with_id(id2)
+        .background(Color::BLACK)
+        .center();
+
+    Harness::create_simple((), widget, |harness| {
+        harness.send_initial_events();
+        harness.just_layout();
+
+        let state = harness.get_state(id1);
+
+        // offset by padding
+        assert_eq!(state.layout_rect().origin(), Point::new(10., 10.,));
+        // offset by padding, but then inset by paint insets
+        assert_eq!(state.paint_rect().origin(), Point::new(10., -10.,));
+        // layout size is fixed
+        assert_eq!(state.layout_rect().size(), Size::new(100., 100.,));
+        // paint size is modified by insets
+        assert_eq!(state.paint_rect().size(), Size::new(100., 140.,));
+
+        // now does the container widget correctly propogate the child's paint rect?
+        let state = harness.get_state(id2);
+
+        assert_eq!(state.layout_rect().origin(), Point::ZERO);
+        // offset by padding, but then inset by paint insets
+        assert_eq!(state.paint_rect().origin(), Point::new(0., -10.,));
+        // 100 + 10 on each side
+        assert_eq!(state.layout_rect().size(), Size::new(120., 120.,));
+        // paint size is modified by insets
+        assert_eq!(state.paint_rect().size(), Size::new(120., 140.,));
+    })
+}
+
+#[cfg(FALSE)]
+#[test]
+/// Does a Flex correctly compute the union of multiple children's paint rects?
+fn flex_paint_rect_overflow() {
+    let id = WidgetId::next();
+
+    let widget = Flex::row()
+        .with_flex_child(
+            ModularWidget::new(())
+                .layout_fn(|_, ctx, bc, _, _| {
+                    ctx.set_paint_insets(Insets::new(20., 0., 0., 0.));
+                    bc.constrain(Size::new(10., 10.))
+                })
+                .expand(),
+            1.0,
+        )
+        .with_flex_child(
+            ModularWidget::new(())
+                .layout_fn(|_, ctx, bc, _, _| {
+                    ctx.set_paint_insets(Insets::new(0., 20., 0., 0.));
+                    bc.constrain(Size::new(10., 10.))
+                })
+                .expand(),
+            1.0,
+        )
+        .with_flex_child(
+            ModularWidget::new(())
+                .layout_fn(|_, ctx, bc, _, _| {
+                    ctx.set_paint_insets(Insets::new(0., 0., 0., 20.));
+                    bc.constrain(Size::new(10., 10.))
+                })
+                .expand(),
+            1.0,
+        )
+        .with_flex_child(
+            ModularWidget::new(())
+                .layout_fn(|_, ctx, bc, _, _| {
+                    ctx.set_paint_insets(Insets::new(0., 0., 20., 0.));
+                    bc.constrain(Size::new(10., 10.))
+                })
+                .expand(),
+            1.0,
+        )
+        .with_id(id)
+        .fix_height(200.)
+        .padding(10.)
+        .center();
+
+    Harness::create_simple((), widget, |harness| {
+        harness.set_initial_size(Size::new(300., 300.));
+        harness.send_initial_events();
+        harness.just_layout();
+
+        let state = harness.get_state(id);
+        assert_eq!(state.layout_rect().origin(), Point::new(10., 10.,));
+        assert_eq!(state.paint_rect().origin(), Point::new(-10., -10.,));
+
+        // each of our children insets 20. on a different side; their union
+        // is a uniform 20. inset.
+        let expected_paint_rect = state.layout_rect() + Insets::uniform(20.);
+        assert_eq!(state.paint_rect().size(), expected_paint_rect.size());
+    })
+}
+
+#[cfg(FALSE)]
+#[test]
+fn aspect_ratio_tight_constraints() {
+    let id = WidgetId::next();
+    let (width, height) = (400., 400.);
+    let aspect = AspectRatioBox::<()>::new(Label::new("hello!"), 1.0)
+        .with_id(id)
+        .fix_width(width)
+        .fix_height(height)
+        .center();
+
+    let (window_width, window_height) = (600., 600.);
+
+    Harness::create_simple((), aspect, |harness| {
+        harness.set_initial_size(Size::new(window_width, window_height));
+        harness.send_initial_events();
+        harness.just_layout();
+        let state = harness.get_state(id);
+        assert_eq!(state.layout_rect().size(), Size::new(width, height));
+    });
+}
+
+#[cfg(FALSE)]
+#[test]
+fn aspect_ratio_infinite_constraints() {
+    let id = WidgetId::next();
+    let (width, height) = (100., 100.);
+    let label = Label::new("hello!").fix_width(width).height(height);
+    let aspect = AspectRatioBox::<()>::new(label, 1.0)
+        .with_id(id)
+        .scroll()
+        .center();
+
+    let (window_width, window_height) = (600., 600.);
+
+    Harness::create_simple((), aspect, |harness| {
+        harness.set_initial_size(Size::new(window_width, window_height));
+        harness.send_initial_events();
+        harness.just_layout();
+        let state = harness.get_state(id);
+        assert_eq!(state.layout_rect().size(), Size::new(width, height));
+    });
+}
+
+#[cfg(FALSE)]
+#[test]
+fn aspect_ratio_tight_constraint_on_width() {
+    let id = WidgetId::next();
+    let label = Label::new("hello!");
+    let aspect = AspectRatioBox::<()>::new(label, 2.0)
+        .with_id(id)
+        .fix_width(300.)
+        .center();
+
+    let (window_width, window_height) = (600., 50.);
+
+    Harness::create_simple((), aspect, |harness| {
+        harness.set_initial_size(Size::new(window_width, window_height));
+        harness.send_initial_events();
+        harness.just_layout();
+        let state = harness.get_state(id);
+        assert_eq!(state.layout_rect().size(), Size::new(300., 50.));
+    });
+}
+
+#[cfg(FALSE)]
+#[test]
+fn aspect_ratio() {
+    let id = WidgetId::next();
+    let label = Label::new("hello!");
+    let aspect = AspectRatioBox::<()>::new(label, 2.0)
+        .with_id(id)
+        .center()
+        .center();
+
+    let (window_width, window_height) = (1000., 1000.);
+
+    Harness::create_simple((), aspect, |harness| {
+        harness.set_initial_size(Size::new(window_width, window_height));
+        harness.send_initial_events();
+        harness.just_layout();
+        let state = harness.get_state(id);
+        assert_eq!(state.layout_rect().size(), Size::new(1000., 500.));
+    });
+}
