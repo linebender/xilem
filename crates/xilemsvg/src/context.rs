96fc084ea18456dce487eff23778673a4f5c8552
Raph Levien <raph@google.com>
2023-04-25 20:56:46 -0700

    Add xilemsvg example
    This is a proof of concept that uses the common xilem_core to generate vector graphics content rendered as SVG DOM nodes in the browser.
    
    This could plausibly be made into an actual library, and it's likely that an evolution of this will be used to create interactive illustrations, for example in the raphlinus blog.

---
+++ crates/xilemsvg/src/context.rs
@@ -0,0 +1,98 @@
+use std::any::Any;
+
+use bitflags::bitflags;
+use web_sys::Document;
+
+use xilem_core::{Id, IdPath, Message};
+
+use crate::app::AppRunner;
+
+// Note: xilem has derive Clone here. Not sure.
+pub struct Cx {
+    id_path: IdPath,
+    document: Document,
+    app_ref: Option<Box<dyn AppRunner>>,
+}
+
+pub struct MessageThunk {
+    id_path: IdPath,
+    app_ref: Box<dyn AppRunner>,
+}
+
+bitflags! {
+    #[derive(Default)]
+    pub struct ChangeFlags: u32 {
+        const STRUCTURE = 1;
+        const OTHER_CHANGE = 2;
+    }
+}
+
+impl Cx {
+    pub fn new() -> Self {
+        let window = web_sys::window().expect("no global `window` exists");
+        let document = window.document().expect("should have a document on window");
+        Cx {
+            id_path: Vec::new(),
+            document,
+            app_ref: None,
+        }
+    }
+
+    pub fn push(&mut self, id: Id) {
+        self.id_path.push(id);
+    }
+
+    pub fn pop(&mut self) {
+        self.id_path.pop();
+    }
+
+    #[allow(unused)]
+    pub fn id_path(&self) -> &IdPath {
+        &self.id_path
+    }
+
+    /// Run some logic with an id added to the id path.
+    ///
+    /// This is an ergonomic helper that ensures proper nesting of the id path.
+    pub fn with_id<T, F: FnOnce(&mut Cx) -> T>(&mut self, id: Id, f: F) -> T {
+        self.push(id);
+        let result = f(self);
+        self.pop();
+        result
+    }
+
+    /// Allocate a new id and run logic with the new id added to the id path.
+    ///
+    /// Also an ergonomic helper.
+    pub fn with_new_id<T, F: FnOnce(&mut Cx) -> T>(&mut self, f: F) -> (Id, T) {
+        let id = Id::next();
+        self.push(id);
+        let result = f(self);
+        self.pop();
+        (id, result)
+    }
+
+    pub fn document(&self) -> &Document {
+        &self.document
+    }
+
+    pub fn message_thunk(&self) -> MessageThunk {
+        MessageThunk {
+            id_path: self.id_path.clone(),
+            app_ref: self.app_ref.as_ref().unwrap().clone_box(),
+        }
+    }
+    pub(crate) fn set_runner(&mut self, runner: impl AppRunner + 'static) {
+        self.app_ref = Some(Box::new(runner));
+    }
+}
+
+impl MessageThunk {
+    pub fn push_message(&self, message_body: impl Any + Send + 'static) {
+        let message = Message {
+            id_path: self.id_path.clone(),
+            body: Box::new(message_body),
+        };
+        self.app_ref.handle_message(message);
+    }
+}
