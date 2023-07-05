133b6c3e6dd098b09d0fd5239572f35c71d425e4
Philipp Mildenberger <philipp@mildenberger.me>
2023-07-05 17:02:07 +0200

    Fix macro compile issues (and fix clippy issue with unnecessary lifetimes)

--- crates/xilem_svg/src/view.rs
+++ crates/xilem_svg/src/view.rs
@@ -63,7 +63,7 @@ impl Pod {
         node.into_pod()
     }
 
-    fn downcast_mut<'a, T: 'static>(&'a mut self) -> Option<&'a mut T> {
+    fn downcast_mut<T: 'static>(&mut self) -> Option<&mut T> {
         self.0.as_any_mut().downcast_mut()
     }
 
@@ -74,4 +74,5 @@ impl Pod {
 
 xilem_core::generate_view_trait! {View, DomElement, Cx, ChangeFlags;}
 xilem_core::generate_viewsequence_trait! {ViewSequence, View, ViewMarker, DomElement, Cx, ChangeFlags, Pod;}
-xilem_core::generate_anyview_trait! {View, Cx, ChangeFlags, AnyElement}
+xilem_core::generate_anyview_trait! {AnyView, View, ViewMarker, Cx, ChangeFlags, AnyElement, BoxedView;}
+xilem_core::generate_memoize_view! {Memoize, MemoizeState, View, ViewMarker, Cx, ChangeFlags, s, memoize}
