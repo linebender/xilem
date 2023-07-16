252094ff7cb5f0f0a209ec270fbb1d8d519f0ea7
Jonas Platte <jplatte+git@posteo.de>
2023-07-16 16:07:14 +0200

    xilem_html: Simplify ViewExt implementation

--- crates/xilem_html/src/view_ext.rs
+++ crates/xilem_html/src/view_ext.rs
@@ -3,7 +3,7 @@
 
 use std::borrow::Cow;
 
-use crate::{class::Class, event::OptionalAction, events as e, view::View, Event};
+use crate::{class::Class, event::OptionalAction, events, view::View, Event};
 
 /// A trait that makes it possible to attach event listeners and more to views
 /// in the continuation style.
@@ -15,7 +15,10 @@ pub trait ViewExt<T, A>: View<T, A> + Sized {
     >(
         self,
         f: F,
-    ) -> e::OnClick<T, A, Self, F, OA>;
+    ) -> events::OnClick<T, A, Self, F, OA> {
+        events::on_click(self, f)
+    }
+
     /// Add an `ondblclick` event listener.
     fn on_dblclick<
         OA: OptionalAction<A>,
@@ -23,7 +26,10 @@ pub trait ViewExt<T, A>: View<T, A> + Sized {
     >(
         self,
         f: F,
-    ) -> e::OnDblClick<T, A, Self, F, OA>;
+    ) -> events::OnDblClick<T, A, Self, F, OA> {
+        events::on_dblclick(self, f)
+    }
+
     /// Add an `oninput` event listener.
     fn on_input<
         OA: OptionalAction<A>,
@@ -31,7 +37,10 @@ pub trait ViewExt<T, A>: View<T, A> + Sized {
     >(
         self,
         f: F,
-    ) -> e::OnInput<T, A, Self, F, OA>;
+    ) -> events::OnInput<T, A, Self, F, OA> {
+        events::on_input(self, f)
+    }
+
     /// Add an `onkeydown` event listener.
     fn on_keydown<
         OA: OptionalAction<A>,
@@ -39,64 +48,24 @@ pub trait ViewExt<T, A>: View<T, A> + Sized {
     >(
         self,
         f: F,
-    ) -> e::OnKeyDown<T, A, Self, F, OA>;
+    ) -> events::OnKeyDown<T, A, Self, F, OA> {
+        events::on_keydown(self, f)
+    }
+
     fn on_blur<
         OA: OptionalAction<A>,
         F: Fn(&mut T, &Event<web_sys::FocusEvent, Self::Element>) -> OA,
     >(
         self,
         f: F,
-    ) -> e::OnBlur<T, A, Self, F, OA>;
+    ) -> events::OnBlur<T, A, Self, F, OA> {
+        events::on_blur(self, f)
+    }
+
     /// Apply a CSS class to the child view.
     fn class(self, class: impl Into<Cow<'static, str>>) -> Class<Self> {
         crate::class::class(self, class)
     }
 }
 
-impl<T, A, V: View<T, A>> ViewExt<T, A> for V {
-    fn on_click<
-        OA: OptionalAction<A>,
-        F: Fn(&mut T, &Event<web_sys::MouseEvent, Self::Element>) -> OA,
-    >(
-        self,
-        f: F,
-    ) -> e::OnClick<T, A, Self, F, OA> {
-        e::on_click(self, f)
-    }
-    fn on_dblclick<
-        OA: OptionalAction<A>,
-        F: Fn(&mut T, &Event<web_sys::MouseEvent, Self::Element>) -> OA,
-    >(
-        self,
-        f: F,
-    ) -> e::OnDblClick<T, A, Self, F, OA> {
-        e::on_dblclick(self, f)
-    }
-    fn on_input<
-        OA: OptionalAction<A>,
-        F: Fn(&mut T, &Event<web_sys::InputEvent, Self::Element>) -> OA,
-    >(
-        self,
-        f: F,
-    ) -> e::OnInput<T, A, Self, F, OA> {
-        crate::events::on_input(self, f)
-    }
-    fn on_keydown<
-        OA: OptionalAction<A>,
-        F: Fn(&mut T, &Event<web_sys::KeyboardEvent, Self::Element>) -> OA,
-    >(
-        self,
-        f: F,
-    ) -> e::OnKeyDown<T, A, Self, F, OA> {
-        crate::events::on_keydown(self, f)
-    }
-    fn on_blur<
-        OA: OptionalAction<A>,
-        F: Fn(&mut T, &Event<web_sys::FocusEvent, Self::Element>) -> OA,
-    >(
-        self,
-        f: F,
-    ) -> e::OnBlur<T, A, Self, F, OA> {
-        e::on_blur(self, f)
-    }
-}
+impl<T, A, V: View<T, A>> ViewExt<T, A> for V {}
