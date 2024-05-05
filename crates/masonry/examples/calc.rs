linebender/xilem

commit 52bbfa047e859a11f1cc95d62a612f5e9318dc4d

A foundation for text editing and selection in Masonry (#241)
This brings in a lot of the old work from Druid and Masonry (prior to
linebender/masonry#56) on text, as well as some types from Glazier.

Needed work:
- [X] Text display using abstract types
- [x] Text selection with mouse
- [x] Text input with keyboard
- [ ] IME integration (of the kind winit understands)

Follow up work:
- [ ] Keyboard control of the selection area (hard-coded hotkeys)
- [ ] [Proper
placement](https://raphlinus.github.io/text/2020/10/26/text-layout.html#shaping-cluster)
of cursor
- [ ] Input methods
- [ ] Proper hotkey handling
- [ ] Copy and/or paste

crates/masonry/examples/calc.rs
@@ -222,9 +222,9 @@ impl AppDriver for CalcState {
         ctx.get_root::<Flex>()
             .child_mut(1)
             .unwrap()
-            .downcast::<Label>()
+            .downcast::<Label<String>>()
             .unwrap()
-            .set_text(self.value.clone());
+            .set_text((&*self.value).into());
     }
 }
 

crates/masonry/src/widget/button.rs
@@ -9,9 +9,10 @@ use vello::Scene;
 
 use crate::action::Action;
 use crate::paint_scene_helpers::{fill_lin_gradient, stroke, UnitPoint};
+use crate::text2::TextStorage;
 use crate::widget::{Label, WidgetMut, WidgetPod, WidgetRef};
 use crate::{
-    theme, ArcStr, BoxConstraints, EventCtx, Insets, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
+    theme, BoxConstraints, EventCtx, Insets, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
     PointerEvent, Size, StatusChange, TextEvent, Widget,
 };
 
@@ -23,11 +24,11 @@ const LABEL_INSETS: Insets = Insets::uniform_xy(8., 2.);
 /// A button with a text label.
 ///
 /// Emits [`Action::ButtonPressed`] when pressed.
-pub struct Button {
-    label: WidgetPod<Label>,
+pub struct Button<T: TextStorage> {
+    label: WidgetPod<Label<T>>,
 }
 
-impl Button {
+impl<T: TextStorage> Button<T> {
     /// Create a new button with a text label.
     ///
     /// # Examples
@@ -37,7 +38,7 @@ impl Button {
     ///
     /// let button = Button::new("Increment");
     /// ```
-    pub fn new(text: impl Into<ArcStr>) -> Button {
+    pub fn new(text: T) -> Button<T> {
         Button::from_label(Label::new(text))
     }
 
@@ -49,28 +50,28 @@ impl Button {
     /// use masonry::Color;
     /// use masonry::widget::{Button, Label};
     ///
-    /// let label = Label::new("Increment").with_text_color(Color::rgb(0.5, 0.5, 0.5));
+    /// let label = Label::new("Increment").with_text_brush(Color::rgb(0.5, 0.5, 0.5));
     /// let button = Button::from_label(label);
     /// ```
-    pub fn from_label(label: Label) -> Button {
+    pub fn from_label(label: Label<T>) -> Button<T> {
         Button {
             label: WidgetPod::new(label),
         }
     }
 }
 
-impl WidgetMut<'_, Button> {
+impl<T: TextStorage> WidgetMut<'_, Button<T>> {
     /// Set the text.
-    pub fn set_text(&mut self, new_text: impl Into<ArcStr>) {
-        self.label_mut().set_text(new_text.into());
+    pub fn set_text(&mut self, new_text: T) {
+        self.label_mut().set_text(new_text);
     }
 
-    pub fn label_mut(&mut self) -> WidgetMut<'_, Label> {
+    pub fn label_mut(&mut self) -> WidgetMut<'_, Label<T>> {
         self.ctx.get_mut(&mut self.widget.label)
     }
 }
 
-impl Widget for Button {
+impl<T: TextStorage> Widget for Button<T> {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         match event {
             PointerEvent::PointerDown(_, _) => {
@@ -181,7 +182,7 @@ impl Widget for Button {
     }
 
     fn get_debug_text(&self) -> Option<String> {
-        Some(self.label.as_ref().text().to_string())
+        Some(self.label.as_ref().text().as_str().to_string())
     }
 }
 
@@ -216,11 +217,10 @@ mod tests {
     #[test]
     fn edit_button() {
         let image_1 = {
-            let button = Button::from_label(
-                Label::new("The quick brown fox jumps over the lazy dog")
-                    .with_text_color(PRIMARY_LIGHT)
-                    .with_text_size(20.0),
-            );
+            let label = Label::new("The quick brown fox jumps over the lazy dog")
+                .with_text_brush(PRIMARY_LIGHT)
+                .with_text_size(20.0);
+            let button = Button::from_label(label);
 
             let mut harness = TestHarness::create_with_size(button, Size::new(50.0, 50.0));
 
@@ -233,12 +233,14 @@ mod tests {
             let mut harness = TestHarness::create_with_size(button, Size::new(50.0, 50.0));
 
             harness.edit_root_widget(|mut button| {
-                let mut button = button.downcast::<Button>().unwrap();
+                let mut button = button.downcast::<Button<&'static str>>().unwrap();
                 button.set_text("The quick brown fox jumps over the lazy dog");
 
                 let mut label = button.label_mut();
-                label.set_text_color(PRIMARY_LIGHT);
-                label.set_text_size(20.0);
+                label.set_text_properties(|props| {
+                    props.set_brush(PRIMARY_LIGHT);
+                    props.set_text_size(20.0);
+                });
             });
 
             harness.render()

crates/masonry/src/widget/checkbox.rs
@@ -11,6 +11,7 @@ use vello::Scene;
 use crate::action::Action;
 use crate::kurbo::{BezPath, Cap, Join, Size};
 use crate::paint_scene_helpers::{fill_lin_gradient, stroke, UnitPoint};
+use crate::text2::TextStorage;
 use crate::widget::{Label, WidgetMut, WidgetRef};
 use crate::{
     theme, ArcStr, BoxConstraints, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
@@ -18,14 +19,14 @@ use crate::{
 };
 
 /// A checkbox that can be toggled.
-pub struct Checkbox {
+pub struct Checkbox<T: TextStorage = ArcStr> {
     checked: bool,
-    label: WidgetPod<Label>,
+    label: WidgetPod<Label<T>>,
 }
 
-impl Checkbox {
+impl<T: TextStorage> Checkbox<T> {
     /// Create a new `Checkbox` with a text label.
-    pub fn new(checked: bool, text: impl Into<ArcStr>) -> Checkbox {
+    pub fn new(checked: bool, text: T) -> Checkbox<T> {
         Checkbox {
             checked,
             label: WidgetPod::new(Label::new(text)),
@@ -33,7 +34,7 @@ impl Checkbox {
     }
 
     /// Create a new `Checkbox` with the given label.
-    pub fn from_label(checked: bool, label: Label) -> Checkbox {
+    pub fn from_label(checked: bool, label: Label<T>) -> Checkbox<T> {
         Checkbox {
             checked,
             label: WidgetPod::new(label),
@@ -41,23 +42,23 @@ impl Checkbox {
     }
 }
 
-impl WidgetMut<'_, Checkbox> {
+impl<T: TextStorage> WidgetMut<'_, Checkbox<T>> {
     pub fn set_checked(&mut self, checked: bool) {
         self.widget.checked = checked;
         self.ctx.request_paint();
     }
 
     /// Set the text.
-    pub fn set_text(&mut self, new_text: impl Into<ArcStr>) {
-        self.label_mut().set_text(new_text.into());
+    pub fn set_text(&mut self, new_text: T) {
+        self.label_mut().set_text(new_text);
     }
 
-    pub fn label_mut(&mut self) -> WidgetMut<'_, Label> {
+    pub fn label_mut(&mut self) -> WidgetMut<'_, Label<T>> {
         self.ctx.get_mut(&mut self.widget.label)
     }
 }
 
-impl Widget for Checkbox {
+impl<T: TextStorage> Widget for Checkbox<T> {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         match event {
             PointerEvent::PointerDown(_, _) => {
@@ -177,7 +178,7 @@ impl Widget for Checkbox {
         Some(format!(
             "[{}] {}",
             if self.checked { "X" } else { " " },
-            self.label.as_ref().text()
+            self.label.as_ref().text().as_str()
         ))
     }
 }
@@ -225,7 +226,7 @@ mod tests {
             let checkbox = Checkbox::from_label(
                 true,
                 Label::new("The quick brown fox jumps over the lazy dog")
-                    .with_text_color(PRIMARY_LIGHT)
+                    .with_text_brush(PRIMARY_LIGHT)
                     .with_text_size(20.0),
             );
 
@@ -240,12 +241,12 @@ mod tests {
             let mut harness = TestHarness::create_with_size(checkbox, Size::new(50.0, 50.0));
 
             harness.edit_root_widget(|mut checkbox| {
-                let mut checkbox = checkbox.downcast::<Checkbox>().unwrap();
+                let mut checkbox = checkbox.downcast::<Checkbox<&'static str>>().unwrap();
                 checkbox.set_checked(true);
                 checkbox.set_text("The quick brown fox jumps over the lazy dog");
 
                 let mut label = checkbox.label_mut();
-                label.set_text_color(PRIMARY_LIGHT);
+                label.set_text_brush(PRIMARY_LIGHT);
                 label.set_text_size(20.0);
             });
 

commit 9542aaf2d8931d0a3efad4c84f47cea2d7f30845

Cleanup the text widgets to not be generic (#260)
This is fallout from #241. As discussed on Zulip, using different
underlying text types here is a very advanced feature, which will not be
needed by any real consumers. It also creates some footguns

Probably conflicts with #257, so that should land first

crates/masonry/examples/calc.rs
@@ -255,8 +255,8 @@ impl AppDriver for CalcState {
             .get_element()
             .child_mut(1)
             .unwrap()
-            .downcast::<Label<String>>()
-            .set_text((&*self.value).into());
+            .downcast::<Label>()
+            .set_text(&*self.value);
     }
 }
 

crates/masonry/src/widget/button.rs
@@ -13,7 +13,7 @@ use crate::paint_scene_helpers::{fill_lin_gradient, stroke, UnitPoint};
 use crate::text2::TextStorage;
 use crate::widget::{Label, WidgetMut, WidgetPod, WidgetRef};
 use crate::{
-    theme, AccessCtx, AccessEvent, BoxConstraints, EventCtx, Insets, LayoutCtx, LifeCycle,
+    theme, AccessCtx, AccessEvent, ArcStr, BoxConstraints, EventCtx, Insets, LayoutCtx, LifeCycle,
     LifeCycleCtx, PaintCtx, PointerEvent, Size, StatusChange, TextEvent, Widget,
 };
 
@@ -25,11 +25,11 @@ const LABEL_INSETS: Insets = Insets::uniform_xy(8., 2.);
 /// A button with a text label.
 ///
 /// Emits [`Action::ButtonPressed`] when pressed.
-pub struct Button<T: TextStorage> {
-    label: WidgetPod<Label<T>>,
+pub struct Button {
+    label: WidgetPod<Label>,
 }
 
-impl<T: TextStorage> Button<T> {
+impl Button {
     /// Create a new button with a text label.
     ///
     /// # Examples
@@ -39,7 +39,7 @@ impl<T: TextStorage> Button<T> {
     ///
     /// let button = Button::new("Increment");
     /// ```
-    pub fn new(text: T) -> Button<T> {
+    pub fn new(text: impl Into<ArcStr>) -> Button {
         Button::from_label(Label::new(text))
     }
 
@@ -54,25 +54,25 @@ impl<T: TextStorage> Button<T> {
     /// let label = Label::new("Increment").with_text_brush(Color::rgb(0.5, 0.5, 0.5));
     /// let button = Button::from_label(label);
     /// ```
-    pub fn from_label(label: Label<T>) -> Button<T> {
+    pub fn from_label(label: Label) -> Button {
         Button {
             label: WidgetPod::new(label),
         }
     }
 }
 
-impl<T: TextStorage> WidgetMut<'_, Button<T>> {
+impl WidgetMut<'_, Button> {
     /// Set the text.
-    pub fn set_text(&mut self, new_text: T) {
+    pub fn set_text(&mut self, new_text: impl Into<ArcStr>) {
         self.label_mut().set_text(new_text);
     }
 
-    pub fn label_mut(&mut self) -> WidgetMut<'_, Label<T>> {
+    pub fn label_mut(&mut self) -> WidgetMut<'_, Label> {
         self.ctx.get_mut(&mut self.widget.label)
     }
 }
 
-impl<T: TextStorage> Widget for Button<T> {
+impl Widget for Button {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         match event {
             PointerEvent::PointerDown(_, _) => {
@@ -261,7 +261,7 @@ mod tests {
             let mut harness = TestHarness::create_with_size(button, Size::new(50.0, 50.0));
 
             harness.edit_root_widget(|mut button| {
-                let mut button = button.downcast::<Button<&'static str>>();
+                let mut button = button.downcast::<Button>();
                 button.set_text("The quick brown fox jumps over the lazy dog");
 
                 let mut label = button.label_mut();

crates/masonry/src/widget/checkbox.rs
@@ -20,14 +20,14 @@ use crate::{
 };
 
 /// A checkbox that can be toggled.
-pub struct Checkbox<T: TextStorage = ArcStr> {
+pub struct Checkbox {
     checked: bool,
-    label: WidgetPod<Label<T>>,
+    label: WidgetPod<Label>,
 }
 
-impl<T: TextStorage> Checkbox<T> {
+impl Checkbox {
     /// Create a new `Checkbox` with a text label.
-    pub fn new(checked: bool, text: T) -> Checkbox<T> {
+    pub fn new(checked: bool, text: impl Into<ArcStr>) -> Checkbox {
         Checkbox {
             checked,
             label: WidgetPod::new(Label::new(text)),
@@ -35,7 +35,7 @@ impl<T: TextStorage> Checkbox<T> {
     }
 
     /// Create a new `Checkbox` with the given label.
-    pub fn from_label(checked: bool, label: Label<T>) -> Checkbox<T> {
+    pub fn from_label(checked: bool, label: Label) -> Checkbox {
         Checkbox {
             checked,
             label: WidgetPod::new(label),
@@ -43,23 +43,25 @@ impl<T: TextStorage> Checkbox<T> {
     }
 }
 
-impl<T: TextStorage> WidgetMut<'_, Checkbox<T>> {
+impl WidgetMut<'_, Checkbox> {
     pub fn set_checked(&mut self, checked: bool) {
         self.widget.checked = checked;
         self.ctx.request_paint();
     }
 
     /// Set the text.
-    pub fn set_text(&mut self, new_text: T) {
+    ///
+    /// We enforce this to be an `ArcStr` to make the allocation explicit.
+    pub fn set_text(&mut self, new_text: ArcStr) {
         self.label_mut().set_text(new_text);
     }
 
-    pub fn label_mut(&mut self) -> WidgetMut<'_, Label<T>> {
+    pub fn label_mut(&mut self) -> WidgetMut<'_, Label> {
         self.ctx.get_mut(&mut self.widget.label)
     }
 }
 
-impl<T: TextStorage> Widget for Checkbox<T> {
+impl Widget for Checkbox {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         match event {
             PointerEvent::PointerDown(_, _) => {
@@ -280,9 +282,9 @@ mod tests {
             let mut harness = TestHarness::create_with_size(checkbox, Size::new(50.0, 50.0));
 
             harness.edit_root_widget(|mut checkbox| {
-                let mut checkbox = checkbox.downcast::<Checkbox<&'static str>>();
+                let mut checkbox = checkbox.downcast::<Checkbox>();
                 checkbox.set_checked(true);
-                checkbox.set_text("The quick brown fox jumps over the lazy dog");
+                checkbox.set_text(ArcStr::from("The quick brown fox jumps over the lazy dog"));
 
                 let mut label = checkbox.label_mut();
                 label.set_text_brush(PRIMARY_LIGHT);

crates/masonry/src/widget/flex.rs
@@ -1392,7 +1392,7 @@ mod tests {
             let mut child = flex.child_mut(1).unwrap();
             assert_eq!(
                 child
-                    .try_downcast::<Label<&'static str>>()
+                    .try_downcast::<Label>()
                     .unwrap()
                     .widget
                     .text()

crates/masonry/src/widget/label.rs
@@ -34,26 +34,29 @@ pub enum LineBreaking {
 }
 
 /// A widget displaying non-editable text.
-pub struct Label<T: TextStorage> {
-    text_layout: TextLayout<T>,
+pub struct Label {
+    // We hardcode the underlying storage type as `ArcStr` for `Label`
+    // More advanced use cases will almost certainly need a custom widget, anyway
+    // (Rich text is not yet fully integrated, and so the architecture by which a label
+    // has rich text properties specified still needs to be designed)
+    text_layout: TextLayout<ArcStr>,
     line_break_mode: LineBreaking,
     show_disabled: bool,
     brush: TextBrush,
-    // disabled: bool,
 }
 
-impl<T: TextStorage> Label<T> {
+impl Label {
     /// Create a new label.
-    pub fn new(text: T) -> Self {
+    pub fn new(text: impl Into<ArcStr>) -> Self {
         Self {
-            text_layout: TextLayout::new(text, crate::theme::TEXT_SIZE_NORMAL as f32),
+            text_layout: TextLayout::new(text.into(), crate::theme::TEXT_SIZE_NORMAL as f32),
             line_break_mode: LineBreaking::Overflow,
             show_disabled: true,
             brush: crate::theme::TEXT_COLOR.into(),
         }
     }
 
-    pub fn text(&self) -> &T {
+    pub fn text(&self) -> &ArcStr {
         self.text_layout.text()
     }
 
@@ -85,21 +88,19 @@ impl<T: TextStorage> Label<T> {
         self.line_break_mode = line_break_mode;
         self
     }
-}
 
-impl Label<ArcStr> {
     /// Create a label with empty text.
     pub fn empty() -> Self {
-        Self::new("".into())
+        Self::new("")
     }
 }
 
-impl<T: TextStorage> WidgetMut<'_, Label<T>> {
-    pub fn text(&self) -> &T {
+impl WidgetMut<'_, Label> {
+    pub fn text(&self) -> &ArcStr {
         self.widget.text_layout.text()
     }
 
-    pub fn set_text_properties<R>(&mut self, f: impl FnOnce(&mut TextLayout<T>) -> R) -> R {
+    pub fn set_text_properties<R>(&mut self, f: impl FnOnce(&mut TextLayout<ArcStr>) -> R) -> R {
         let ret = f(&mut self.widget.text_layout);
         if self.widget.text_layout.needs_rebuild() {
             self.ctx.request_layout();
@@ -107,7 +108,8 @@ impl<T: TextStorage> WidgetMut<'_, Label<T>> {
         ret
     }
 
-    pub fn set_text(&mut self, new_text: T) {
+    pub fn set_text(&mut self, new_text: impl Into<ArcStr>) {
+        let new_text = new_text.into();
         self.set_text_properties(|layout| layout.set_text(new_text));
     }
 
@@ -138,7 +140,7 @@ impl<T: TextStorage> WidgetMut<'_, Label<T>> {
     }
 }
 
-impl<T: TextStorage> Widget for Label<T> {
+impl Widget for Label {
     fn on_pointer_event(&mut self, _ctx: &mut EventCtx, event: &PointerEvent) {
         match event {
             PointerEvent::PointerMove(_point) => {
@@ -353,7 +355,7 @@ mod tests {
             let mut harness = TestHarness::create_with_size(label, Size::new(50.0, 50.0));
 
             harness.edit_root_widget(|mut label| {
-                let mut label = label.downcast::<Label<&'static str>>();
+                let mut label = label.downcast::<Label>();
                 label.set_text("The quick brown fox jumps over the lazy dog");
                 label.set_text_brush(PRIMARY_LIGHT);
                 label.set_font_family(FontFamily::Generic(GenericFamily::Monospace));

crates/masonry/src/widget/prose.rs
@@ -12,10 +12,10 @@ use tracing::trace;
 use vello::{peniko::BlendMode, Scene};
 
 use crate::{
-    text2::{Selectable, TextBrush, TextWithSelection},
+    text2::{TextBrush, TextStorage, TextWithSelection},
     widget::label::LABEL_X_PADDING,
-    AccessCtx, AccessEvent, BoxConstraints, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
-    PointerEvent, StatusChange, TextEvent, Widget,
+    AccessCtx, AccessEvent, ArcStr, BoxConstraints, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx,
+    PaintCtx, PointerEvent, StatusChange, TextEvent, Widget,
 };
 
 use super::{LineBreaking, WidgetMut, WidgetRef};
@@ -26,17 +26,18 @@ use super::{LineBreaking, WidgetMut, WidgetRef};
 ///
 /// This should be preferred over [`Label`](super::Label) for most
 /// immutable text, other than that within
-pub struct Prose<T: Selectable> {
-    text_layout: TextWithSelection<T>,
+pub struct Prose {
+    // See `Label` for discussion of the choice of text type
+    text_layout: TextWithSelection<ArcStr>,
     line_break_mode: LineBreaking,
     show_disabled: bool,
     brush: TextBrush,
 }
 
-impl<T: Selectable> Prose<T> {
-    pub fn new(text: T) -> Self {
+impl Prose {
+    pub fn new(text: impl Into<ArcStr>) -> Self {
         Prose {
-            text_layout: TextWithSelection::new(text, crate::theme::TEXT_SIZE_NORMAL as f32),
+            text_layout: TextWithSelection::new(text.into(), crate::theme::TEXT_SIZE_NORMAL as f32),
             line_break_mode: LineBreaking::WordWrap,
             show_disabled: true,
             brush: crate::theme::TEXT_COLOR.into(),
@@ -44,7 +45,7 @@ impl<T: Selectable> Prose<T> {
     }
 
     // TODO: Can we reduce code duplication with `Label` widget somehow?
-    pub fn text(&self) -> &T {
+    pub fn text(&self) -> &ArcStr {
         self.text_layout.text()
     }
 
@@ -79,12 +80,15 @@ impl<T: Selectable> Prose<T> {
     }
 }
 
-impl<T: Selectable> WidgetMut<'_, Prose<T>> {
-    pub fn text(&self) -> &T {
+impl WidgetMut<'_, Prose> {
+    pub fn text(&self) -> &ArcStr {
         self.widget.text_layout.text()
     }
 
-    pub fn set_text_properties<R>(&mut self, f: impl FnOnce(&mut TextWithSelection<T>) -> R) -> R {
+    pub fn set_text_properties<R>(
+        &mut self,
+        f: impl FnOnce(&mut TextWithSelection<ArcStr>) -> R,
+    ) -> R {
         let ret = f(&mut self.widget.text_layout);
         if self.widget.text_layout.needs_rebuild() {
             self.ctx.request_layout();
@@ -92,7 +96,15 @@ impl<T: Selectable> WidgetMut<'_, Prose<T>> {
         ret
     }
 
-    pub fn set_text(&mut self, new_text: T) {
+    /// Change the text. If the user currently has a selection in the box, this will delete that selection.
+    ///
+    /// We enforce this to be an `ArcStr` to make the allocation explicit.
+    pub fn set_text(&mut self, new_text: ArcStr) {
+        if self.ctx.is_focused() {
+            tracing::info!(
+                "Called reset_text on a focused `Prose`. This will lose the user's current selection"
+            );
+        }
         self.set_text_properties(|layout| layout.set_text(new_text));
     }
 
@@ -123,7 +135,7 @@ impl<T: Selectable> WidgetMut<'_, Prose<T>> {
     }
 }
 
-impl<T: Selectable> Widget for Prose<T> {
+impl Widget for Prose {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         let window_origin = ctx.widget_state.window_origin();
         let inner_origin = Point::new(window_origin.x + LABEL_X_PADDING, window_origin.y);
@@ -210,6 +222,7 @@ impl<T: Selectable> Widget for Prose<T> {
                 ctx.request_layout();
             }
             LifeCycle::BuildFocusChain => {
+                // TODO: This is *definitely* empty
                 if !self.text_layout.text().links().is_empty() {
                     tracing::warn!("Links present in text, but not yet integrated");
                 }

crates/masonry/src/widget/textbox.rs
@@ -15,7 +15,7 @@ use vello::{
 };
 
 use crate::{
-    text2::{EditableText, TextBrush, TextEditor, TextWithSelection},
+    text2::{TextBrush, TextEditor, TextStorage, TextWithSelection},
     AccessCtx, AccessEvent, BoxConstraints, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
     PointerEvent, StatusChange, TextEvent, Widget,
 };
@@ -27,17 +27,22 @@ const TEXTBOX_PADDING: f64 = 4.0;
 /// The textbox widget is a widget which shows text which can be edited by the user
 ///
 /// For immutable text [`Prose`](super::Prose) should be preferred
-pub struct Textbox<T: EditableText> {
-    editor: TextEditor<T>,
+// TODO: RichTextBox 👀
+pub struct Textbox {
+    // We hardcode the underlying storage type as `String`.
+    // We might change this to a rope based structure at some point.
+    // If you need a text box which uses a different text type, you should
+    // create a custom widget
+    editor: TextEditor<String>,
     line_break_mode: LineBreaking,
     show_disabled: bool,
     brush: TextBrush,
 }
 
-impl<T: EditableText> Textbox<T> {
-    pub fn new(text: T) -> Self {
+impl Textbox {
+    pub fn new(initial_text: impl Into<String>) -> Self {
         Textbox {
-            editor: TextEditor::new(text, crate::theme::TEXT_SIZE_NORMAL as f32),
+            editor: TextEditor::new(initial_text.into(), crate::theme::TEXT_SIZE_NORMAL as f32),
             line_break_mode: LineBreaking::WordWrap,
             show_disabled: true,
             brush: crate::theme::TEXT_COLOR.into(),
@@ -45,7 +50,7 @@ impl<T: EditableText> Textbox<T> {
     }
 
     // TODO: Can we reduce code duplication with `Label` widget somehow?
-    pub fn text(&self) -> &T {
+    pub fn text(&self) -> &str {
         self.editor.text()
     }
 
@@ -80,12 +85,15 @@ impl<T: EditableText> Textbox<T> {
     }
 }
 
-impl<T: EditableText> WidgetMut<'_, Textbox<T>> {
-    pub fn text(&self) -> &T {
+impl WidgetMut<'_, Textbox> {
+    pub fn text(&self) -> &str {
         self.widget.editor.text()
     }
 
-    pub fn set_text_properties<R>(&mut self, f: impl FnOnce(&mut TextWithSelection<T>) -> R) -> R {
+    pub fn set_text_properties<R>(
+        &mut self,
+        f: impl FnOnce(&mut TextWithSelection<String>) -> R,
+    ) -> R {
         let ret = f(&mut self.widget.editor);
         if self.widget.editor.needs_rebuild() {
             self.ctx.request_layout();
@@ -93,10 +101,19 @@ impl<T: EditableText> WidgetMut<'_, Textbox<T>> {
         ret
     }
 
-    pub fn set_text(&mut self, new_text: T) {
-        // FIXME - Right now doing this resets the caret to the start of the text
-        // It's not clear whether this is the right behaviour, or if there even
-        // is one.
+    /// Reset the contents of the text box.
+    ///
+    /// This is likely to be disruptive if the user is focused on this widget,
+    /// and so should be avoided if possible.
+    // FIXME - it's not clear whether this is the right behaviour, or if there even
+    // is one.
+    // TODO: Create a method which sets the text and the cursor selection to be used if focused?
+    pub fn reset_text(&mut self, new_text: String) {
+        if self.ctx.is_focused() {
+            tracing::warn!(
+                "Called reset_text on a focused `Textbox`. This will lose the user's current selection and cursor"
+            );
+        }
         self.set_text_properties(|layout| layout.set_text(new_text));
     }
 
@@ -127,7 +144,7 @@ impl<T: EditableText> WidgetMut<'_, Textbox<T>> {
     }
 }
 
-impl<T: EditableText> Widget for Textbox<T> {
+impl Widget for Textbox {
     fn on_pointer_event(&mut self, ctx: &mut EventCtx, event: &PointerEvent) {
         let window_origin = ctx.widget_state.window_origin();
         let inner_origin = Point::new(
@@ -218,6 +235,7 @@ impl<T: EditableText> Widget for Textbox<T> {
                 ctx.request_layout();
             }
             LifeCycle::BuildFocusChain => {
+                // TODO: This will always be empty
                 if !self.editor.text().links().is_empty() {
                     tracing::warn!("Links present in text, but not yet integrated");
                 }

crates/masonry/src/widget/widget_ref.rs
@@ -216,9 +216,9 @@ mod tests {
         let label = WidgetPod::new(Label::new("Hello"));
         let dyn_widget: WidgetRef<dyn Widget> = label.as_dyn();
 
-        let label = dyn_widget.downcast::<Label<&'static str>>();
+        let label = dyn_widget.downcast::<Label>();
         assert_matches!(label, Some(_));
-        let label = dyn_widget.downcast::<Button<&'static str>>();
+        let label = dyn_widget.downcast::<Button>();
         assert_matches!(label, None);
     }
 
@@ -229,17 +229,7 @@ mod tests {
 
         let harness = TestHarness::create(label);
 
-        assert_matches!(
-            harness
-                .get_widget(label_id)
-                .downcast::<Label<&'static str>>(),
-            Some(_)
-        );
-        assert_matches!(
-            harness
-                .get_widget(label_id)
-                .downcast::<Button<&'static str>>(),
-            None
-        );
+        assert_matches!(harness.get_widget(label_id).downcast::<Label>(), Some(_));
+        assert_matches!(harness.get_widget(label_id).downcast::<Button>(), None);
     }
 }

crates/xilem_masonry/src/view/button.rs
@@ -24,7 +24,7 @@ impl<F, State, Action> MasonryView<State, Action> for Button<F>
 where
     F: Fn(&mut State) -> Action + Send + 'static,
 {
-    type Element = masonry::widget::Button<ArcStr>;
+    type Element = masonry::widget::Button;
     type ViewState = ();
 
     fn build(&self, cx: &mut ViewCx) -> (WidgetPod<Self::Element>, Self::ViewState) {

crates/xilem_masonry/src/view/label.rs
@@ -40,7 +40,7 @@ impl Label {
 }
 
 impl<State, Action> MasonryView<State, Action> for Label {
-    type Element = masonry::widget::Label<ArcStr>;
+    type Element = masonry::widget::Label;
     type ViewState = ();
 
     fn build(&self, _cx: &mut ViewCx) -> (WidgetPod<Self::Element>, Self::ViewState) {

crates/xilem_masonry/src/view/prose.rs
@@ -41,7 +41,7 @@ impl Prose {
 }
 
 impl<State, Action> MasonryView<State, Action> for Prose {
-    type Element = masonry::widget::Prose<ArcStr>;
+    type Element = masonry::widget::Prose;
     type ViewState = ();
 
     fn build(&self, _cx: &mut ViewCx) -> (WidgetPod<Self::Element>, Self::ViewState) {

crates/xilem_masonry/src/view/textbox.rs
@@ -63,7 +63,7 @@ impl<State, Action> Textbox<State, Action> {
 }
 
 impl<State: 'static, Action: 'static> MasonryView<State, Action> for Textbox<State, Action> {
-    type Element = masonry::widget::Textbox<String>;
+    type Element = masonry::widget::Textbox;
     type ViewState = ();
 
     fn build(&self, cx: &mut ViewCx) -> (WidgetPod<Self::Element>, Self::ViewState) {
@@ -89,9 +89,9 @@ impl<State: 'static, Action: 'static> MasonryView<State, Action> for Textbox<Sta
         // instead, we compare directly to the element's text. This is to handle
         // cases like "Previous data says contents is 'fooba', user presses 'r',
         // now data and contents are both 'foobar' but previous data is 'fooba'"
-        // withou calling `set_text`.
-        if self.contents != element.text().as_str() {
-            element.set_text(self.contents.clone());
+        // without calling `set_text`.
+        if self.contents != element.text() {
+            element.reset_text(self.contents.clone());
             changeflags.changed |= ChangeFlags::CHANGED.changed;
         }
 