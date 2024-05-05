3442069216a7843435cc55b39f9c55c091d498a3
Philipp Mildenberger <philipp@mildenberger.me>
2024-05-05 21:44:46 +0200

    Add license headers

--- xilem/examples/memoization.rs
+++ xilem/examples/memoization.rs
@@ -1,3 +1,6 @@
+// Copyright 2024 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
 use std::sync::Arc;
 use xilem::view::{button, flex, memoize};
 use xilem::{AnyMasonryView, MasonryView, Xilem};
--- xilem/src/view/arc.rs
+++ xilem/src/view/arc.rs
@@ -1,3 +1,6 @@
+// Copyright 2024 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
 use std::{any::Any, ops::Deref, sync::Arc};
 
 use masonry::widget::WidgetMut;
