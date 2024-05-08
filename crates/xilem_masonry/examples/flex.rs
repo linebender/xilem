385350a4746f5c715b81fbc3f23e47a1f9ad05fd
Pearce Keesling <keeslinp@gmail.com>
2024-05-08 09:10:30 -0700

    Slimmed down the flex demo it is more focused on demonstrating just the flex properties

--- crates/xilem_masonry/examples/flex.rs
+++ crates/xilem_masonry/examples/flex.rs
@@ -8,26 +8,15 @@ use xilem::{
     MasonryView, Xilem,
 };
 
-struct AppState {
-    count: u32,
-}
-
-impl AppState {
-    fn decrement(&mut self) {
-        if self.count > 0 {
-            self.count -= 1;
-        };
-    }
-    fn increment(&mut self) {
-        self.count += 1;
-    }
-}
-
-fn app_logic(data: &mut AppState) -> impl MasonryView<AppState> {
+fn app_logic(data: &mut i32) -> impl MasonryView<i32> {
     flex((
-        button("-", AppState::decrement),
-        label(format!("count: {}", data.count)),
-        button("+", AppState::increment),
+        button("-", |data| {
+            *data -= 1;
+        }),
+        label(format!("count: {}", data)),
+        button("+", |data| {
+            *data += 1;
+        }),
     ))
     .direction(xilem::Axis::Horizontal)
     .cross_axis_alignment(CrossAxisAlignment::Center)
@@ -35,8 +24,7 @@ fn app_logic(data: &mut AppState) -> impl MasonryView<AppState> {
 }
 
 fn main() -> Result<(), EventLoopError> {
-    let data = AppState { count: 0 };
-    let app = Xilem::new(data, app_logic);
-    app.run_windowed("Demo".into())?;
+    let app = Xilem::new(0, app_logic);
+    app.run_windowed("Centered Flex".into())?;
     Ok(())
 }
