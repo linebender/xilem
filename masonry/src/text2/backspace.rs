d97b249cb4652b0cc4b3a11332cf0b896487ee67
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-05-11 23:23:29 +0200

    Import tests from xi-editor
    Adapted from those added in
    https://github.com/xi-editor/xi-editor/pull/837
    
    Co-Authored-By: Yang Keao <keao.yang@yahoo.com>

--- masonry/src/text2/backspace.rs
+++ masonry/src/text2/backspace.rs
@@ -189,3 +189,316 @@ fn backspace_offset(text: &impl Selectable, start: usize) -> usize {
 pub fn offset_for_delete_backwards(caret_position: usize, text: &impl Selectable) -> usize {
     backspace_offset(text, caret_position)
 }
+
+#[cfg(test)]
+mod tests {
+    //! These tests originate from https://github.com/xi-editor/xi-editor/pull/837, with the logic itself
+    //! originating from
+
+    #[track_caller]
+    fn assert_delete_backwards(input: &'static str, target: &'static str) {
+        let result = super::offset_for_delete_backwards(input.len(), &input);
+        if result != target.len() {
+            panic!(
+                "Backspacing got {:?}, expected {:?}. Index: got {result}, expected {target}",
+                input.get(..result).unwrap_or("[INVALID RESULT INDEX]"),
+                target
+            );
+        }
+    }
+
+    #[track_caller]
+    fn assert_delete_backwards_seq(targets: &[&'static str]) {
+        let mut ran = false;
+        for val in targets.windows(2) {
+            ran = true;
+            assert_delete_backwards(val[0], val[1]);
+        }
+        if !ran {
+            panic!("Didn't execute");
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Backspacing got \"\", expected \"1\"")]
+    fn assert_delete_backwards_invalid() {
+        assert_delete_backwards("1", "1");
+    }
+
+    #[test]
+    fn delete_combining_enclosing_keycaps() {
+        // Including variation selector-18
+
+        assert_delete_backwards("1\u{E0101}\u{20E3}", "");
+
+        // multiple COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["1\u{20E3}\u{20E3}", "1\u{20E3}", ""]);
+
+        // Isolated multiple COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["\u{20E3}\u{20E3}", "\u{20E3}", ""]);
+    }
+
+    #[test]
+    fn delete_variation_selector_tests() {
+        // Isolated variation selector
+
+        assert_delete_backwards("\u{FE0F}", "");
+
+        assert_delete_backwards("\u{E0100}", "");
+
+        // Isolated multiple variation selectors
+        assert_delete_backwards("\u{FE0F}\u{FE0F}", "\u{FE0F}");
+        assert_delete_backwards("\u{FE0F}\u{E0100}", "\u{FE0F}");
+
+        assert_delete_backwards("\u{E0100}\u{FE0F}", "\u{E0100}");
+        assert_delete_backwards("\u{E0100}\u{E0100}", "\u{E0100}");
+
+        // Multiple variation selectors
+        assert_delete_backwards("#\u{FE0F}\u{FE0F}", "#\u{FE0F}");
+        assert_delete_backwards("#\u{FE0F}\u{E0100}", "#\u{FE0F}");
+
+        assert_delete_backwards("#\u{FE0F}", "");
+
+        assert_delete_backwards("#\u{E0100}\u{FE0F}", "#\u{E0100}");
+        assert_delete_backwards("#\u{E0100}\u{E0100}", "#\u{E0100}");
+
+        assert_delete_backwards("#\u{E0100}", "");
+    }
+
+    #[test]
+    fn delete_emoji_zwj_sequence_tests() {
+        // U+200D is ZERO WIDTH JOINER
+        assert_delete_backwards("\u{1F441}\u{200D}\u{1F5E8}", ""); // 👁‍🗨
+
+        // U+FE0E is variation selector-15
+
+        assert_delete_backwards("\u{1F441}\u{200D}\u{1F5E8}\u{FE0E}", "");
+        // 👁‍🗨︎
+
+        assert_delete_backwards("\u{1F469}\u{200D}\u{1F373}", "");
+        // 👩‍🍳
+
+        assert_delete_backwards("\u{1F487}\u{200D}\u{2640}", "");
+        // 💇‍♀
+
+        assert_delete_backwards("\u{1F487}\u{200D}\u{2640}\u{FE0F}", "");
+        // 💇‍♀️
+
+        assert_delete_backwards(
+            "\u{1F468}\u{200D}\u{2764}\u{FE0F}\u{200D}\u{1F48B}\u{200D}\u{1F468}",
+            "",
+        );
+        // 👨‍❤️‍💋‍👨
+
+        // Emoji modifier can be appended to the first emoji.
+
+        assert_delete_backwards("\u{1F469}\u{1F3FB}\u{200D}\u{1F4BC}", "");
+        // 👩🏻‍💼
+
+        // End with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{1F441}\u{200D}", "\u{1F441}", ""]); // 👁‍
+
+        // Start with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{200D}\u{1F5E8}", "\u{200D}", ""]);
+
+        assert_delete_backwards_seq(&[
+            "\u{FE0E}\u{200D}\u{1F5E8}",
+            "\u{FE0E}\u{200D}",
+            "\u{FE0E}",
+            "",
+        ]);
+
+        // Multiple ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&[
+            "\u{1F441}\u{200D}\u{200D}\u{1F5E8}",
+            "\u{1F441}\u{200D}\u{200D}",
+            "\u{1F441}\u{200D}",
+            "\u{1F441}",
+            "",
+        ]);
+
+        // Isolated multiple ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{200D}\u{200D}", "\u{200D}", ""]);
+    }
+
+    #[test]
+    fn delete_flags_tests() {
+        // Isolated regional indicator symbol
+
+        assert_delete_backwards("\u{1F1FA}", "");
+
+        // Odd numbered regional indicator symbols
+        assert_delete_backwards_seq(&["\u{1F1FA}\u{1F1F8}\u{1F1FA}", "\u{1F1FA}\u{1F1F8}", ""]);
+
+        // Incomplete sequence. (no tag_term: U+E007E)
+        assert_delete_backwards_seq(&[
+            "a\u{1F3F4}\u{E0067}b",
+            "a\u{1F3F4}\u{E0067}",
+            "a\u{1F3F4}",
+            "a",
+            "",
+        ]);
+
+        // No tag_base
+        assert_delete_backwards_seq(&[
+            "a\u{E0067}\u{E007F}b",
+            "a\u{E0067}\u{E007F}",
+            "a\u{E0067}",
+            "a",
+            "",
+        ]);
+
+        // Isolated tag chars
+        assert_delete_backwards_seq(&[
+            "a\u{E0067}\u{E0067}b",
+            "a\u{E0067}\u{E0067}",
+            "a\u{E0067}",
+            "a",
+            "",
+        ]);
+
+        // Isolated tab term.
+        assert_delete_backwards_seq(&[
+            "a\u{E007F}\u{E007F}b",
+            "a\u{E007F}\u{E007F}",
+            "a\u{E007F}",
+            "a",
+            "",
+        ]);
+
+        // Immediate tag_term after tag_base
+        assert_delete_backwards_seq(&[
+            "a\u{1F3F4}\u{E007F}\u{1F3F4}\u{E007F}b",
+            "a\u{1F3F4}\u{E007F}\u{1F3F4}\u{E007F}",
+            "a\u{1F3F4}\u{E007F}",
+            "a",
+            "",
+        ]);
+    }
+
+    #[test]
+    fn delete_emoji_modifier_tests() {
+        // U+1F3FB is EMOJI MODIFIER FITZPATRICK TYPE-1-2.
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}", ""]);
+
+        // Isolated emoji modifier
+        assert_delete_backwards_seq(&["\u{1F3FB}", ""]);
+
+        // Isolated multiple emoji modifier
+        assert_delete_backwards_seq(&["\u{1F3FB}\u{1F3FB}", "\u{1F3FB}", ""]);
+
+        // Multiple emoji modifiers
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}\u{1F3FB}", "\u{1F466}\u{1F3FB}", ""]);
+    }
+
+    #[test]
+    fn delete_mixed_edge_cases_tests() {
+        // COMBINING ENCLOSING KEYCAP + variation selector
+        assert_delete_backwards_seq(&["1\u{20E3}\u{FE0F}", "1", ""]);
+
+        // Variation selector + COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["\u{2665}\u{FE0F}\u{20E3}", "\u{2665}\u{FE0F}", ""]);
+
+        // COMBINING ENCLOSING KEYCAP + ending with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["1\u{20E3}\u{200D}", "1\u{20E3}", ""]);
+
+        // COMBINING ENCLOSING KEYCAP + ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&[
+            "1\u{20E3}\u{200D}\u{1F5E8}",
+            "1\u{20E3}\u{200D}",
+            "1\u{20E3}",
+            "",
+        ]);
+
+        // Start with ZERO WIDTH JOINER + COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["\u{200D}\u{20E3}", "\u{200D}", ""]);
+
+        // ZERO WIDTH JOINER + COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&[
+            "\u{1F441}\u{200D}\u{20E3}",
+            "\u{1F441}\u{200D}",
+            "\u{1F441}",
+            "",
+        ]);
+
+        // COMBINING ENCLOSING KEYCAP + regional indicator symbol
+        assert_delete_backwards_seq(&["1\u{20E3}\u{1F1FA}", "1\u{20E3}", ""]);
+
+        // Regional indicator symbol + COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["\u{1F1FA}\u{20E3}", "\u{1F1FA}", ""]);
+
+        // COMBINING ENCLOSING KEYCAP + emoji modifier
+        assert_delete_backwards_seq(&["1\u{20E3}\u{1F3FB}", "1\u{20E3}", ""]);
+
+        // Emoji modifier + COMBINING ENCLOSING KEYCAP
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}\u{20E3}", "\u{1F466}\u{1F3FB}", ""]);
+
+        // Variation selector + end with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{2665}\u{FE0F}\u{200D}", "\u{2665}\u{FE0F}", ""]);
+
+        // Variation selector + ZERO WIDTH JOINER
+
+        assert_delete_backwards("\u{1F469}\u{200D}\u{2764}\u{FE0F}\u{200D}\u{1F469}", "");
+
+        // Start with ZERO WIDTH JOINER + variation selector
+
+        assert_delete_backwards("\u{200D}\u{FE0F}", "");
+
+        // ZERO WIDTH JOINER + variation selector
+        assert_delete_backwards_seq(&["\u{1F469}\u{200D}\u{FE0F}", "\u{1F469}", ""]);
+
+        // Variation selector + regional indicator symbol
+        assert_delete_backwards_seq(&["\u{2665}\u{FE0F}\u{1F1FA}", "\u{2665}\u{FE0F}", ""]);
+
+        // Regional indicator symbol + variation selector
+
+        assert_delete_backwards("\u{1F1FA}\u{FE0F}", "");
+
+        // Variation selector + emoji modifier
+        assert_delete_backwards_seq(&["\u{2665}\u{FE0F}\u{1F3FB}", "\u{2665}\u{FE0F}", ""]);
+
+        // Emoji modifier + variation selector
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}\u{FE0F}", "\u{1F466}", ""]);
+
+        // Start withj ZERO WIDTH JOINER + regional indicator symbol
+        assert_delete_backwards_seq(&["\u{200D}\u{1F1FA}", "\u{200D}", ""]);
+
+        // ZERO WIDTH JOINER + Regional indicator symbol
+        assert_delete_backwards_seq(&[
+            "\u{1F469}\u{200D}\u{1F1FA}",
+            "\u{1F469}\u{200D}",
+            "\u{1F469}",
+            "",
+        ]);
+
+        // Regional indicator symbol + end with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{1F1FA}\u{200D}", "\u{1F1FA}", ""]);
+
+        // Regional indicator symbol + ZERO WIDTH JOINER
+
+        assert_delete_backwards("\u{1F1FA}\u{200D}\u{1F469}", "");
+
+        // Start with ZERO WIDTH JOINER + emoji modifier
+        assert_delete_backwards_seq(&["\u{200D}\u{1F3FB}", "\u{200D}", ""]);
+
+        // ZERO WIDTH JOINER + emoji modifier
+        assert_delete_backwards_seq(&[
+            "\u{1F469}\u{200D}\u{1F3FB}",
+            "\u{1F469}\u{200D}",
+            "\u{1F469}",
+            "",
+        ]);
+
+        // Emoji modifier + end with ZERO WIDTH JOINER
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}\u{200D}", "\u{1F466}\u{1F3FB}", ""]);
+
+        // Regional indicator symbol + Emoji modifier
+        assert_delete_backwards_seq(&["\u{1F1FA}\u{1F3FB}", "\u{1F1FA}", ""]);
+
+        // Emoji modifier + regional indicator symbol
+        assert_delete_backwards_seq(&["\u{1F466}\u{1F3FB}\u{1F1FA}", "\u{1F466}\u{1F3FB}", ""]);
+
+        // RIS + LF
+        assert_delete_backwards_seq(&["\u{1F1E6}\u{000A}", "\u{1F1E6}", ""]);
+    }
+}
