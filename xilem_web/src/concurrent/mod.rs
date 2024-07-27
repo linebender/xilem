43aca95e3c8bc4afe4874b24aea14df127980345
Philipp Mildenberger <philipp@mildenberger.me>
2024-07-27 17:50:37 +0200

    xilem_web: Add a `await_once` view, and a simple example `suspense` showing it in action.

--- xilem_web/src/concurrent/mod.rs
+++ xilem_web/src/concurrent/mod.rs
@@ -3,6 +3,9 @@
 
 //! Async views, allowing concurrent operations, like fetching data from a server
 
-mod memoized_await;
+mod await_once;
+pub use await_once::{await_once, AwaitOnce};
 
+mod memoized_await;
 pub use memoized_await::{memoized_await, MemoizedAwait};
+
