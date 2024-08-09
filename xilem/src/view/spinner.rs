fbb37803960c60ca8bfffcad3e41c4fec50e094a
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-08-09 16:37:18 +0100

    Fix TODO and add some docs

--- xilem/src/view/spinner.rs
+++ xilem/src/view/spinner.rs
@@ -6,17 +6,46 @@ use xilem_core::{Mut, ViewMarker};
 
 use crate::{MessageResult, Pod, View, ViewCtx, ViewId};
 
+/// An indefinite spinner.
+///
+/// This can be used to display that progress is happening on some process,
+/// but that the exact status is not known.
+///
+/// The underlying widget is the Masonry [Spinner](widget::Spinner).
+///
+/// # Examples
+///
+/// ```rust,no_run
+/// # use xilem::{view::{spinner, flex}, WidgetView, core::one_of::Either};
+/// # struct ApiClient;
+/// # struct RequestState { pending: bool }
+/// # impl RequestState {
+/// #     fn request_result(&mut self) -> impl WidgetView<ApiClient> { flex(()) }
+/// # }
+/// #
+/// fn show_request_outcome(data: &mut RequestState) -> impl WidgetView<ApiClient>  {
+///     if data.pending {
+///         Either::A(spinner())
+///     } else {
+///         Either::B(data.request_result())
+///     }
+/// }
+/// ```
 pub fn spinner() -> Spinner {
     Spinner { color: None }
 }
 
+/// The [`View`] created by [`spinner`].
+///
+/// See `spinner`'s docs for more details.
 pub struct Spinner {
     color: Option<Color>,
 }
 
 impl Spinner {
-    pub fn color(mut self, color: Color) -> Self {
-        self.color = Some(color);
+    /// Set the color for this spinner.
+    pub fn color(mut self, color: impl Into<Color>) -> Self {
+        self.color = Some(color.into());
         self
     }
 }
@@ -27,7 +56,7 @@ impl<State, Action> View<State, Action, ViewCtx> for Spinner {
     type ViewState = ();
 
     fn build(&self, _: &mut ViewCtx) -> (Self::Element, Self::ViewState) {
-        (Pod::new(masonry::widget::Spinner::new()), ())
+        (Pod::new(widget::Spinner::new()), ())
     }
 
     fn rebuild<'el>(
@@ -38,8 +67,10 @@ impl<State, Action> View<State, Action, ViewCtx> for Spinner {
         mut element: Mut<'el, Self::Element>,
     ) -> Mut<'el, Self::Element> {
         if prev.color != self.color {
-            // TODO: Don't duplicate the default colour from Masonry
-            element.set_color(self.color.unwrap_or(masonry::theme::TEXT_COLOR));
+            match self.color {
+                Some(color) => element.set_color(color),
+                None => element.reset_color(),
+            };
         }
         element
     }
