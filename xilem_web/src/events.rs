253eb8aba438c9847f89a0b5640b4e944fed14b7
Philipp Mildenberger <philipp@mildenberger.me>
2024-08-09 10:36:37 +0200

    xilem_web: Fix `View::message` (wrong `ViewId` check) in event views

--- xilem_web/src/events.rs
+++ xilem_web/src/events.rs
@@ -212,7 +212,7 @@ where
     let Some((first, remainder)) = id_path.split_first() else {
         throw_str("Parent view of `OnEvent` sent outdated and/or incorrect empty view path");
     };
-    if first.routing_id() != 0 {
+    if *first != ON_EVENT_VIEW_ID {
         throw_str("Parent view of `OnEvent` sent outdated and/or incorrect empty view path");
     }
     if remainder.is_empty() {
@@ -598,7 +598,7 @@ where
         let Some((first, remainder)) = id_path.split_first() else {
             throw_str("Parent view of `OnResize` sent outdated and/or incorrect empty view path");
         };
-        if first.routing_id() != 0 {
+        if *first != ON_EVENT_VIEW_ID {
             throw_str("Parent view of `OnResize` sent outdated and/or incorrect empty view path");
         }
         if remainder.is_empty() {
