991a485bd18e1a48bcdaf9ed28e355f96951b469
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-08-25 18:56:34 +0700

    masonry: Re-enable `clippy::doc_markdown` (#549)
    This is work towards #449.

--- masonry/src/lib.rs
+++ masonry/src/lib.rs
@@ -81,7 +81,7 @@
 //! [Xilem]: https://crates.io/crates/xilem
 
 // TODO: Remove this once the issues within masonry are fixed. Tracked in https://github.com/linebender/xilem/issues/449
-#![allow(rustdoc::broken_intra_doc_links, clippy::doc_markdown)]
+#![allow(rustdoc::broken_intra_doc_links)]
 #![deny(clippy::trivially_copy_pass_by_ref)]
 // #![deny(rustdoc::broken_intra_doc_links)]
 // #![warn(missing_docs)]
