251f42ae8791f6a648b2ab364d23fefbb6607c5b
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-08-28 15:49:12 +0100

    Fix the handling of `Flex::gap` (#560)
    Fixes https://github.com/linebender/xilem/issues/559
    
    See comment on the fix for an explanation. Essentially, the `major`
    value was used for multiple purposes, and this corrects into the
    expected "direction".

--- masonry/src/widget/flex.rs
+++ masonry/src/widget/flex.rs
@@ -847,6 +847,13 @@ impl Widget for Flex {
             tracing::warn!("A child of Flex is flex, but Flex is unbounded.");
         }
 
+        if !self.children.is_empty() {
+            // If we have at least one child, the last child added `gap` to `major`, which means that `major` is
+            // not the total size of the flex in the major axis, it's instead where the "next widget" will be placed.
+            // However, for the rest of this value, we need the total size of the widget in the major axis.
+            major -= gap;
+        }
+
         if flex_sum > 0.0 {
             major = total_major;
         }
