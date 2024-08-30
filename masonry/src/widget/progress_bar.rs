d9ffb55c5ce6c0c8855411dd23a79c7d3ef0a5ca
Richard Dodd (dodj) <richard.o.dodd@gmail.com>
2024-08-30 17:53:39 +0100

    Update masonry/src/widget/progress_bar.rs
    Co-authored-by: Olivier FAURE <couteaubleu@gmail.com>

--- masonry/src/widget/progress_bar.rs
+++ masonry/src/widget/progress_bar.rs
@@ -102,23 +102,13 @@ fn clamp_progress(progress: &mut Option<f64>) {
 
 // --- MARK: IMPL WIDGET ---
 impl Widget for ProgressBar {
-    fn on_pointer_event(&mut self, _ctx: &mut EventCtx, event: &PointerEvent) {
-        match event {
-            // pointer events unhandled for now
-            _ => (),
-        }
-    }
+    // pointer events unhandled for now
+    fn on_pointer_event(&mut self, _ctx: &mut EventCtx, _event: &PointerEvent) {}
 
     fn on_text_event(&mut self, _ctx: &mut EventCtx, _event: &TextEvent) {}
 
-    fn on_access_event(&mut self, ctx: &mut EventCtx, event: &AccessEvent) {
-        if event.target == ctx.widget_id() {
-            match event.action {
-                // access events unhandled for now
-                _ => {}
-            }
-        }
-    }
+    // access events unhandled for now
+    fn on_access_event(&mut self, _ctx: &mut EventCtx, _event: &AccessEvent) {}
 
     fn on_status_change(&mut self, ctx: &mut LifeCycleCtx, _event: &StatusChange) {
         ctx.request_paint();
