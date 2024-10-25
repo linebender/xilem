linebender/xilem

commit 53a5354c2df938e4122608abf6778499bc81f518

xilem_web: Rewrite modifiers (`Attributes`, `Classes` and `Styles`), and cleanup/extend docs (#699)
Previously the modifier systems had design issues i.e. bugs (non-deleted
styles/classes/attributes), and were unnecessary complex.
This aims to solve this (partly) by not using separate traits, but
concrete types and a different mechanism that is closer to how
`ElementSplice` works.

There's a few fundamental properties that composable type-based
modifiers need to support to avoid surprising/buggy behavior:

* Minimize actual changes to the underlying element, as DOM traffic is
expensive.
* Be compatible to memoization: e.g. a `Rotate` view should still be
applicable to possibly memoized transform values of the underlying
element.
* Recreation when the underlying element has changed (e.g. with a change
of variants of a `OneOf`).

To support all this, the modifier system needs to retain modifiers for
each modifier-view, and track its changes of the corresponding view.
Previously all elements were directly written and separated with markers
into a `Vec` to limit the boundaries of such views, but this had issues,
when e.g. all modifiers were deleted (e.g. clearing a `Vec` of classes),
by not reacting to this (I noticed that issue in the todomvc example
with the footer).

With this PR, the count of modifiers of a modifier-view are directly
stored either (hardcoded) in the view impl or its view state, which
cleans up the concrete modifier elements (such as `AttributeModifier`,
not including a separate `Marker` variant), and makes it less prone for
errors (and is slightly less memory-intensive).

The API to use these modifiers in modifier-views was also redesigned to
hopefully be more straight-forward/idiomatic. But as mentioned above
there's still challenges, which introduce complexity (which I'd like to
hide at least for simpler cases than these modifiers, likely in a future
PR).
All of this should now be documented in the new `modifier` module, where

xilem_web/src/svg/kurbo_shape.rs
@@ -4,22 +4,26 @@
 //! Implementation of the View trait for various kurbo shapes.
 
 use crate::{
-    attribute::WithAttributes,
     core::{MessageResult, Mut, OrphanView, ViewId},
-    AttributeValue, Attributes, DynMessage, IntoAttributeValue, Pod, ViewCtx, SVG_NS,
+    modifiers::{Attributes, With},
+    DynMessage, FromWithContext, Pod, ViewCtx, SVG_NS,
 };
 use peniko::kurbo::{BezPath, Circle, Line, Rect};
 
-fn create_element(name: &str, ctx: &mut ViewCtx, attr_size_hint: usize) -> Pod<web_sys::Element> {
-    ctx.add_modifier_size_hint::<Attributes>(attr_size_hint);
-    #[cfg(feature = "hydration")]
-    if ctx.is_hydrating() {
-        Pod::hydrate_element_with_ctx(Vec::new(), ctx.hydrate_node().unwrap(), ctx)
-    } else {
-        Pod::new_element_with_ctx(Vec::new(), SVG_NS, name, ctx)
-    }
-    #[cfg(not(feature = "hydration"))]
-    Pod::new_element_with_ctx(Vec::new(), SVG_NS, name, ctx)
+fn create_element<R>(
+    name: &str,
+    ctx: &mut ViewCtx,
+    attr_size_hint: usize,
+    f: impl FnOnce(Pod<web_sys::Element>, &mut ViewCtx) -> R,
+) -> R {
+    ctx.with_size_hint::<Attributes, _>(attr_size_hint, |ctx| {
+        let element = if ctx.is_hydrating() {
+            Pod::hydrate_element_with_ctx(Vec::new(), ctx)
+        } else {
+            Pod::new_element_with_ctx(Vec::new(), SVG_NS, name, ctx)
+        };
+        f(element, ctx)
+    })
 }
 
 impl<State: 'static, Action: 'static> OrphanView<Line, State, Action, DynMessage> for ViewCtx {
@@ -30,28 +34,31 @@ impl<State: 'static, Action: 'static> OrphanView<Line, State, Action, DynMessage
         view: &Line,
         ctx: &mut ViewCtx,
     ) -> (Self::OrphanElement, Self::OrphanViewState) {
-        let mut element: Self::OrphanElement = create_element("line", ctx, 4).into();
-        element.set_attribute(&"x1".into(), &view.p0.x.into_attr_value());
-        element.set_attribute(&"y1".into(), &view.p0.y.into_attr_value());
-        element.set_attribute(&"x2".into(), &view.p1.x.into_attr_value());
-        element.set_attribute(&"y2".into(), &view.p1.y.into_attr_value());
-        element.mark_end_of_attribute_modifier();
-        (element, ())
+        create_element("line", ctx, 4, |element, ctx| {
+            let mut element = Self::OrphanElement::from_with_ctx(element, ctx);
+            let attrs: &mut Attributes = element.modifier();
+            attrs.push(("x1", view.p0.x));
+            attrs.push(("y1", view.p0.y));
+            attrs.push(("x2", view.p1.x));
+            attrs.push(("y2", view.p1.y));
+            (element, ())
+        })
     }
 
     fn orphan_rebuild(
         new: &Line,
-        _prev: &Line,
+        prev: &Line,
         (): &mut Self::OrphanViewState,
         _ctx: &mut ViewCtx,
-        mut element: Mut<Self::OrphanElement>,
+        element: Mut<Self::OrphanElement>,
     ) {
-        element.rebuild_attribute_modifier();
-        element.set_attribute(&"x1".into(), &new.p0.x.into_attr_value());
-        element.set_attribute(&"y1".into(), &new.p0.y.into_attr_value());
-        element.set_attribute(&"x2".into(), &new.p1.x.into_attr_value());
-        element.set_attribute(&"y2".into(), &new.p1.y.into_attr_value());
-        element.mark_end_of_attribute_modifier();
+        Attributes::rebuild(element, 4, |mut element| {
+            let attrs: &mut Attributes = element.modifier();
+            attrs.update_with_same_key("x1", &prev.p0.x, &new.p0.x);
+            attrs.update_with_same_key("y1", &prev.p0.y, &new.p0.y);
+            attrs.update_with_same_key("x2", &prev.p1.x, &new.p1.x);
+            attrs.update_with_same_key("y2", &prev.p1.y, &new.p1.y);
+        });
     }
 
     fn orphan_teardown(
@@ -81,28 +88,31 @@ impl<State: 'static, Action: 'static> OrphanView<Rect, State, Action, DynMessage
         view: &Rect,
         ctx: &mut ViewCtx,
     ) -> (Self::OrphanElement, Self::OrphanViewState) {
-        let mut element: Self::OrphanElement = create_element("rect", ctx, 4).into();
-        element.set_attribute(&"x".into(), &view.x0.into_attr_value());
-        element.set_attribute(&"y".into(), &view.y0.into_attr_value());
-        element.set_attribute(&"width".into(), &view.width().into_attr_value());
-        element.set_attribute(&"height".into(), &view.height().into_attr_value());
-        element.mark_end_of_attribute_modifier();
-        (element, ())
+        create_element("rect", ctx, 4, |element, ctx| {
+            let mut element = Self::OrphanElement::from_with_ctx(element, ctx);
+            let attrs: &mut Attributes = element.modifier();
+            attrs.push(("x", view.x0));
+            attrs.push(("y", view.y0));
+            attrs.push(("width", view.width()));
+            attrs.push(("height", view.height()));
+            (element, ())
+        })
     }
 
     fn orphan_rebuild(
         new: &Rect,
-        _prev: &Rect,
+        prev: &Rect,
         (): &mut Self::OrphanViewState,
         _ctx: &mut ViewCtx,
-        mut element: Mut<Self::OrphanElement>,
+        element: Mut<Self::OrphanElement>,
     ) {
-        element.rebuild_attribute_modifier();
-        element.set_attribute(&"x".into(), &new.x0.into_attr_value());
-        element.set_attribute(&"y".into(), &new.y0.into_attr_value());
-        element.set_attribute(&"width".into(), &new.width().into_attr_value());
-        element.set_attribute(&"height".into(), &new.height().into_attr_value());
-        element.mark_end_of_attribute_modifier();
+        Attributes::rebuild(element, 4, |mut element| {
+            let attrs: &mut Attributes = element.modifier();
+            attrs.update_with_same_key("x", &prev.x0, &new.x0);
+            attrs.update_with_same_key("y", &prev.y0, &new.y0);
+            attrs.update_with_same_key("width", &prev.width(), &new.width());
+            attrs.update_with_same_key("height", &prev.height(), &new.height());
+        });
     }
 
     fn orphan_teardown(
@@ -132,26 +142,29 @@ impl<State: 'static, Action: 'static> OrphanView<Circle, State, Action, DynMessa
         view: &Circle,
         ctx: &mut ViewCtx,
     ) -> (Self::OrphanElement, Self::OrphanViewState) {
-        let mut element: Self::OrphanElement = create_element("circle", ctx, 3).into();
-        element.set_attribute(&"cx".into(), &view.center.x.into_attr_value());
-        element.set_attribute(&"cy".into(), &view.center.y.into_attr_value());
-        element.set_attribute(&"r".into(), &view.radius.into_attr_value());
-        element.mark_end_of_attribute_modifier();
-        (element, ())
+        create_element("circle", ctx, 3, |element, ctx| {
+            let mut element = Self::OrphanElement::from_with_ctx(element, ctx);
+            let attrs: &mut Attributes = element.modifier();
+            attrs.push(("cx", view.center.x));
+            attrs.push(("cy", view.center.y));
+            attrs.push(("r", view.radius));
+            (element, ())
+        })
     }
 
     fn orphan_rebuild(
         new: &Circle,
-        _prev: &Circle,
+        prev: &Circle,
         (): &mut Self::OrphanViewState,
         _ctx: &mut ViewCtx,
-        mut element: Mut<Self::OrphanElement>,
+        element: Mut<Self::OrphanElement>,
     ) {
-        element.rebuild_attribute_modifier();
-        element.set_attribute(&"cx".into(), &new.center.x.into_attr_value());
-        element.set_attribute(&"cy".into(), &new.center.y.into_attr_value());
-        element.set_attribute(&"r".into(), &new.radius.into_attr_value());
-        element.mark_end_of_attribute_modifier();
+        Attributes::rebuild(element, 3, |mut element| {
+            let attrs: &mut Attributes = element.modifier();
+            attrs.update_with_same_key("cx", &prev.center.x, &new.center.x);
+            attrs.update_with_same_key("cy", &prev.center.y, &new.center.y);
+            attrs.update_with_same_key("height", &prev.radius, &new.radius);
+        });
     }
 
     fn orphan_teardown(
@@ -174,34 +187,37 @@ impl<State: 'static, Action: 'static> OrphanView<Circle, State, Action, DynMessa
 }
 
 impl<State: 'static, Action: 'static> OrphanView<BezPath, State, Action, DynMessage> for ViewCtx {
-    type OrphanViewState = Option<AttributeValue>;
+    type OrphanViewState = ();
     type OrphanElement = Pod<web_sys::SvgPathElement>;
 
     fn orphan_build(
         view: &BezPath,
         ctx: &mut ViewCtx,
     ) -> (Self::OrphanElement, Self::OrphanViewState) {
-        let mut element: Self::OrphanElement = create_element("path", ctx, 1).into();
-        let svg_repr = view.to_svg().into_attr_value();
-        element.set_attribute(&"d".into(), &svg_repr);
-        element.mark_end_of_attribute_modifier();
-        (element, svg_repr)
+        create_element("path", ctx, 1, |element, ctx| {
+            let mut element = Self::OrphanElement::from_with_ctx(element, ctx);
+            element.props.attributes().push(("path", view.to_svg()));
+            (element, ())
+        })
     }
 
     fn orphan_rebuild(
         new: &BezPath,
         prev: &BezPath,
-        svg_repr: &mut Self::OrphanViewState,
+        (): &mut Self::OrphanViewState,
         _ctx: &mut ViewCtx,
-        mut element: Mut<Self::OrphanElement>,
+        element: Mut<Self::OrphanElement>,
     ) {
-        // slight optimization to avoid serialization/allocation
-        if new != prev {
-            *svg_repr = new.to_svg().into_attr_value();
-        }
-        element.rebuild_attribute_modifier();
-        element.set_attribute(&"d".into(), svg_repr);
-        element.mark_end_of_attribute_modifier();
+        Attributes::rebuild(element, 1, |mut element| {
+            let attrs: &mut Attributes = element.modifier();
+            if attrs.was_created() {
+                attrs.push(("path", new.to_svg()));
+            } else if new != prev {
+                attrs.mutate(|m| *m = ("path", new.to_svg()).into());
+            } else {
+                attrs.skip(1);
+            }
+        });
     }
 
     fn orphan_teardown(

xilem_core/src/views/one_of.rs
@@ -84,6 +84,33 @@ where
     }
 }
 
+impl<T, A, B, C, D, E, F, G, H, I> AsMut<T> for OneOf<A, B, C, D, E, F, G, H, I>
+where
+    A: AsMut<T>,
+    B: AsMut<T>,
+    C: AsMut<T>,
+    D: AsMut<T>,
+    E: AsMut<T>,
+    F: AsMut<T>,
+    G: AsMut<T>,
+    H: AsMut<T>,
+    I: AsMut<T>,
+{
+    fn as_mut(&mut self) -> &mut T {
+        match self {
+            OneOf::A(e) => <A as AsMut<T>>::as_mut(e),
+            OneOf::B(e) => <B as AsMut<T>>::as_mut(e),
+            OneOf::C(e) => <C as AsMut<T>>::as_mut(e),
+            OneOf::D(e) => <D as AsMut<T>>::as_mut(e),
+            OneOf::E(e) => <E as AsMut<T>>::as_mut(e),
+            OneOf::F(e) => <F as AsMut<T>>::as_mut(e),
+            OneOf::G(e) => <G as AsMut<T>>::as_mut(e),
+            OneOf::H(e) => <H as AsMut<T>>::as_mut(e),
+            OneOf::I(e) => <I as AsMut<T>>::as_mut(e),
+        }
+    }
+}
+
 /// A context type which can support [`OneOf9`] and [related views](super::one_of).
 ///
 /// This should be implemented by users of Xilem Core.

xilem_web/Cargo.toml
@@ -177,7 +177,6 @@ features = [
 ]
 
 [features]
-default = ["hydration"]
-hydration = []
+default = []
 # This interns some often used strings, such as element tags ("div" etc.), which slightly improves performance when creating elements at the cost of a bigger wasm binary
 intern_strings = ["wasm-bindgen/enable-interning"]

xilem_web/src/app.rs
@@ -141,7 +141,6 @@ where
                 &inner.root,
                 inner.ctx.fragment.clone(),
                 false,
-                #[cfg(feature = "hydration")]
                 false,
             );
             new_fragment.seq_rebuild(

xilem_web/src/attribute.rs
@@ -1,396 +1,0 @@
-// Copyright 2024 the Xilem Authors
-// SPDX-License-Identifier: Apache-2.0
-
-use crate::{
-    core::{MessageResult, Mut, View, ViewElement, ViewId, ViewMarker},
-    vecmap::VecMap,
-    AttributeValue, DomNode, DomView, DynMessage, ElementProps, Pod, PodMut, ViewCtx,
-};
-use std::marker::PhantomData;
-use wasm_bindgen::{JsCast, UnwrapThrowExt};
-
-type CowStr = std::borrow::Cow<'static, str>;
-
-/// This trait allows (modifying) HTML/SVG/MathML attributes on DOM [`Element`](`crate::interfaces::Element`)s.
-///
-/// Modifications have to be done on the up-traversal of [`View::rebuild`], i.e. after [`View::rebuild`] was invoked for descendent views.
-/// See [`Attr::build`] and [`Attr::rebuild`], how to use this for [`ViewElement`]s that implement this trait.
-/// When these methods are used, they have to be used in every reconciliation pass (i.e. [`View::rebuild`]).
-pub trait WithAttributes {
-    /// Needs to be invoked within a [`View::rebuild`] before traversing to descendent views, and before any modifications (with [`set_attribute`](`WithAttributes::set_attribute`)) are done in that view
-    fn rebuild_attribute_modifier(&mut self);
-
-    /// Needs to be invoked after any modifications are done
-    fn mark_end_of_attribute_modifier(&mut self);
-
-    /// Sets or removes (when value is `None`) an attribute from the underlying element.
-    ///
-    /// When in [`View::rebuild`] this has to be invoked *after* traversing the inner `View` with [`View::rebuild`]
-    fn set_attribute(&mut self, name: &CowStr, value: &Option<AttributeValue>);
-
-    // TODO first find a use-case for this...
-    // fn get_attr(&self, name: &str) -> Option<&AttributeValue>;
-}
-
-#[derive(Debug, PartialEq)]
-enum AttributeModifier {
-    Remove(CowStr),
-    Set(CowStr, AttributeValue),
-    EndMarker(u16),
-}
-
-const HYDRATING: u16 = 1 << 14;
-const CREATING: u16 = 1 << 15;
-const RESERVED_BIT_MASK: u16 = HYDRATING | CREATING;
-
-/// This contains all the current attributes of an [`Element`](`crate::interfaces::Element`)
-#[derive(Debug, Default)]
-pub struct Attributes {
-    attribute_modifiers: Vec<AttributeModifier>,
-    updated_attributes: VecMap<CowStr, ()>,
-    idx: u16,
-    /// the two most significant bits are reserved for whether this was just created (bit 15) and if it's currently being hydrated (bit 14)
-    start_idx: u16,
-}
-
-impl Attributes {
-    pub(crate) fn new(size_hint: usize, #[cfg(feature = "hydration")] in_hydration: bool) -> Self {
-        #[allow(unused_mut)]
-        let mut start_idx = CREATING;
-        #[cfg(feature = "hydration")]
-        if in_hydration {
-            start_idx |= HYDRATING;
-        }
-
-        Self {
-            attribute_modifiers: Vec::with_capacity(size_hint),
-            start_idx,
-            ..Default::default()
-        }
-    }
-}
-
-fn set_attribute(element: &web_sys::Element, name: &str, value: &str) {
-    debug_assert_ne!(
-        name, "class",
-        "Using `class` as attribute is not supported, use the `el.class()` modifier instead"
-    );
-    debug_assert_ne!(
-        name, "style",
-        "Using `style` as attribute is not supported, use the `el.style()` modifier instead"
-    );
-
-    // we have to special-case `value` because setting the value using `set_attribute`
-    // doesn't work after the value has been changed.
-    // TODO not sure, whether this is always a good idea, in case custom or other interfaces such as HtmlOptionElement elements are used that have "value" as an attribute name.
-    // We likely want to use the DOM attributes instead.
-    if name == "value" {
-        if let Some(input_element) = element.dyn_ref::<web_sys::HtmlInputElement>() {
-            input_element.set_value(value);
-        } else {
-            element.set_attribute("value", value).unwrap_throw();
-        }
-    } else if name == "checked" {
-        if let Some(input_element) = element.dyn_ref::<web_sys::HtmlInputElement>() {
-            input_element.set_checked(true);
-        } else {
-            element.set_attribute("checked", value).unwrap_throw();
-        }
-    } else {
-        element.set_attribute(name, value).unwrap_throw();
-    }
-}
-
-fn remove_attribute(element: &web_sys::Element, name: &str) {
-    debug_assert_ne!(
-        name, "class",
-        "Using `class` as attribute is not supported, use the `el.class()` modifier instead"
-    );
-    debug_assert_ne!(
-        name, "style",
-        "Using `style` as attribute is not supported, use the `el.style()` modifier instead"
-    );
-    // we have to special-case `checked` because setting the value using `set_attribute`
-    // doesn't work after the value has been changed.
-    if name == "checked" {
-        if let Some(input_element) = element.dyn_ref::<web_sys::HtmlInputElement>() {
-            input_element.set_checked(false);
-        } else {
-            element.remove_attribute("checked").unwrap_throw();
-        }
-    } else {
-        element.remove_attribute(name).unwrap_throw();
-    }
-}
-
-impl Attributes {
-    /// applies potential changes of the attributes of an element to the underlying DOM node
-    pub fn apply_attribute_changes(&mut self, element: &web_sys::Element) {
-        if (self.start_idx & HYDRATING) == HYDRATING {
-            self.start_idx &= !RESERVED_BIT_MASK;
-            return;
-        }
-
-        if (self.start_idx & CREATING) == CREATING {
-            for modifier in self.attribute_modifiers.iter().rev() {
-                match modifier {
-                    AttributeModifier::Remove(name) => {
-                        remove_attribute(element, name);
-                    }
-                    AttributeModifier::Set(name, value) => {
-                        set_attribute(element, name, &value.serialize());
-                    }
-                    AttributeModifier::EndMarker(_) => (),
-                }
-            }
-            self.start_idx &= !RESERVED_BIT_MASK;
-            debug_assert!(self.updated_attributes.is_empty());
-            return;
-        }
-
-        if !self.updated_attributes.is_empty() {
-            for modifier in self.attribute_modifiers.iter().rev() {
-                match modifier {
-                    AttributeModifier::Remove(name) => {
-                        if self.updated_attributes.remove(name).is_some() {
-                            remove_attribute(element, name);
-                        }
-                    }
-                    AttributeModifier::Set(name, value) => {
-                        if self.updated_attributes.remove(name).is_some() {
-                            set_attribute(element, name, &value.serialize());
-                        }
-                    }
-                    AttributeModifier::EndMarker(_) => (),
-                }
-            }
-            debug_assert!(self.updated_attributes.is_empty());
-        }
-    }
-}
-
-impl WithAttributes for Attributes {
-    fn set_attribute(&mut self, name: &CowStr, value: &Option<AttributeValue>) {
-        if (self.start_idx & RESERVED_BIT_MASK) != 0 {
-            let modifier = if let Some(value) = value {
-                AttributeModifier::Set(name.clone(), value.clone())
-            } else {
-                AttributeModifier::Remove(name.clone())
-            };
-            self.attribute_modifiers.push(modifier);
-        } else if let Some(modifier) = self.attribute_modifiers.get_mut(self.idx as usize) {
-            let dirty = match (&modifier, value) {
-                // early return if nothing has changed, avoids allocations
-                (AttributeModifier::Set(old_name, old_value), Some(new_value))
-                    if old_name == name =>
-                {
-                    if old_value == new_value {
-                        false
-                    } else {
-                        self.updated_attributes.insert(name.clone(), ());
-                        true
-                    }
-                }
-                (AttributeModifier::Remove(removed), None) if removed == name => false,
-                (AttributeModifier::Set(old_name, _), None)
-                | (AttributeModifier::Remove(old_name), Some(_))
-                    if old_name == name =>
-                {
-                    self.updated_attributes.insert(name.clone(), ());
-                    true
-                }
-                (AttributeModifier::EndMarker(_), None)
-                | (AttributeModifier::EndMarker(_), Some(_)) => {
-                    self.updated_attributes.insert(name.clone(), ());
-                    true
-                }
-                (AttributeModifier::Set(old_name, _), _)
-                | (AttributeModifier::Remove(old_name), _) => {
-                    self.updated_attributes.insert(name.clone(), ());
-                    self.updated_attributes.insert(old_name.clone(), ());
-                    true
-                }
-            };
-            if dirty {
-                *modifier = if let Some(value) = value {
-                    AttributeModifier::Set(name.clone(), value.clone())
-                } else {
-                    AttributeModifier::Remove(name.clone())
-                };
-            }
-            // else remove it out of updated_attributes? (because previous attributes are overwritten) not sure if worth it because potentially worse perf
-        } else {
-            let new_modifier = if let Some(value) = value {
-                AttributeModifier::Set(name.clone(), value.clone())
-            } else {
-                AttributeModifier::Remove(name.clone())
-            };
-            self.updated_attributes.insert(name.clone(), ());
-            self.attribute_modifiers.push(new_modifier);
-        }
-        self.idx += 1;
-    }
-
-    fn rebuild_attribute_modifier(&mut self) {
-        if self.idx == 0 {
-            self.start_idx &= RESERVED_BIT_MASK;
-        } else {
-            let AttributeModifier::EndMarker(start_idx) =
-                self.attribute_modifiers[(self.idx - 1) as usize]
-            else {
-                unreachable!("this should not happen, as either `rebuild_attribute_modifier` happens first, or follows an `mark_end_of_attribute_modifier`")
-            };
-            self.idx = start_idx;
-            self.start_idx = start_idx | (self.start_idx & RESERVED_BIT_MASK);
-        }
-    }
-
-    fn mark_end_of_attribute_modifier(&mut self) {
-        let start_idx = self.start_idx & !RESERVED_BIT_MASK;
-        match self.attribute_modifiers.get_mut(self.idx as usize) {
-            Some(AttributeModifier::EndMarker(prev_start_idx)) if *prev_start_idx == start_idx => {} // attribute modifier hasn't changed
-            Some(modifier) => *modifier = AttributeModifier::EndMarker(start_idx),
-            None => self
-                .attribute_modifiers
-                .push(AttributeModifier::EndMarker(start_idx)),
-        }
-        self.idx += 1;
-        self.start_idx = self.idx | (self.start_idx & RESERVED_BIT_MASK);
-    }
-}
-
-impl WithAttributes for ElementProps {
-    fn rebuild_attribute_modifier(&mut self) {
-        self.attributes().rebuild_attribute_modifier();
-    }
-
-    fn mark_end_of_attribute_modifier(&mut self) {
-        self.attributes().mark_end_of_attribute_modifier();
-    }
-
-    fn set_attribute(&mut self, name: &CowStr, value: &Option<AttributeValue>) {
-        self.attributes().set_attribute(name, value);
-    }
-}
-
-impl<N> WithAttributes for Pod<N>
-where
-    N: DomNode,
-    N::Props: WithAttributes,
-{
-    fn rebuild_attribute_modifier(&mut self) {
-        self.props.rebuild_attribute_modifier();
-    }
-
-    fn mark_end_of_attribute_modifier(&mut self) {
-        self.props.mark_end_of_attribute_modifier();
-    }
-
-    fn set_attribute(&mut self, name: &CowStr, value: &Option<AttributeValue>) {
-        self.props.set_attribute(name, value);
-    }
-}
-
-impl<N> WithAttributes for PodMut<'_, N>
-where
-    N: DomNode,
-    N::Props: WithAttributes,
-{
-    fn rebuild_attribute_modifier(&mut self) {
-        self.props.rebuild_attribute_modifier();
-    }
-
-    fn mark_end_of_attribute_modifier(&mut self) {
-        self.props.mark_end_of_attribute_modifier();
-    }
-
-    fn set_attribute(&mut self, name: &CowStr, value: &Option<AttributeValue>) {
-        self.props.set_attribute(name, value);
-    }
-}
-
-/// Syntax sugar for adding a type bound on the `ViewElement` of a view, such that both, [`ViewElement`] and [`ViewElement::Mut`] are bound to [`WithAttributes`]
-pub trait ElementWithAttributes:
-    for<'a> ViewElement<Mut<'a>: WithAttributes> + WithAttributes
-{
-}
-
-impl<T> ElementWithAttributes for T
-where
-    T: ViewElement + WithAttributes,
-    for<'a> T::Mut<'a>: WithAttributes,
-{
-}
-
-/// A view to add or remove an attribute to/from an element, see [`Element::attr`](`crate::interfaces::Element::attr`) for how it's usually used.
-#[derive(Clone, Debug)]
-pub struct Attr<E, T, A> {
-    el: E,
-    name: CowStr,
-    value: Option<AttributeValue>,
-    phantom: PhantomData<fn() -> (T, A)>,
-}
-
-impl<E, T, A> Attr<E, T, A> {
-    pub fn new(el: E, name: CowStr, value: Option<AttributeValue>) -> Self {
-        Attr {
-            el,
-            name,
-            value,
-            phantom: PhantomData,
-        }
-    }
-}
-
-impl<E, T, A> ViewMarker for Attr<E, T, A> {}
-impl<E, T, A> View<T, A, ViewCtx, DynMessage> for Attr<E, T, A>
-where
-    T: 'static,
-    A: 'static,
-    E: DomView<T, A, DomNode: DomNode<Props: WithAttributes>>,
-{
-    type Element = E::Element;
-
-    type ViewState = E::ViewState;
-
-    fn build(&self, ctx: &mut ViewCtx) -> (Self::Element, Self::ViewState) {
-        ctx.add_modifier_size_hint::<Attributes>(1);
-        let (mut element, state) = self.el.build(ctx);
-        element.set_attribute(&self.name, &self.value);
-        element.mark_end_of_attribute_modifier();
-        (element, state)
-    }
-
-    fn rebuild(
-        &self,
-        prev: &Self,
-        view_state: &mut Self::ViewState,
-        ctx: &mut ViewCtx,
-        mut element: Mut<Self::Element>,
-    ) {
-        element.rebuild_attribute_modifier();
-        self.el
-            .rebuild(&prev.el, view_state, ctx, element.reborrow_mut());
-        element.set_attribute(&self.name, &self.value);
-        element.mark_end_of_attribute_modifier();
-    }
-
-    fn teardown(
-        &self,
-        view_state: &mut Self::ViewState,
-        ctx: &mut ViewCtx,
-        element: Mut<Self::Element>,
-    ) {
-        self.el.teardown(view_state, ctx, element);
-    }
-
-    fn message(
-        &self,
-        view_state: &mut Self::ViewState,
-        id_path: &[ViewId],
-        message: DynMessage,
-        app_state: &mut T,
-    ) -> MessageResult<A, DynMessage> {
-        self.el.message(view_state, id_path, message, app_state)
-    }
-}

commit 16df6712d1462af84188ab93d62bd8e0d05b9e7c

Fix kurbo::BezPath d attribute for xilem_web::svg::path element (#715)
Regressed in #699

cc @Philipp-M 🦆

xilem_web/src/svg/kurbo_shape.rs
@@ -196,7 +196,7 @@ impl<State: 'static, Action: 'static> OrphanView<BezPath, State, Action, DynMess
     ) -> (Self::OrphanElement, Self::OrphanViewState) {
         create_element("path", ctx, 1, |element, ctx| {
             let mut element = Self::OrphanElement::from_with_ctx(element, ctx);
-            element.props.attributes().push(("path", view.to_svg()));
+            element.props.attributes().push(("d", view.to_svg()));
             (element, ())
         })
     }
@@ -211,9 +211,9 @@ impl<State: 'static, Action: 'static> OrphanView<BezPath, State, Action, DynMess
         Attributes::rebuild(element, 1, |mut element| {
             let attrs: &mut Attributes = element.modifier();
             if attrs.was_created() {
-                attrs.push(("path", new.to_svg()));
+                attrs.push(("d", new.to_svg()));
             } else if new != prev {
-                attrs.mutate(|m| *m = ("path", new.to_svg()).into());
+                attrs.mutate(|m| *m = ("d", new.to_svg()).into());
             } else {
                 attrs.skip(1);
             }