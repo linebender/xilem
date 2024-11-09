acf87607bf8b798a3f7b7d2b637ac1e2ceca1488
Philipp Mildenberger <philipp@mildenberger.me>
2024-11-09 13:11:54 +0100

    xilem_web: Extend `svgdraw` example to be an infinitely zoomable canvas (with mousewheel)

--- xilem_web/web_examples/svgdraw/src/main.rs
+++ xilem_web/web_examples/svgdraw/src/main.rs
@@ -3,6 +3,7 @@
 
 //! An example showing how SVG paths can be used for a vector-drawing application
 
+use std::rc::Rc;
 use wasm_bindgen::UnwrapThrowExt;
 use xilem_web::{
     document_body,
@@ -17,7 +18,7 @@ use xilem_web::{
         kurbo::{BezPath, Point, QuadSpline, Shape, Stroke},
         peniko::Color,
     },
-    App, DomFragment, PointerMsg,
+    AnyDomView, App, DomFragment, PointerMsg,
 };
 
 const RAINBOW_COLORS: &[Color] = &[
@@ -70,37 +71,111 @@ impl SplineLine {
 
 #[derive(Default)]
 struct Draw {
-    lines: Vec<SplineLine>,
+    cur_line: Option<SplineLine>,
+    cursor_position: Point,
+    canvas_position: Point,
+    draw_position: Point,
+    lines_cached: Vec<Rc<AnyDomView<Draw>>>,
     new_line_width: f64,
     is_drawing: bool,
+    is_moving: bool,
+    zoom: f64,
 }
 
 impl Draw {
+    fn update_cursor(&mut self, cursor: Point) {
+        let last_cursor_position = self.cursor_position;
+        self.cursor_position = cursor;
+        let cursor_delta = self.cursor_position - last_cursor_position;
+        let zoom_corrected_delta = cursor_delta / self.zoom;
+        self.draw_position += zoom_corrected_delta;
+        if self.is_moving {
+            self.canvas_position -= zoom_corrected_delta;
+            self.draw_position -= zoom_corrected_delta;
+        }
+    }
+
+    // TODO support pinch as well
+    fn zoom_with_wheel_event(&mut self, event: web_sys::WheelEvent) {
+        let delta_mode_factor = match event.delta_mode() {
+            2 => 100.0, // Pages
+            1 => 16.0,  // Lines
+            _ => 1.0,   // Pixels and Default
+        };
+        let mut delta = event.delta_y() * delta_mode_factor;
+
+        let delta_sign = delta.signum();
+        let factor = 1.0 + 0.001 * delta.abs();
+        delta = if delta_sign < 0.0 {
+            factor
+        } else {
+            1.0 / factor
+        };
+
+        self.zoom *= delta;
+        self.canvas_position -= (self.cursor_position.to_vec2() * (1.0 - delta)) / self.zoom;
+    }
+
+    fn start_new_line(&mut self) {
+        let line = SplineLine::new(self.draw_position, random_color(), self.new_line_width);
+        self.lines_cached
+            .push(Rc::new(line.view()) as Rc<AnyDomView<Self>>);
+        self.cur_line = Some(line);
+        self.is_drawing = true;
+    }
+
+    fn extend_current_line(&mut self) {
+        if self.is_drawing {
+            let cur_line = self.cur_line.as_mut().unwrap();
+            cur_line.points.push(self.draw_position);
+            *self.lines_cached.last_mut().unwrap() =
+                Rc::new(cur_line.view()) as Rc<AnyDomView<Self>>;
+        }
+    }
+
+    fn finish_current_line(&mut self) {
+        self.is_drawing = false;
+    }
+
     fn view(&mut self) -> impl DomFragment<Self> {
-        let lines = self.lines.iter().map(SplineLine::view).collect::<Vec<_>>();
-        let canvas = svg(g(lines).fill(Color::TRANSPARENT))
-            .pointer(|state: &mut Self, e| {
-                match e {
-                    PointerMsg::Down(p) => {
-                        let l = SplineLine::new(p.position, random_color(), state.new_line_width);
-                        state.lines.push(l);
-                        state.is_drawing = true;
-                    }
-                    PointerMsg::Move(p) => {
-                        if state.is_drawing {
-                            state.lines.last_mut().unwrap().points.push(p.position);
-                        }
-                    }
-                    PointerMsg::Up(_) => state.is_drawing = false,
-                };
-            })
-            .style([s("width", "100vw"), s("height", "100vh")]);
+        let x = -self.canvas_position.x;
+        let y = -self.canvas_position.y;
+        let zoom = self.zoom;
+        let canvas = svg(g(self.lines_cached.clone())
+            .fill(Color::TRANSPARENT)
+            .style(s(
+                "transform",
+                format!("scale({zoom}) translate({x}px, {y}px)"),
+            )))
+        .pointer(|state: &mut Self, e| {
+            state.update_cursor(e.position());
+            match e {
+                PointerMsg::Down(e) => match e.button {
+                    0 => state.start_new_line(),
+                    1 | 2 => state.is_moving = true,
+                    _ => (),
+                },
+                PointerMsg::Move(_) => state.extend_current_line(),
+                PointerMsg::Up(e) => match e.button {
+                    0 => state.finish_current_line(),
+                    1 | 2 => state.is_moving = false,
+                    _ => (),
+                },
+            };
+        })
+        .style([s("width", "100vw"), s("height", "100vh")])
+        .on_wheel(|state, event| state.zoom_with_wheel_event(event))
+        .on_click(|_, event| event.prevent_default())
+        .passive(false)
+        .on_contextmenu(|_, event| event.prevent_default())
+        .passive(false);
 
         let controls = label((
-            span("Stroke width:"),
+            // a space width would be more ideal, but for some reason spaces are truncated...
+            span(format!("Stroke width {:>05.2}: ", self.new_line_width)),
             div(input(())
                 .attr("type", "range")
-                .attr("min", 1)
+                .attr("min", 0.1)
                 .attr("max", 30)
                 .attr("step", 0.01)
                 .attr("value", self.new_line_width)
@@ -123,6 +198,7 @@ fn main() {
         document_body(),
         Draw {
             new_line_width: 5.0,
+            zoom: 1.0,
             ..Draw::default()
         },
         Draw::view,
