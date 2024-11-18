2898db4f58e2eb7c5d31265e3bc4000fdff841b3
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-11-18 15:41:47 +0000

    Make `text_contexts` always available

--- masonry/src/contexts.rs
+++ masonry/src/contexts.rs
@@ -199,6 +199,26 @@ impl_context_method!(
     }
 );
 
+// Methods for all exclusive context types (i.e. those which have exclusive access to the global state).
+impl_context_method! {
+    AccessCtx<'_>,
+    ComposeCtx<'_>,
+    EventCtx<'_>,
+    LayoutCtx<'_>,
+    MutateCtx<'_>,
+    PaintCtx<'_>,
+    UpdateCtx<'_>,
+    {
+        /// Get the contexts needed to build and paint text sections.
+        ///
+        /// Note that in many cases, these contexts are.
+        pub fn text_contexts(&mut self) -> (&mut FontContext, &mut LayoutContext<BrushIndex>) {
+        (
+            &mut self.global_state.font_context,
+            &mut self.global_state.text_layout_context,
+        )
+    }
+}}
 // --- MARK: GET LAYOUT ---
 // Methods on all context types except LayoutCtx
 // These methods access layout info calculated during the layout pass.
@@ -1118,17 +1138,6 @@ impl ComposeCtx<'_> {
         }
     }
 }
-
-// --- MARK: OTHER STUFF ---
-impl_context_method!(EventCtx<'_>, LayoutCtx<'_>, PaintCtx<'_>, {
-    /// Get the contexts needed to build and paint text sections.
-    pub fn text_contexts(&mut self) -> (&mut FontContext, &mut LayoutContext<BrushIndex>) {
-        (
-            &mut self.global_state.font_context,
-            &mut self.global_state.text_layout_context,
-        )
-    }
-});
 
 impl PaintCtx<'_> {
     /// Whether debug paint is enabled.
