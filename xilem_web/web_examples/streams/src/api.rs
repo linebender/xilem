919a6e62d4b37aeeefe6e899d93e3a8ec815b537
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-11-26 01:59:11 +0100

    Add an example for memoized_stream

---
+++ xilem_web/web_examples/streams/src/api.rs
@@ -0,0 +1,95 @@
+use std::future::Future;
+
+use anyhow::anyhow;
+use async_stream::stream;
+use futures::{channel::oneshot, select, FutureExt, Stream};
+use gloo_timers::future::TimeoutFuture;
+use rand::Rng;
+
+#[derive(Debug)]
+pub(crate) enum StreamMessage {
+    Started(AbortHandle),
+    SearchResult(anyhow::Result<Option<String>>),
+    Aborted,
+    TimedOut,
+    Finished,
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+pub(crate) struct MockConnection;
+
+impl MockConnection {
+    pub(crate) fn search(&self, search_term: String) -> impl Stream<Item = StreamMessage> {
+        let (shutdown_signal, abort_handle) = ShutdownSignal::new();
+        let mut shutdown = shutdown_signal.into_future().fuse();
+        stream! {
+            yield StreamMessage::Started(abort_handle);
+            loop {
+                let mut timeout = TimeoutFuture::new(3_000).fuse();
+                let mut search_result = Box::pin(simulated_search(&search_term).fuse());
+                select! {
+                    () = shutdown => {
+                        yield StreamMessage::Aborted;
+                        break;
+                    }
+                    () = timeout => {
+                        yield StreamMessage::TimedOut;
+                        return;
+                    }
+                    result = search_result => {
+                        let search_more = match &result {
+                           Ok(Some(_)) => true,
+                           Ok(None) | Err(_) => false
+                        };
+                        yield StreamMessage::SearchResult(result);
+                        if !search_more {
+                           break;
+                        }
+                    }
+                    complete => panic!("stream completed unexpectedly"),
+                }
+            }
+            yield StreamMessage::Finished;
+        }
+    }
+}
+
+async fn simulated_search(search_term: &str) -> anyhow::Result<Option<String>> {
+    let mut rng = rand::thread_rng();
+
+    let delay_ms = rng.gen_range(5..=600);
+    TimeoutFuture::new(delay_ms).fuse().await;
+    let random_value: u8 = rng.gen_range(0..=100);
+
+    match random_value {
+        0..=10 => Err(anyhow!("Simulated error")),
+        11..=50 => Ok(None), // Nothing found
+        _ => Ok(Some(format!("Result for '{search_term}'"))),
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct AbortHandle {
+    abort_tx: oneshot::Sender<()>,
+}
+
+impl AbortHandle {
+    pub(crate) fn abort(self) {
+        let _ = self.abort_tx.send(());
+    }
+}
+
+struct ShutdownSignal {
+    shutdown_rx: oneshot::Receiver<()>,
+}
+
+impl ShutdownSignal {
+    fn new() -> (Self, AbortHandle) {
+        let (abort_tx, shutdown_rx) = oneshot::channel();
+        (Self { shutdown_rx }, AbortHandle { abort_tx })
+    }
+
+    fn into_future(self) -> impl Future<Output = ()> {
+        self.shutdown_rx.map(|_| ())
+    }
+}
---
+++ xilem_web/web_examples/streams/src/main.rs
@@ -0,0 +1,110 @@
+// Copyright 2024 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! This example demonstrates the use of [`memoized_stream`].
+
+use std::pin::Pin;
+
+use futures::{stream, Stream};
+use xilem_web::{
+    concurrent::memoized_stream, core::fork, document_body, elements::html,
+    input_event_target_value, interfaces::Element, App,
+};
+
+mod api;
+use self::api::{AbortHandle, MockConnection, StreamMessage};
+
+#[derive(Default)]
+struct AppState {
+    search_term: String,
+    db: MockConnection,
+    current_search: Option<AbortHandle>,
+}
+
+const DEBOUNCE_MILLIS: usize = 500;
+
+fn app_logic(state: &mut AppState) -> impl Element<AppState> {
+    log::debug!("Run app logic");
+
+    let search_stream = memoized_stream(
+        (state.search_term.clone(), state.db.clone()),
+        create_search_stream,
+        handle_stream_message,
+    )
+    .debounce_ms(DEBOUNCE_MILLIS)
+    .reset_debounce_on_update(true);
+
+    html::div(fork(
+        html::div((
+            html::input(())
+                .on_keyup(on_search_input_keyup)
+                .attr("value", state.search_term.clone())
+                .attr("placeholder", "Type to search"),
+            html::p((
+                "Search is running: ",
+                if state.current_search.is_some() {
+                    "yes"
+                } else {
+                    "no"
+                },
+            )),
+        )),
+        search_stream,
+    ))
+}
+
+fn on_search_input_keyup(state: &mut AppState, ev: web_sys::KeyboardEvent) {
+    if ev.key() == "Escape" {
+        state.search_term.clear();
+        return;
+    }
+    if let Some(abort_handle) = state.current_search.take() {
+        abort_handle.abort();
+    };
+    state.search_term = input_event_target_value(&ev).expect("input value");
+}
+
+fn create_search_stream(
+    (term, conn): &(String, MockConnection),
+) -> Pin<Box<dyn Stream<Item = StreamMessage>>> {
+    if term.is_empty() {
+        Box::pin(stream::empty())
+    } else {
+        Box::pin(conn.search(term.clone()))
+    }
+}
+
+fn handle_stream_message(state: &mut AppState, message: StreamMessage) {
+    match message {
+        StreamMessage::Started(abort_handle) => {
+            log::debug!("Search stream started");
+            debug_assert!(
+                state.current_search.is_none(),
+                "The previous search should already have been canceled"
+            );
+            state.current_search = Some(abort_handle);
+        }
+        StreamMessage::SearchResult(result) => {
+            log::debug!("Stream result {result:?}");
+        }
+        StreamMessage::Aborted => {
+            log::debug!("Stream aborted");
+            state.current_search = None;
+        }
+        StreamMessage::TimedOut => {
+            log::debug!("Stream timed out");
+            state.current_search = None;
+        }
+        StreamMessage::Finished => {
+            log::debug!("Stream finished");
+            state.current_search = None;
+        }
+    }
+}
+
+fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    log::info!("Start application");
+    App::new(document_body(), AppState::default(), app_logic).run();
+}
