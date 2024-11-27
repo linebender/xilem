321df716e53697db7a88c357a916f9b1e79bad4f
Kirpal Grewal <kirpal@outlook.com>
2024-11-27 17:36:25 +0000

    update lib.rs

--- tree_arena/src/lib.rs
+++ tree_arena/src/lib.rs
@@ -1,16 +1,9 @@
 // Copyright 2024 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//! This module will eventually be factored out into a separate crate.
-//!
-//! In the meantime, we intentionally don't make the types in this module part of
-//! our public API, but still implement methods that a standalone crate would have.
-//!
-//! The types defined in this module don't *actually* implement an arena. They use
-//! 100% safe code, which has a significant performance overhead. The final version
-//! will use an arena and unsafe code, but should have the exact same exported API as
-//! this module.
-
+//! This crate implements a tree data structure for use in Masonry
+//! It contains both a safe implementation (that is used by default)
+//! and an unsafe implementation that can be used to improve performance
 type NodeId = u64;
 
 #[cfg(not(feature = "safe_tree"))]
