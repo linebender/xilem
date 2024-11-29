6a5abfcdc1cec86fa61efd15b2536f5e525deeaa
Thomas Churchman <thomas@kepow.org>
2024-11-29 14:43:13 +0100

    Import consistency

--- masonry/src/passes/update.rs
+++ masonry/src/passes/update.rs
@@ -6,7 +6,7 @@ use std::collections::HashSet;
 use cursor_icon::CursorIcon;
 use tracing::{info_span, trace};
 
-use crate::passes::event::run_on_pointer_event_pass;
+use crate::passes::event::{run_on_pointer_event_pass, run_on_text_event_pass};
 use crate::passes::{enter_span, enter_span_if, merge_state_up, recurse_on_children};
 use crate::render_root::{RenderRoot, RenderRootSignal, RenderRootState};
 use crate::tree_arena::ArenaMut;
@@ -14,8 +14,6 @@ use crate::{
     PointerEvent, QueryCtx, RegisterCtx, TextEvent, Update, UpdateCtx, Widget, WidgetId,
     WidgetState,
 };
-
-use super::event::run_on_text_event_pass;
 
 // --- MARK: HELPERS ---
 fn get_id_path(root: &RenderRoot, widget_id: Option<WidgetId>) -> Vec<WidgetId> {
