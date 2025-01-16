d82ff4fd4afc795a601c8399d4a817ec67dd75e0
Olivier FAURE <couteaubleu@gmail.com>
2025-01-16 15:39:17 +0100

    Fix broken doc link

--- masonry/src/contexts.rs
+++ masonry/src/contexts.rs
@@ -807,10 +807,10 @@ impl_context_method!(
         /// The focused widget is the one that receives keyboard events.
         ///
         /// Returns `true` if this specific widget is focused.
-        /// To check if any descendants are focused use [`has_focused`].
+        /// To check if any descendants are focused use [`has_focus_target`].
         ///
         /// [text focus]: crate::doc::doc_06_masonry_concepts#text-focus
-        /// [`has_focused`]: Self::has_focused
+        /// [`has_focus_target`]: Self::has_focus_target
         pub fn is_focus_target(&self) -> bool {
             self.global_state.focused_widget == Some(self.widget_id())
         }
