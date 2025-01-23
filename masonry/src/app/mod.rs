92b44586271dec7736f617f77a0061bcb894757b
Olivier FAURE <couteaubleu@gmail.com>
2025-01-23 15:24:36 +0100

    Remove some cruft

--- masonry/src/app/mod.rs
+++ masonry/src/app/mod.rs
@@ -17,12 +17,3 @@ pub use render_root::{RenderRoot, RenderRootOptions, RenderRootSignal, WindowSiz
 
 pub(crate) use render_root::{MutateCallback, RenderRootState};
 pub(crate) use tracing_backend::{try_init_test_tracing, try_init_tracing};
-
-/*
-(event::|widgets::|)
-
-(AllowRawMut|FromDynWidget|Widget|WidgetRef|WidgetMut|WidgetId|WidgetPod|WidgetState|WidgetArena|CreateWidget|MutateCtx|QueryCtx|EventCtx|RegisterCtx|UpdateCtx|LayoutCtx|ComposeCtx|PaintCtx|AccessCtx|RawWrapper|RawWrapperMut|WindowEvent|PointerButton|PointerEvent|TextEvent|WindowTheme|Update|ObjectFit|Action|BoxConstraints|AccessEvent|Update)
-
-
-(AppDriver|DriverCtx|run|run_with|EventLoop|EventLoopBuilder|EventLoopProxy|MasonryState|MasonryUserEvent|RenderRoot|RenderRootOptions|RenderRootSignal|WindowSizePolicy|MutateCallback|RenderRootState|try_init_test_tracing|try_init_tracing)
-*/
