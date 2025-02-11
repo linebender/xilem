b50873e931e7b86814bc5404acdb4797a9206c9c
eugenesvk <eugenesvk@users.noreply.github.com>
2025-02-11 18:56:40 +0700

    xilem: simplify position names

--- xilem/src/view/button9.rs
+++ xilem/src/view/button9.rs
@@ -60,50 +60,50 @@ pub struct ButtonL9<F> {
 }
 /// Label for ButtonL9
 pub struct Label9 {
-  tl1:Label, ti2:Label, tj3:Label, // ↖  ↑  ↗
-  hl4:Label, hi5:Label, hj6:Label, // ←  •  →
-  ll7:Label, li8:Label, lj9:Label, // ↙  ↓  ↘
+  p1:Label, p2:Label, p3:Label, // ↖  ↑  ↗
+  p4:Label, p5:Label, p6:Label, // ←  •  →
+  p7:Label, p8:Label, p9:Label, // ↙  ↓  ↘
 }
 
 impl<F> ButtonL9<F>{
-  /// Create a new button with a text label at the center (hi5m other labels are blank, use `.addx` methods to fill them)
+  /// Create a new button with a text label at the center (p5m other labels are blank, use `.addx` methods to fill them)
   pub fn new(                     label:impl Into<Label>, pad:Option<Insets>, callback:F) -> Self {
     let label = Label9 {
-      tl1:"".into(), ti2:""   .into(), tj3:"".into(), // ↖  ↑  ↗
-      hl4:"".into(), hi5:label.into(), hj6:"".into(), // ←  •  →
-      ll7:"".into(), li8:""   .into(), lj9:"".into(), // ↙  ↓  ↘
+      p1:"".into(), p2:""   .into(), p3:"".into(), // ↖  ↑  ↗
+      p4:"".into(), p5:label.into(), p6:"".into(), // ←  •  →
+      p7:"".into(), p8:""   .into(), p9:"".into(), // ↙  ↓  ↘
     };
     let pad = Pad9 {
-      tl1:None, ti2:None, tj3:None, // ↖  ↑  ↗
-      hl4:None, hi5:pad , hj6:None, // ←  •  →
-      ll7:None, li8:None, lj9:None, // ↙  ↓  ↘
+      p1:None, p2:None, p3:None, // ↖  ↑  ↗
+      p4:None, p5:pad , p6:None, // ←  •  →
+      p7:None, p8:None, p9:None, // ↙  ↓  ↘
     };
     let opt = LabelOpt{pad};
     Self {label, opt, callback}
   }
-  /// Helper .methods for adding individual labels (add=center hi5)
-  pub fn add (mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.hi5 = label.into(); self.opt.pad.hi5 = pad; self}
-  pub fn add1(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.tl1 = label.into(); self.opt.pad.tl1 = pad; self}
-  pub fn add2(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.ti2 = label.into(); self.opt.pad.ti2 = pad; self}
-  pub fn add3(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.tj3 = label.into(); self.opt.pad.tj3 = pad; self}
-  pub fn add4(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.hl4 = label.into(); self.opt.pad.hl4 = pad; self}
-  pub fn add5(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.hi5 = label.into(); self.opt.pad.hi5 = pad; self}
-  pub fn add6(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.hj6 = label.into(); self.opt.pad.hj6 = pad; self}
-  pub fn add7(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.ll7 = label.into(); self.opt.pad.ll7 = pad; self}
-  pub fn add8(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.li8 = label.into(); self.opt.pad.li8 = pad; self}
-  pub fn add9(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.lj9 = label.into(); self.opt.pad.lj9 = pad; self}
-  // pub fn add (mut self,         label:impl Into<Label>, pad:Option<Insets>) {self.addx(LPos::hi5,label,pad)}
+  /// Helper .methods for adding individual labels (add=center p5)
+  pub fn add (mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p5 = label.into(); self.opt.pad.p5 = pad; self}
+  pub fn add1(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p1 = label.into(); self.opt.pad.p1 = pad; self}
+  pub fn add2(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p2 = label.into(); self.opt.pad.p2 = pad; self}
+  pub fn add3(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p3 = label.into(); self.opt.pad.p3 = pad; self}
+  pub fn add4(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p4 = label.into(); self.opt.pad.p4 = pad; self}
+  pub fn add5(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p5 = label.into(); self.opt.pad.p5 = pad; self}
+  pub fn add6(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p6 = label.into(); self.opt.pad.p6 = pad; self}
+  pub fn add7(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p7 = label.into(); self.opt.pad.p7 = pad; self}
+  pub fn add8(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p8 = label.into(); self.opt.pad.p8 = pad; self}
+  pub fn add9(mut self,         label:impl Into<Label>, pad:Option<Insets>) -> Self {self.label.p9 = label.into(); self.opt.pad.p9 = pad; self}
+  // pub fn add (mut self,         label:impl Into<Label>, pad:Option<Insets>) {self.addx(LPos::p5,label,pad)}
   /// Helper .method for adding a label to a given position (same as in [`LPos`])
   pub fn addx(mut self,idx:LPos,label:impl Into<Label>, pad:Option<Insets>) -> Self {match idx {
-    LPos::tl1 => {self.label.tl1 = label.into(); self.opt.pad.tl1 = pad}, //↖
-    LPos::ti2 => {self.label.ti2 = label.into(); self.opt.pad.ti2 = pad}, //↑
-    LPos::tj3 => {self.label.tj3 = label.into(); self.opt.pad.tj3 = pad}, //↗
-    LPos::hl4 => {self.label.hl4 = label.into(); self.opt.pad.hl4 = pad}, //←
-    LPos::hi5 => {self.label.hi5 = label.into(); self.opt.pad.hi5 = pad}, //•
-    LPos::hj6 => {self.label.hj6 = label.into(); self.opt.pad.hj6 = pad}, //→
-    LPos::ll7 => {self.label.ll7 = label.into(); self.opt.pad.ll7 = pad}, //↙
-    LPos::li8 => {self.label.li8 = label.into(); self.opt.pad.li8 = pad}, //↓
-    LPos::lj9 => {self.label.lj9 = label.into(); self.opt.pad.lj9 = pad}, //↘
+    LPos::p1 => {self.label.p1 = label.into(); self.opt.pad.p1 = pad}, //↖
+    LPos::p2 => {self.label.p2 = label.into(); self.opt.pad.p2 = pad}, //↑
+    LPos::p3 => {self.label.p3 = label.into(); self.opt.pad.p3 = pad}, //↗
+    LPos::p4 => {self.label.p4 = label.into(); self.opt.pad.p4 = pad}, //←
+    LPos::p5 => {self.label.p5 = label.into(); self.opt.pad.p5 = pad}, //•
+    LPos::p6 => {self.label.p6 = label.into(); self.opt.pad.p6 = pad}, //→
+    LPos::p7 => {self.label.p7 = label.into(); self.opt.pad.p7 = pad}, //↙
+    LPos::p8 => {self.label.p8 = label.into(); self.opt.pad.p8 = pad}, //↓
+    LPos::p9 => {self.label.p9 = label.into(); self.opt.pad.p9 = pad}, //↘
   } self }
 }
 
@@ -129,15 +129,15 @@ where F: Fn(&mut State, PointerB) -> MsgRes<Action> + Send + Sync + 'static {
 
   fn build(&self, ctx:&mut ViewCtx) -> (Self::Element, Self::ViewState) {
     // build based on LabelViews, which already implement build themselves:(Self::Element, Self::ViewState)
-    let (child1,()) = ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::build(&self.label.tl1,ctx)});
-    let (child2,()) = ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::build(&self.label.ti2,ctx)});
-    let (child3,()) = ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::build(&self.label.tj3,ctx)});
-    let (child4,()) = ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::build(&self.label.hl4,ctx)});
-    let (child5,()) = ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::build(&self.label.hi5,ctx)});
-    let (child6,()) = ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::build(&self.label.hj6,ctx)});
-    let (child7,()) = ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::build(&self.label.ll7,ctx)});
-    let (child8,()) = ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::build(&self.label.li8,ctx)});
-    let (child9,()) = ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::build(&self.label.lj9,ctx)});
+    let (child1,()) = ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::build(&self.label.p1,ctx)});
+    let (child2,()) = ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::build(&self.label.p2,ctx)});
+    let (child3,()) = ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::build(&self.label.p3,ctx)});
+    let (child4,()) = ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::build(&self.label.p4,ctx)});
+    let (child5,()) = ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::build(&self.label.p5,ctx)});
+    let (child6,()) = ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::build(&self.label.p6,ctx)});
+    let (child7,()) = ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::build(&self.label.p7,ctx)});
+    let (child8,()) = ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::build(&self.label.p8,ctx)});
+    let (child9,()) = ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::build(&self.label.p9,ctx)});
     // pass built elements to the masonry widgets
     let label = [
       into_widget_pod(child1), into_widget_pod(child2), into_widget_pod(child3), // ↖  ↑  ↗
@@ -148,62 +148,62 @@ where F: Fn(&mut State, PointerB) -> MsgRes<Action> + Send + Sync + 'static {
       // child7.into_widget_pod(), child6.into_widget_pod(), child9.into_widget_pod(), // ↙  ↓  ↘
     ];
     let pad = Pad9 {
-      tl1:self.opt.pad.tl1.clone(), ti2:self.opt.pad.ti2.clone(), tj3:self.opt.pad.tj3.clone(), // ↖  ↑  ↗
-      hl4:self.opt.pad.hl4.clone(), hi5:self.opt.pad.hi5.clone(), hj6:self.opt.pad.hj6.clone(), // ←  •  →
-      ll7:self.opt.pad.ll7.clone(), li8:self.opt.pad.li8.clone(), lj9:self.opt.pad.lj9.clone(), // ↙  ↓  ↘
+      p1:self.opt.pad.p1.clone(), p2:self.opt.pad.p2.clone(), p3:self.opt.pad.p3.clone(), // ↖  ↑  ↗
+      p4:self.opt.pad.p4.clone(), p5:self.opt.pad.p5.clone(), p6:self.opt.pad.p6.clone(), // ←  •  →
+      p7:self.opt.pad.p7.clone(), p8:self.opt.pad.p8.clone(), p9:self.opt.pad.p9.clone(), // ↙  ↓  ↘
     };
     ctx.with_leaf_action_widget(|ctx| {ctx.new_pod(widget9::ButtonL9::from_label_pod(label,pad)) } )
   }
 
   fn rebuild(&self, prev:&Self, state:&mut Self::ViewState, ctx:&mut ViewCtx, mut el:Mut<Self::Element>) {
     // rebuild based on LabelViews, which already implement rebuild themselves (compare all the props)
-    ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::rebuild(&self.label.tl1,&prev.label.tl1,state,ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::rebuild(&self.label.ti2,&prev.label.ti2,state,ctx, widget9::ButtonL9::label2_mut(&mut el));});
-    ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::rebuild(&self.label.tj3,&prev.label.tj3,state,ctx, widget9::ButtonL9::label3_mut(&mut el));});
-    ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::rebuild(&self.label.hl4,&prev.label.hl4,state,ctx, widget9::ButtonL9::label4_mut(&mut el));});
-    ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::rebuild(&self.label.hi5,&prev.label.hi5,state,ctx, widget9::ButtonL9::label5_mut(&mut el));});
-    ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::rebuild(&self.label.hj6,&prev.label.hj6,state,ctx, widget9::ButtonL9::label6_mut(&mut el));});
-    ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::rebuild(&self.label.ll7,&prev.label.ll7,state,ctx, widget9::ButtonL9::label7_mut(&mut el));});
-    ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::rebuild(&self.label.li8,&prev.label.li8,state,ctx, widget9::ButtonL9::label8_mut(&mut el));});
-    ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::rebuild(&self.label.lj9,&prev.label.lj9,state,ctx, widget9::ButtonL9::label9_mut(&mut el));});
+    ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::rebuild(&self.label.p1,&prev.label.p1,state,ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::rebuild(&self.label.p2,&prev.label.p2,state,ctx, widget9::ButtonL9::label2_mut(&mut el));});
+    ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::rebuild(&self.label.p3,&prev.label.p3,state,ctx, widget9::ButtonL9::label3_mut(&mut el));});
+    ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::rebuild(&self.label.p4,&prev.label.p4,state,ctx, widget9::ButtonL9::label4_mut(&mut el));});
+    ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::rebuild(&self.label.p5,&prev.label.p5,state,ctx, widget9::ButtonL9::label5_mut(&mut el));});
+    ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::rebuild(&self.label.p6,&prev.label.p6,state,ctx, widget9::ButtonL9::label6_mut(&mut el));});
+    ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::rebuild(&self.label.p7,&prev.label.p7,state,ctx, widget9::ButtonL9::label7_mut(&mut el));});
+    ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::rebuild(&self.label.p8,&prev.label.p8,state,ctx, widget9::ButtonL9::label8_mut(&mut el));});
+    ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::rebuild(&self.label.p9,&prev.label.p9,state,ctx, widget9::ButtonL9::label9_mut(&mut el));});
 
     // rebuild based on LabelOpt, do manuall diff for each prop
-    if prev.opt.pad.tl1 != self.opt.pad.tl1 {widget9::ButtonL9::set_pad1 (&mut el, self.opt.pad.tl1);}
-    if prev.opt.pad.ti2 != self.opt.pad.ti2 {widget9::ButtonL9::set_pad2 (&mut el, self.opt.pad.ti2);}
-    if prev.opt.pad.tj3 != self.opt.pad.tj3 {widget9::ButtonL9::set_pad3 (&mut el, self.opt.pad.tj3);}
-    if prev.opt.pad.hl4 != self.opt.pad.hl4 {widget9::ButtonL9::set_pad4 (&mut el, self.opt.pad.hl4);}
-    if prev.opt.pad.hi5 != self.opt.pad.hi5 {widget9::ButtonL9::set_pad5 (&mut el, self.opt.pad.hi5);}
-    if prev.opt.pad.hj6 != self.opt.pad.hj6 {widget9::ButtonL9::set_pad6 (&mut el, self.opt.pad.hj6);}
-    if prev.opt.pad.ll7 != self.opt.pad.ll7 {widget9::ButtonL9::set_pad7 (&mut el, self.opt.pad.ll7);}
-    if prev.opt.pad.li8 != self.opt.pad.li8 {widget9::ButtonL9::set_pad8 (&mut el, self.opt.pad.li8);}
-    if prev.opt.pad.lj9 != self.opt.pad.lj9 {widget9::ButtonL9::set_pad9 (&mut el, self.opt.pad.lj9);}
+    if prev.opt.pad.p1 != self.opt.pad.p1 {widget9::ButtonL9::set_pad1 (&mut el, self.opt.pad.p1);}
+    if prev.opt.pad.p2 != self.opt.pad.p2 {widget9::ButtonL9::set_pad2 (&mut el, self.opt.pad.p2);}
+    if prev.opt.pad.p3 != self.opt.pad.p3 {widget9::ButtonL9::set_pad3 (&mut el, self.opt.pad.p3);}
+    if prev.opt.pad.p4 != self.opt.pad.p4 {widget9::ButtonL9::set_pad4 (&mut el, self.opt.pad.p4);}
+    if prev.opt.pad.p5 != self.opt.pad.p5 {widget9::ButtonL9::set_pad5 (&mut el, self.opt.pad.p5);}
+    if prev.opt.pad.p6 != self.opt.pad.p6 {widget9::ButtonL9::set_pad6 (&mut el, self.opt.pad.p6);}
+    if prev.opt.pad.p7 != self.opt.pad.p7 {widget9::ButtonL9::set_pad7 (&mut el, self.opt.pad.p7);}
+    if prev.opt.pad.p8 != self.opt.pad.p8 {widget9::ButtonL9::set_pad8 (&mut el, self.opt.pad.p8);}
+    if prev.opt.pad.p9 != self.opt.pad.p9 {widget9::ButtonL9::set_pad9 (&mut el, self.opt.pad.p9);}
   }
 
   fn teardown(&self, _:&mut Self::ViewState, ctx:&mut ViewCtx, mut el:Mut<Self::Element>) {
     // teardown LabelViews, which already implement teardown themselves
-    ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::teardown(&self.label.tl1,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::teardown(&self.label.ti2,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::teardown(&self.label.tj3,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::teardown(&self.label.hl4,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::teardown(&self.label.hi5,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::teardown(&self.label.hj6,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::teardown(&self.label.ll7,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::teardown(&self.label.li8,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
-    ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::teardown(&self.label.lj9,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw1, |ctx|{View::<State,Action,_>::teardown(&self.label.p1,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw2, |ctx|{View::<State,Action,_>::teardown(&self.label.p2,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw3, |ctx|{View::<State,Action,_>::teardown(&self.label.p3,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw4, |ctx|{View::<State,Action,_>::teardown(&self.label.p4,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw5, |ctx|{View::<State,Action,_>::teardown(&self.label.p5,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw6, |ctx|{View::<State,Action,_>::teardown(&self.label.p6,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw7, |ctx|{View::<State,Action,_>::teardown(&self.label.p7,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw8, |ctx|{View::<State,Action,_>::teardown(&self.label.p8,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
+    ctx.with_id(id_lvw9, |ctx|{View::<State,Action,_>::teardown(&self.label.p9,&mut (),ctx, widget9::ButtonL9::label1_mut(&mut el));});
     ctx.teardown_leaf(el); // teardown the button element itself
   }
 
   fn message(&self, _:&mut Self::ViewState, id_path:&[ViewId], message:DynMessage, app_state:&mut State) -> MsgRes<Action> {
     match id_path.split_first() {
-      Some((&id_lvw1, rest)) => self.label.tl1.message(&mut(), rest, message, app_state),
-      Some((&id_lvw2, rest)) => self.label.ti2.message(&mut(), rest, message, app_state),
-      Some((&id_lvw3, rest)) => self.label.tj3.message(&mut(), rest, message, app_state),
-      Some((&id_lvw4, rest)) => self.label.hl4.message(&mut(), rest, message, app_state),
-      Some((&id_lvw5, rest)) => self.label.hi5.message(&mut(), rest, message, app_state),
-      Some((&id_lvw6, rest)) => self.label.hj6.message(&mut(), rest, message, app_state),
-      Some((&id_lvw7, rest)) => self.label.ll7.message(&mut(), rest, message, app_state),
-      Some((&id_lvw8, rest)) => self.label.li8.message(&mut(), rest, message, app_state),
-      Some((&id_lvw9, rest)) => self.label.lj9.message(&mut(), rest, message, app_state),
+      Some((&id_lvw1, rest)) => self.label.p1.message(&mut(), rest, message, app_state),
+      Some((&id_lvw2, rest)) => self.label.p2.message(&mut(), rest, message, app_state),
+      Some((&id_lvw3, rest)) => self.label.p3.message(&mut(), rest, message, app_state),
+      Some((&id_lvw4, rest)) => self.label.p4.message(&mut(), rest, message, app_state),
+      Some((&id_lvw5, rest)) => self.label.p5.message(&mut(), rest, message, app_state),
+      Some((&id_lvw6, rest)) => self.label.p6.message(&mut(), rest, message, app_state),
+      Some((&id_lvw7, rest)) => self.label.p7.message(&mut(), rest, message, app_state),
+      Some((&id_lvw8, rest)) => self.label.p8.message(&mut(), rest, message, app_state),
+      Some((&id_lvw9, rest)) => self.label.p9.message(&mut(), rest, message, app_state),
       None => match message.downcast::<masonry::core::Action>() {
         Ok(action)   => {
           if let masonry::core::Action::ButtonPressed(button) = *action {
