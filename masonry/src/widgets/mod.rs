3967c6e5479fc6f3966b51dac60d92edd840193f
Richard Dodd <richard.o.dodd@gmail.com>
2025-02-24 00:59:05 +0000

    add canvas widget

--- masonry/src/widgets/mod.rs
+++ masonry/src/widgets/mod.rs
@@ -11,6 +11,7 @@ mod tests;
 
 mod align;
 mod button;
+mod canvas;
 mod checkbox;
 mod flex;
 mod grid;
@@ -31,6 +32,7 @@ mod zstack;
 
 pub use self::align::Align;
 pub use self::button::Button;
+pub use self::canvas::Canvas;
 pub use self::checkbox::Checkbox;
 pub use self::flex::{Axis, CrossAxisAlignment, Flex, FlexParams, MainAxisAlignment};
 pub use self::grid::{Grid, GridParams};
