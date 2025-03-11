2cc1a9fa3708b89fb7239878d4a8dab9a8936bab
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-03-11 16:57:39 +0000

    Start to simplify-ish the model

--- masonry_core/src/widgets/virtual_scroll.rs
+++ masonry_core/src/widgets/virtual_scroll.rs
@@ -33,24 +33,31 @@ pub struct VirtualScrollAction {
 pub struct VirtualScroll<W: Widget + ?Sized> {
     /// The range of items in the "id" space which are able to be used.
     ///
-    /// This is used to cap scrolling; the items included can never scroll off-screen[^1][^2].
+    /// This is used to cap scrolling; items outside of this range will never be loaded[^1][^2][^3].
     /// For example, in an email program, this would be `[id_of_most_recent_email]..=[id_of_oldest_email]`
     /// (note that the id of the oldest email might not be known; as soon as it is known, `id_of_oldest_email`
     /// can be set).
     ///
     /// The default is `i64::MIN..i64::MAX`
     ///
+    /// Items outside of this range will be stashed.
+    ///
     /// [^1]: The exact interaction with a "drag down to refresh" feature has not been scrutinised.
     /// [^2]: Currently, we lock the bottom of the range to the bottom of the final item. This should be configurable.
+    /// [^3]: Behaviour when the range is shrunk to something containing the active range has not been considered.
     valid_range: Range<i64>,
 
     /// The range in the id space which is "active", i.e. which the virtual scrolling controller has the
     /// ability to lay out. Note that items is not necessarily dense in these; that is, if an
-    /// item has not been provided by the application, we don't fall over.
+    /// item has not been provided by the application, we avoid falling over.
     active_range: Range<i64>,
 
+    /// The range in the id space which the virtual scrolling controller *wants* to layout.
+    /// Items which are in active_range but not in this range will be stashed in the mutate pass.
+    target_range: Range<i64>,
+
     /// All children of the virtual scroller.
-    // TODO: Does this need to be a `BTreeMap`, or maybe a sparse `VecDeque`
+    // TODO: Does this need to be a `BTreeMap`, or maybe a sparse `VecDeque` (for quicker in-order iteration)
     items: HashMap<i64, WidgetPod<W>>,
     // TODO: Handle focus even if the focused item scrolls off-screen.
     // TODO: Maybe this should be the focused items and its two neighbours, so tab focusing works?
@@ -58,12 +65,24 @@ pub struct VirtualScroll<W: Widget + ?Sized> {
 
     // Question: For a given scroll position, should the anchor always be the same?
     // Answer: Let's say yes for now, and re-evaluate if it becomes necessary.
-    //  - Reason to not have this is that it adds some pathologies to an "up/down" movement
+    //  - Reason to not have this is that it adds some potential worst-case performance issues if scrolling up/down
     anchor_index: i64,
-    // TODO: Use a fixed-point scheme, because
+    // TODO: Use a fixed-point scheme, because adding and subtracting amounts could change the scale, theoretically causing visible jumps.
     // This will likely wait for layout to do something similar
     scroll_offset_from_anchor: f64,
 
+    /// The average height of items, determined experimentally.
+    /// This is used if there are no items to determine the mean item height otherwise. This approach means:
+    /// 1) For the easy case where every item is the same height (e.g. email), we get the right answer
+    /// 2) For slightly harder cases, we get as sensible a result as is reasonable, without requiring a complex API
+    ///    to get the needed information.
+    mean_item_height: f64,
+
+    /// The height of the current anchor.
+    /// Used to determine if scrolling will require a relayout (because the anchor has changed).
+    anchor_height: f64,
+
+    /// The available width in the last layout call, used so that layout of children can be skipped if it won't have changed.
     old_width: f64,
 }
 
@@ -88,6 +107,8 @@ impl<W: Widget> Widget for VirtualScroll<W> {
                 height: f64::INFINITY,
             },
         );
+        // The number of loaded items before the anchor
+        let count_before_anchor = 0;
         let mut height_before_anchor = 0.;
         // The height of all loaded items after the anchor.
         // Note that this includes the height of the anchor itself.
@@ -95,11 +116,16 @@ impl<W: Widget> Widget for VirtualScroll<W> {
         let mut count = 0_u64;
         let mut first_item = i64::MAX;
         let mut last_item = i64::MIN;
+
+        // Calculate the sizes of all children
         for (idx, child) in &mut self.items {
             if !self.active_range.contains(idx) {
+                if cfg!(debug_assertions) && ctx.child_needs_layout(child) {
+                    unreachable!("Mutate pass didn't run before layout, breaking assumptions.")
+                }
                 // This item is stashed, and so can't be used.
                 // (If we decide we want to use it, we'll do so by re-enabling
-                // it in a following mutate pass). Note that we might still access its height;
+                // it in a following mutate pass). Note that we might still access to its height;
                 // that might not be *right*, but it gives an estimate anyway
                 ctx.skip_layout(child);
                 continue;
@@ -112,6 +138,7 @@ impl<W: Widget> Widget for VirtualScroll<W> {
                 ctx.child_size(child)
             };
             if *idx < self.anchor_index {
+                count_before_anchor += 1;
                 height_before_anchor += resulting_size.height;
             } else {
                 height_after_anchor += resulting_size.height;
@@ -119,69 +146,89 @@ impl<W: Widget> Widget for VirtualScroll<W> {
             count += 1;
         }
 
-        let mean_item_size = (height_before_anchor + height_after_anchor) / count as f64;
-        let mut additions = SmallVec::new();
-        let mut removals = SmallVec::new();
+        let mean_item_height = if count > 0 {
+            (height_before_anchor + height_after_anchor) / count as f64
+        } else {
+            self.mean_item_height
+        };
+        self.mean_item_height = mean_item_height;
+
+        let items_after_anchor: u64 = (self.active_range.end - self.anchor_index)
+            .try_into()
+            .unwrap();
+        let height_after_anchor =
+        // count_before_anchor < count as only conditionally incremented, when count is unconditionally incremented
+            height_after_anchor + (items_after_anchor - count - count_before_anchor) as f64 * mean_item_height;
+        let height_before_anchor =
+            height_after_anchor + count_before_anchor as f64 * mean_item_height;
         // If we've significantly out-ran the virtual scrolling (as a loose heuristic), calculate a new anchor using a heuristic
         // and drop all currently loaded items.
-        if (self.scroll_offset_from_anchor
-            > height_after_anchor + 3. * viewport_size.height + 3000.)
-            || (self.scroll_offset_from_anchor
-                < -height_before_anchor - 3. * viewport_size.height - 3000.)
+        // if (self.scroll_offset_from_anchor
+        //     > height_after_anchor + 3. * viewport_size.height + 3000.)
+        //     || (self.scroll_offset_from_anchor
+        //         < -height_before_anchor - 3. * viewport_size.height - 3000.)
+        // {
+        // let items = core::mem::take(&mut self.current_items);
+        // // TODO: We *probably* want to slide down the items.
+        // for (idx, mut item) in items {
+        //     ctx.skip_layout(&mut item);
+        //     self.items_pending_removal.insert(idx, item);
+        //     removals.push(idx);
+        // }
+        // if mean_item_size.is_finite() {
+        //     let diff_count = self.scroll_offset_from_anchor / mean_item_size;
+        //     let diff_count = diff_count.floor();
+        //     self.anchor_index =
+        //         (self.anchor_index + diff_count as i64).clamp(self.first_item, self.last_item);
+        //     self.scroll_offset_from_anchor -= diff_count * mean_item_size;
+        // } else {
+        //     debug_assert_eq!(
+        //         count, 0,
+        //         "Assumption: If the division produced an infinite result, it's because of a divide by zero."
+        //     );
+        //     // TODO: How can we handle this sanely? We're scrolled very far down (3 pages + 3000 logical pixels),
+        //     // but we don't have any idea how big the items are; that could be within a single item.
+        //     // I guess we need to just wait for the anchor to be loaded?
+        // }
+        // } else
+
+        // Determine the new anchor
         {
-            let items = core::mem::take(&mut self.current_items);
-            // TODO: We *probably* want to slide down the items.
-            for (idx, mut item) in items {
-                ctx.skip_layout(&mut item);
-                self.items_pending_removal.insert(idx, item);
-                removals.push(idx);
-            }
-            if mean_item_size.is_finite() {
-                let diff_count = self.scroll_offset_from_anchor / mean_item_size;
-                let diff_count = diff_count.floor();
-                self.anchor_index =
-                    (self.anchor_index + diff_count as i64).clamp(self.first_item, self.last_item);
-                self.scroll_offset_from_anchor -= diff_count * mean_item_size;
-            } else {
-                debug_assert_eq!(
-                    count, 0,
-                    "Assumption: If the division produced an infinite result, it's because of a divide by zero."
-                );
-                // TODO: How can we handle this sanely? We're scrolled very far down (3 pages + 3000 logical pixels),
-                // but we don't have any idea how big the items are; that could be within a single item.
-                // I guess we need to just wait for the anchor to be loaded?
-            }
-        } else {
-            let previous_anchor = self.anchor_index;
+            // let previous_anchor = self.anchor_index;
             loop {
                 if self.scroll_offset_from_anchor < 0. {
-                    if self.anchor_index == self.first_item {
+                    if self.anchor_index <= self.active_range.start {
+                        // TODO: Is this the right time to do this clamping?
+                        self.anchor_index = self.active_range.start;
+                        // Don't scroll above the old anchor
                         self.scroll_offset_from_anchor = 0.;
                         break;
                     }
-                    let new_idx = self.anchor_index - 1;
-                    let new_anchor = self.current_items.get(&new_idx);
+                    self.anchor_index = self.anchor_index - 1;
+                    let new_anchor = self.items.get(&self.anchor_index);
                     let new_anchor_height = if let Some(new_anchor) = new_anchor {
+                        // Note: This child might be stashed, but our best estimate of its height is its old height?
+                        // This would only be the case if actions haven't been re-ran, I think?
                         ctx.child_size(new_anchor).height
                     } else {
-                        // We need to request this new anchor to be loaded
-                        break;
+                        mean_item_height
                     };
                     self.scroll_offset_from_anchor += new_anchor_height;
-                    height_before_anchor -= new_anchor_height;
-                    height_after_anchor += new_anchor_height;
-                    self.anchor_index = new_idx;
                 } else {
-                    let anchor_pod = self.current_items.get(&self.anchor_index);
-                    let anchor_height = if let Some(anchor_pod) = anchor_pod {
+                    let current_anchor = self.items.get(&self.anchor_index);
+                    let anchor_height = if let Some(anchor_pod) = current_anchor {
                         ctx.child_size(anchor_pod).height
                     } else {
-                        // We need to request this new anchor to be loaded
-                        break;
+                        mean_item_height
                     };
                     if self.scroll_offset_from_anchor > anchor_height {
-                        if self.anchor_index != self.last_item {
-                            self.scroll_offset_from_anchor = anchor_height;
+                        if self.anchor_index >= self.active_range.end {
+                            // TODO: Is this the right time to do this clamping?
+                            self.anchor_index = self.active_range.end;
+                            self.scroll_offset_from_anchor = self
+                                .scroll_offset_from_anchor
+                                // Lock scrolling to be at most a page below the last item
+                                .max(anchor_height + viewport_size.height);
                             break;
                         }
                         self.scroll_offset_from_anchor -= anchor_height;
