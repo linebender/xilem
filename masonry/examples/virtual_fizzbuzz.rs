b3c6591e54e07cae82f0e302f7722479615901b9
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-03-14 18:17:51 +0000

    Rename example and start to document

--- masonry/examples/virtual_fizzbuzz.rs
+++ masonry/examples/virtual_fizzbuzz.rs
@@ -1,7 +1,10 @@
 // Copyright 2024 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//! Shows how to use a grid layout in Masonry.
+//! A demonstration of the [`VirtualScroll`] widget, producing an infinite[^1] FizzBuzz.
+//!
+//! [^1]: Limited to `i64::MIN..i64::MAX-1`; that is, there are `2^64-1` possible items.
+//! However, there is (currently...) no way to jump to a specific item, so it's impossible to reach the end.
 
 // On Windows platform, don't show a console when opening the app.
 #![windows_subsystem = "windows"]
@@ -12,6 +15,18 @@ use masonry::dpi::LogicalSize;
 use masonry::widgets::{Label, RootWidget, VirtualScroll, VirtualScrollAction};
 
 use winit::window::Window;
+
+/// The widget kind contained in the scroll area. This is currently a generic parameter of [`VirtualScroll`], although
+/// note that [`dyn Widget`](masonry::core::Widget) can also be used for dynamic children kinds.
+///
+/// We use a type alias for this, as any downcasting needs to know the type of W.
+type ScrollContents = Label;
+
+/// Function to create the virtual scroll area.
+fn init() -> VirtualScroll<ScrollContents> {
+    // We start our fizzbuzzing with the top of the screen at item 0
+    VirtualScroll::new(0)
+}
 
 struct Driver {
     scroll_id: WidgetId,
@@ -24,12 +39,16 @@ impl AppDriver for Driver {
     fn on_action(&mut self, ctx: &mut DriverCtx<'_>, widget_id: WidgetId, action: Action) {
         if widget_id == self.scroll_id {
             if let Action::Other(action) = action {
+                // The VirtualScroll widget will send us a VirtualScrollAction every time it wants different
+                // items to be loaded or unloaded.
                 let action = action.downcast::<VirtualScrollAction>().unwrap();
                 ctx.render_root().edit_root_widget(|mut root| {
                     let mut root = root.downcast::<RootWidget<VirtualScroll<ScrollContents>>>();
                     let mut scroll = RootWidget::child_mut(&mut root);
                     for idx in action.old_active.clone() {
                         if !action.target.contains(&idx) {
+                            // If we had different work to do in response to the item being unloaded
+                            // (for example, saving some related data?), then we'd do it here
                             VirtualScroll::remove_child(&mut scroll, idx);
                         }
                     }
@@ -37,7 +56,7 @@ impl AppDriver for Driver {
                         if !action.old_active.contains(&idx) {
                             let evil = false;
                             if evil {
-                                // Pathological implementation: we should handle this well (although we warn)
+                                // Adversarial implementation: we should handle this well (although we warn)
                                 if idx % 10 == 0 {
                                     VirtualScroll::add_child(
                                         &mut scroll,
@@ -82,14 +101,8 @@ impl AppDriver for Driver {
     }
 }
 
-type ScrollContents = Label;
-
-fn make_scroll() -> VirtualScroll<ScrollContents> {
-    VirtualScroll::new(0)
-}
-
 fn main() {
-    let main_widget = WidgetPod::new(make_scroll());
+    let main_widget = WidgetPod::new(init());
     let driver = Driver {
         scroll_id: main_widget.id(),
         fizz: "Fizz".into(),
@@ -98,7 +111,7 @@ fn main() {
     };
     let window_size = LogicalSize::new(800.0, 500.0);
     let window_attributes = Window::default_attributes()
-        .with_title("Grid Layout")
+        .with_title("Infinite FizzBuzz")
         .with_resizable(true)
         .with_min_inner_size(window_size);
 
