f4ae5497ee1e7f143803294d6bed266222decafb
Olivier FAURE <couteaubleu@gmail.com>
2025-03-23 16:40:55 +0100

    Split most of the crate into masonry_core sub-crate.
    This change is done very carefully to minimize the number of changed files, and use almost only file renames.
    This is both for avoiding merge conflicts, keeping file history unbroken, and keeping the PR easy to review.
    
    A future PR will rename masonry to masonry_core, masonry_winit to masonry change all the doc tests and snapshots that depend on those names, etc.
    
    After *that*, future PRs will have actual code changes splitting winit away from masonry_core.

---
+++ masonry_core/src/lib.rs
@@ -0,0 +1,67 @@
+// Copyright 2018 the Xilem Authors and the Druid Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Traits and types of the Masonry toolkit.
+//! See Masonry documentation for more details, examples and resources.
+
+// LINEBENDER LINT SET - lib.rs - v3
+// See https://linebender.org/wiki/canonical-lints/
+// These lints shouldn't apply to examples or tests.
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+// These lints shouldn't apply to examples.
+#![warn(clippy::print_stdout, clippy::print_stderr)]
+// Targeting e.g. 32-bit means structs containing usize can give false positives for 64-bit.
+#![cfg_attr(target_pointer_width = "64", warn(clippy::trivially_copy_pass_by_ref))]
+// END LINEBENDER LINT SET
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(
+    test,
+    expect(
+        unused_crate_dependencies,
+        reason = "False-positive with dev-dependencies only used in examples"
+    )
+)]
+#![expect(clippy::allow_attributes_without_reason, reason = "Deferred: Noisy")]
+// TODO: Remove any items listed as "Deferred"
+#![expect(clippy::needless_doctest_main, reason = "Deferred: Noisy")]
+#![expect(clippy::should_implement_trait, reason = "Deferred: Noisy")]
+#![cfg_attr(not(debug_assertions), expect(unused, reason = "Deferred: Noisy"))]
+#![expect(missing_debug_implementations, reason = "Deferred: Noisy")]
+#![expect(clippy::cast_possible_truncation, reason = "Deferred: Noisy")]
+#![expect(clippy::missing_assert_message, reason = "Deferred: Noisy")]
+#![expect(elided_lifetimes_in_paths, reason = "Deferred: Noisy")]
+#![expect(unreachable_pub, reason = "Potentially controversial code style")]
+#![expect(
+    unnameable_types,
+    reason = "Requires lint_reasons rustc feature for exceptions"
+)]
+#![expect(clippy::todo, reason = "We have a lot of 'real' todos")]
+#![expect(
+    clippy::shadow_unrelated,
+    reason = "Potentially controversial code style"
+)]
+#![expect(clippy::single_match, reason = "General policy not decided")]
+
+// TODO - Add logo
+
+#[macro_use]
+pub mod util;
+
+#[cfg(doc)]
+pub mod doc;
+
+mod passes;
+
+pub mod app;
+pub mod core;
+pub mod properties;
+pub mod testing;
+pub mod theme;
+pub mod widgets;
+
+pub use vello::peniko::color::palette;
+pub use vello::{kurbo, peniko};
+pub use {cursor_icon, dpi, parley, vello};
+
+// TODO - Move to core?
+pub use util::{AsAny, Handled, UnitPoint};
