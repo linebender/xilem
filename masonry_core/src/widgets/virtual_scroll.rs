a2ae2ff90d38cfb27c85a5feca8887c4ea811388
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-03-27 17:14:24 +0000

    Fixup merge conflicts

--- masonry_core/src/widgets/virtual_scroll.rs
+++ masonry_core/src/widgets/virtual_scroll.rs
@@ -914,19 +914,21 @@ mod tests {
         let virtual_scroll_id = harness.root_widget().id();
         fn driver(action: VirtualScrollAction, mut scroll: WidgetMut<'_, VirtualScroll<Label>>) {
             VirtualScroll::will_handle_action(&mut scroll, &action);
-            for idx in action.old_active {
+            for idx in action.old_active.clone() {
                 if !action.target.contains(&idx) {
                     VirtualScroll::remove_child(&mut scroll, idx);
                 }
             }
             for idx in action.target {
-                VirtualScroll::add_child(
-                    &mut scroll,
-                    idx,
-                    WidgetPod::new(
-                        Label::new(format!("{idx}")).with_style(StyleProperty::FontSize(30.)),
-                    ),
-                );
+                if !action.old_active.contains(&idx) {
+                    VirtualScroll::add_child(
+                        &mut scroll,
+                        idx,
+                        WidgetPod::new(
+                            Label::new(format!("{idx}")).with_style(StyleProperty::FontSize(30.)),
+                        ),
+                    );
+                }
             }
         }
 
