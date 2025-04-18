809874da5fffedc68971eb31cfe8907cf28f2194
eugenesvk <eugenesvk@users.noreply.github.com>
2025-04-18 20:38:39 +0700

    add buttons to dynamically change some label alignment

--- xilem/examples/gallery_label_xilem_view.rs
+++ xilem/examples/gallery_label_xilem_view.rs
@@ -8,6 +8,7 @@
 // add URL support for doc links
 // add non-desktop platforms
 
+use tracing::warn;
 use masonry::dpi::LogicalSize;
 use winit::window::Window;
 use xilem::view::{CrossAxisAlignment, FlexExt, FlexParams};
@@ -17,14 +18,93 @@ use masonry::parley::fontique;
 use masonry::peniko::Color;
 use winit::error::EventLoopError;
 use xilem::view::{
-    flex, grid, label, portal, prose, sized_box, Axis, GridExt, Label, Padding, Prose,
+    button, flex, grid, label, portal, prose, sized_box, Axis, GridExt, Label, Padding, Prose,
 };
 use xilem::{palette::css, EventLoop, FontWeight, LineBreaking, TextAlignment, WidgetView, Xilem};
 
 const LABEL_COLOR: Color = css::ROYAL_BLUE;
 
-#[derive(Default)]
-struct AppState {}
+struct AppState {
+    l1i1:TextAlignment,
+    l1i2:TextAlignment,
+    l1i3:TextAlignment,
+    l1i4:TextAlignment,
+    l2i1:TextAlignment,
+    l2i2:TextAlignment,
+    l2i3:TextAlignment,
+    l2i4:TextAlignment,
+    l3i1:TextAlignment,
+    l3i2:TextAlignment,
+    l3i3:TextAlignment,
+    l3i4:TextAlignment,
+    l4i1:TextAlignment,
+    l4i1x:CrossAxisAlignment,
+    l4i2:TextAlignment,
+    l4i3:TextAlignment,
+    l4i4:TextAlignment,
+}
+impl Default for AppState { fn default() -> AppState {
+    AppState {
+        l1i1: TextAlignment::Start ,
+        l1i2: TextAlignment::Middle,
+        l1i3: TextAlignment::End,
+        l1i4: TextAlignment::Justified,
+        l2i1: TextAlignment::Start ,
+        l2i2: TextAlignment::Middle,
+        l2i3: TextAlignment::End,
+        l2i4: TextAlignment::Justified,
+        l3i1: TextAlignment::Start ,
+        l3i2: TextAlignment::Middle,
+        l3i3: TextAlignment::End,
+        l3i4: TextAlignment::Justified,
+        l4i1: TextAlignment::Start ,
+        l4i1x: CrossAxisAlignment::Start ,
+        l4i2: TextAlignment::Middle,
+        l4i3: TextAlignment::End,
+        l4i4: TextAlignment::Justified,
+  }}
+}
+fn text_align_cycle(cur:&TextAlignment) -> TextAlignment {
+    match cur {
+        TextAlignment::Start     => TextAlignment::Middle,
+        TextAlignment::Middle    => TextAlignment::End,
+        TextAlignment::End       => TextAlignment::Justified,
+        TextAlignment::Justified => TextAlignment::Start,
+    }
+}
+fn text_x_align_cycle(cur:&CrossAxisAlignment) -> CrossAxisAlignment {
+    match cur {
+        CrossAxisAlignment::Start    => CrossAxisAlignment::Center,
+        CrossAxisAlignment::Center   => CrossAxisAlignment::End,
+        CrossAxisAlignment::End      => CrossAxisAlignment::Baseline,
+        CrossAxisAlignment::Baseline => CrossAxisAlignment::Fill,
+        CrossAxisAlignment::Fill     => CrossAxisAlignment::Start,
+    }
+}
+
+impl AppState {
+    fn realign(&mut self, pos:impl AsRef<str> + std::fmt::Debug) {
+        let p = pos.as_ref();
+        if       p=="l1i1" {self.l1i1 = text_align_cycle(&self.l1i1)
+        }else if p=="l1i2" {self.l1i2 = text_align_cycle(&self.l1i2)
+        }else if p=="l1i3" {self.l1i3 = text_align_cycle(&self.l1i3)
+        }else if p=="l1i4" {self.l1i4 = text_align_cycle(&self.l1i4)
+        }else if p=="l2i1" {self.l2i1 = text_align_cycle(&self.l2i1)
+        }else if p=="l2i2" {self.l2i2 = text_align_cycle(&self.l2i2)
+        }else if p=="l2i3" {self.l2i3 = text_align_cycle(&self.l2i3)
+        }else if p=="l2i4" {self.l2i4 = text_align_cycle(&self.l2i4)
+        }else if p=="l3i1" {self.l3i1 = text_align_cycle(&self.l3i1)
+        }else if p=="l3i2" {self.l3i2 = text_align_cycle(&self.l3i2)
+        }else if p=="l3i3" {self.l3i3 = text_align_cycle(&self.l3i3)
+        }else if p=="l3i4" {self.l3i4 = text_align_cycle(&self.l3i4)
+        }else if p=="l4i1" {self.l4i1 = text_align_cycle(&self.l4i1)
+        }else if p=="l4i2" {self.l4i2 = text_align_cycle(&self.l4i2)
+        }else if p=="l4i3" {self.l4i3 = text_align_cycle(&self.l4i3)
+        }else if p=="l4i4" {self.l4i4 = text_align_cycle(&self.l4i4)
+        }else if p=="l4i1x" {self.l4i1x = text_x_align_cycle(&self.l4i1x)
+        }else              {warn!("expecting either of ‘l1–4i1–4’ or ‘l4i1x’, got {:?}",pos)}
+    }
+}
 
 fn title_prose(text: impl Into<ArcStr>) -> Prose {
     prose(text)
@@ -42,7 +122,8 @@ fn lc(text: impl Into<ArcStr>) -> Label {
     //colored label
     label(text).brush(LABEL_COLOR)
 }
-fn app_logic(_d: &mut AppState) -> impl WidgetView<AppState> {
+fn app_logic(d: &mut AppState) -> impl WidgetView<AppState> {
+    // let cb = |D:&mut AppState|{}; // empty callback for empty buttons
     let m_c = Color::from_rgb8(0x11, 0x11, 0x11); //main text
     let l_c = LABEL_COLOR;
     let mut i = 0;
@@ -64,42 +145,67 @@ fn app_logic(_d: &mut AppState) -> impl WidgetView<AppState> {
     {i+=1;},title_prose(format!("§ {i} .alignment")),
     txt_prose("  4 options: ≝Start Middle End Justified\n  https://docs.rs/parley/latest/parley/layout/enum.Alignment.html")
     ),
-    (label("  • flex in a 200×170 sized_box to show the impact of constraints").alignment(TextAlignment::Justified).brush(m_c),
+    (label("  • flex in a 200×170 sized_box to show the impact of constraints (buttons to change alignment)").alignment(TextAlignment::Justified).brush(m_c),
+    flex((
+      button("Δ1/4", |d:&mut AppState|{d.realign("l1i1");}),
+      button("Δ2/4", |d:&mut AppState|{d.realign("l1i2");}),
+      button("Δ3/4", |d:&mut AppState|{d.realign("l1i3");}),
+      button("Δ4/4", |d:&mut AppState|{d.realign("l1i4");}),
+      )).direction(Axis::Horizontal),
     sized_box(
       flex((
-        lc("1/4 alignment Start"     ).alignment(TextAlignment::Start        ),
-        lc("2/4 alignment Middle"    ).alignment(TextAlignment::Middle       ),
-        lc("3/4 alignment End"       ).alignment(TextAlignment::End          ),
-        lc("4/4 alignment Justified" ).alignment(TextAlignment::Justified    ),
+        lc(format!("1/4 alignment {:?}",d.l1i1)).alignment(d.l1i1),
+        lc(format!("2/4 alignment {:?}",d.l1i2)).alignment(d.l1i2),
+        lc(format!("3/4 alignment {:?}",d.l1i3)).alignment(d.l1i3),
+        lc(format!("4/4 alignment {:?}",d.l1i4)).alignment(d.l1i4),
       ))
       ).width(200f64).height(170f64).padding(Padding::from(0.))
        .background(css::LIGHT_GRAY) // .border(css::RED,0.).rounded(RoundedRectRadii::from_single_radius(0.))
     ,),
-    (label("  • grid in a 200×170 sized_box to make labels same-width (one per row in 4×1 table)").alignment(TextAlignment::Justified).brush(m_c),
+    (label("  • grid in a 200×170 sized_box to make labels same-width (one per row in 4×1 table; buttons to change alignment)").alignment(TextAlignment::Justified).brush(m_c),
+    flex((
+      button("Δ1/4", |d:&mut AppState|{d.realign("l2i1");}),
+      button("Δ2/4", |d:&mut AppState|{d.realign("l2i2");}),
+      button("Δ3/4", |d:&mut AppState|{d.realign("l2i3");}),
+      button("Δ4/4", |d:&mut AppState|{d.realign("l2i4");}),
+      )).direction(Axis::Horizontal),
     sized_box(
         grid((
-            lc("1/4 alignment Start"        ).alignment(TextAlignment::Start        ).grid_pos(0,0),
-            lc("2/4 alignment Middle"       ).alignment(TextAlignment::Middle       ).grid_pos(0,1),
-            lc("3/4 alignment End"          ).alignment(TextAlignment::End          ).grid_pos(0,2),
-            lc("4/4 alignment Justified"    ).alignment(TextAlignment::Justified    ).grid_pos(0,3),
+            lc(format!("1/4 alignment {:?}",d.l2i1)).alignment(d.l2i1).grid_pos(0,0),
+            lc(format!("2/4 alignment {:?}",d.l2i2)).alignment(d.l2i2).grid_pos(0,1),
+            lc(format!("3/4 alignment {:?}",d.l2i3)).alignment(d.l2i3).grid_pos(0,2),
+            lc(format!("4/4 alignment {:?}",d.l2i4)).alignment(d.l2i4).grid_pos(0,3),
             ),1,4,).spacing(0.0)
         ).width(200_f64).height(170_f64).padding(Padding::from(0.))
          .background(css::LIGHT_GRAY) //.border(css::RED,0.).rounded(RoundedRectRadii::from_single_radius(0.))
     ,),
     (label("  • unboxed (constrained by root parent's flex in a portal)\n  (Start=Middle: parent Flex container ≝CrossAxisAlignment::Center,\n  so the alignment for a label starts at the center)").alignment(TextAlignment::Justified).brush(m_c),
-    label("  can be fixed with a custom per-element override .flex(FlexParams::new(1.0,CrossAxisAlignment::Start))").alignment(TextAlignment::Justified).brush(m_c),
-    lc("1/4 alignment Start"        ).alignment(TextAlignment::Start        ),
-    lc("2/4 alignment Middle"       ).alignment(TextAlignment::Middle       ),
-    lc("3/4 alignment End"          ).alignment(TextAlignment::End          ),
-    lc("4/4 alignment Justified"    ).alignment(TextAlignment::Justified    ),
+    label("  can be fixed with a custom per-element override .flex(FlexParams::new(1.0,CrossAxisAlignment::Start)) (buttons to change alignment)").alignment(TextAlignment::Justified).brush(m_c),
+    flex((
+      button("Δ1/4", |d:&mut AppState|{d.realign("l3i1");}),
+      button("Δ2/4", |d:&mut AppState|{d.realign("l3i2");}),
+      button("Δ3/4", |d:&mut AppState|{d.realign("l3i3");}),
+      button("Δ4/4", |d:&mut AppState|{d.realign("l3i4");}),
+      )).direction(Axis::Horizontal),
+    lc(format!("1/4 alignment {:?}",d.l3i1)).alignment(d.l3i1),
+    lc(format!("2/4 alignment {:?}",d.l3i2)).alignment(d.l3i2),
+    lc(format!("3/4 alignment {:?}",d.l3i3)).alignment(d.l3i3),
+    lc(format!("4/4 alignment {:?}",d.l3i4)).alignment(d.l3i4),
     ),
-    (label("  • flex in a 500×140 sized_box").alignment(TextAlignment::Justified).brush(m_c),
+    (label("  • flex in a 500×140 sized_box (buttons to change alignment)").alignment(TextAlignment::Justified).brush(m_c),
+    flex((
+      button("Δ1/4", |d:&mut AppState|{d.realign("l4i1");}),
+      button("Δ1/4 cross", |d:&mut AppState|{d.realign("l4i1x");d.realign("l4i1x");}),
+      button("Δ2/4", |d:&mut AppState|{d.realign("l4i2");}),
+      button("Δ3/4", |d:&mut AppState|{d.realign("l4i3");}),
+      button("Δ4/4", |d:&mut AppState|{d.realign("l4i4");}),
+      )).direction(Axis::Horizontal),
     sized_box(flex((
-        lc("1/4 alignment Start"                                ).alignment(TextAlignment::Start        ),
-        lc("1/4 alignment Start + CrossAxisAlignment::Start "   ).alignment(TextAlignment::Start        ).flex(FlexParams::new(1.0,CrossAxisAlignment::Start)),
-        lc("2/4 alignment Middle"                               ).alignment(TextAlignment::Middle       ),
-        lc("3/4 alignment End"                                  ).alignment(TextAlignment::End          ),
-        lc("4/4 alignment Justified"                            ).alignment(TextAlignment::Justified    ),
+        lc(format!("1/4 alignment {:?}",d.l4i1)).alignment(d.l4i1),
+        lc(format!("1/4 alignment {:?} + CrossAxisAlignment {:?}",d.l4i1,d.l4i1x)).alignment(d.l4i1).flex(FlexParams::new(1.0,d.l4i1x)),
+        lc(format!("2/4 alignment {:?}",d.l4i2)).alignment(d.l4i2),
+        lc(format!("3/4 alignment {:?}",d.l4i3)).alignment(d.l4i3),
+        lc(format!("4/4 alignment {:?}",d.l4i4)).alignment(d.l4i4),
         ))
         ).width(500_f64).height(140_f64).padding(Padding::from(0.))
          .background(css::LIGHT_GRAY) //.border(css::RED,0.).rounded(RoundedRectRadii::from_single_radius(0.))
