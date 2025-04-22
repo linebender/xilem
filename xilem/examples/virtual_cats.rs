7c8fb9baaf3c8b0287326da108f82dc602d2e9cf
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-04-22 19:54:32 +0100

    Virtual Scrolling for Xilem (#921)
    The semantics of how this integration works are really ugly - it really
    feels like there's a better model hiding in here somewhere, but that's
    somewhat by-the-by.
    
    However, it does work.
    
    ---------
    
    Co-authored-by: Jared O'Connell <46976761+jaredoconnell@users.noreply.github.com>

---
+++ xilem/examples/virtual_cats.rs
@@ -0,0 +1,183 @@
+// Copyright 2024 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! An example demonstrating the use of virtual scrolling with Async web requests in Xilem
+//!
+//! Uses the same dataset as the `http_cats` example.
+
+#![expect(clippy::missing_assert_message, reason = "Deferred: Noisy")]
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use masonry::core::ArcStr;
+use masonry::widgets::Alignment;
+use vello::peniko::{Blob, Image};
+use winit::dpi::LogicalSize;
+use winit::error::EventLoopError;
+use winit::window::Window;
+use xilem::core::fork;
+use xilem::view::{
+    ObjectFit, ZStackExt, flex, image, prose, sized_box, spinner, virtual_scroll, zstack,
+};
+use xilem::{EventLoop, EventLoopBuilder, LineBreaking, TextAlignment, WidgetView, Xilem, palette};
+use xilem_core::one_of::Either;
+
+/// The main state of the application.
+struct VirtualCats {
+    statuses: Vec<Status>,
+}
+
+#[derive(Debug)]
+struct Status {
+    code: u32,
+    message: ArcStr,
+    image: ImageState,
+}
+
+#[derive(Debug)]
+/// The state of the download of an image from a URL
+enum ImageState {
+    Pending,
+    // Error(?),
+    Available(Image),
+}
+
+impl VirtualCats {
+    fn virtual_item(&mut self, idx: i64) -> impl WidgetView<Self> + use<> {
+        let index: usize = idx.try_into().expect("VirtualScroll bounds set correctly.");
+        let item = self
+            .statuses
+            .get_mut(index)
+            .expect("VirtualScroll bounds set correctly.");
+        let img = match &item.image {
+            ImageState::Pending => None,
+            ImageState::Available(image) => Some(image),
+        };
+        let task = if img.is_none() {
+            // Capturing the code is valid here, because this will never change for this view.
+            let code = item.code;
+            // If the cat is not loaded yet, we create a task to load it.
+            // N.B. If we wanted to batch requests, the lifecycle unfortunately gets a
+            // lot more complicated currently.
+            // Note that if this item scrolls offscreen, the ongoing load will be cancelled.
+            Some(xilem::view::task_raw(
+                move |proxy| {
+                    async move {
+                        let url = format!("https://http.cat/{code}");
+                        // Add an artificial delay to show how variable sizes work
+                        tokio::time::sleep(Duration::from_millis(300)).await;
+                        let result = image_from_url(&url).await;
+                        match result {
+                            // We choose not to handle the case where the event loop has ended
+                            Ok(image) => drop(proxy.message(image)),
+                            // TODO: Report in the frontend
+                            Err(err) => {
+                                tracing::warn!(
+                                    "Loading image for HTTP status code {code} from {url} failed: {err:?}"
+                                );
+                            }
+                        }
+                    }
+                },
+                move |state: &mut Self, image| {
+                    if let Some(status) = state.statuses.iter_mut().find(|it| it.code == code) {
+                        status.image = ImageState::Available(image);
+                    } else {
+                        unreachable!("We never remove items from `statuses`")
+                    }
+                },
+            ))
+        } else {
+            None
+        };
+        let img = if let Some(img) = img {
+            let attribution = sized_box(
+                sized_box(
+                    prose("Copyright ©️ https://http.cat")
+                        .line_break_mode(LineBreaking::Clip)
+                        .alignment(TextAlignment::End),
+                )
+                .padding(4.)
+                .rounded(4.)
+                .background(palette::css::BLACK.multiply_alpha(0.5)),
+            )
+            .padding((30., 42., 0., 0.));
+            Either::A(zstack((
+                image(img).fit(ObjectFit::FitWidth),
+                attribution.alignment(Alignment::TopTrailing),
+            )))
+        } else {
+            Either::B(sized_box(spinner()).width(80.).height(80.))
+        };
+        fork(flex((prose(item.message.clone()), img)), task)
+    }
+
+    fn view(&mut self) -> impl WidgetView<Self> + use<> {
+        virtual_scroll(0..self.statuses.len() as i64, Self::virtual_item)
+    }
+}
+
+/// Load a [`vello::peniko::Image`] from the given url.
+async fn image_from_url(url: &str) -> anyhow::Result<Image> {
+    let response = reqwest::get(url).await?;
+    let bytes = response.bytes().await?;
+    let image = image::load_from_memory(&bytes)?.into_rgba8();
+    let width = image.width();
+    let height = image.height();
+    let data = image.into_vec();
+    Ok(Image::new(
+        Blob::new(Arc::new(data)),
+        vello::peniko::ImageFormat::Rgba8,
+        width,
+        height,
+    ))
+}
+
+fn run(event_loop: EventLoopBuilder) -> Result<(), EventLoopError> {
+    let data = VirtualCats {
+        statuses: Status::parse_file(),
+    };
+
+    let app = Xilem::new(data, VirtualCats::view);
+    let min_window_size = LogicalSize::new(200., 200.);
+
+    let window_attributes = Window::default_attributes()
+        .with_title("Virtualised HTTP cats")
+        .with_resizable(true)
+        .with_min_inner_size(min_window_size);
+
+    app.run_windowed_in(event_loop, window_attributes)
+}
+
+impl Status {
+    /// Parse the supported HTTP cats.
+    fn parse_file() -> Vec<Self> {
+        let mut lines = STATUS_CODES_CSV.lines();
+        let first_line = lines.next();
+        assert_eq!(first_line, Some("code,message"));
+        lines.flat_map(Self::parse_single).collect()
+    }
+
+    fn parse_single(line: &'static str) -> Option<Self> {
+        let (code, message) = line.split_once(',')?;
+        Some(Self {
+            code: code.parse().ok()?,
+            message: format!("{}:", message.trim()).into(),
+            image: ImageState::Pending,
+        })
+    }
+}
+
+/// The status codes supported by <https://http.cat>, used under the MIT license.
+/// Full details can be found in `xilem/resources/data/http_cats_status/README.md` from
+/// the workspace root.
+const STATUS_CODES_CSV: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/resources/data/http_cats_status/status.csv",
+));
+
+// TODO: This example doesn't support Android because the virtual scroll component doesn't have gesture support.
+fn main() -> Result<(), EventLoopError> {
+    run(EventLoop::with_user_event())
+}
---
+++ xilem/src/view/virtual_scroll.rs
@@ -0,0 +1,388 @@
+// Copyright 2025 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
+use std::{collections::HashMap, marker::PhantomData, ops::Range};
+
+use masonry::{
+    core::{FromDynWidget, Widget, WidgetPod},
+    widgets::{self, VirtualScrollAction},
+};
+use private::VirtualScrollState;
+use xilem_core::{AsyncCtx, DynMessage, MessageResult, View, ViewId, ViewMarker, ViewPathTracker};
+
+use crate::{Pod, ViewCtx, WidgetView};
+
+/// A (vertical) virtual scrolling View, for Masonry's [`VirtualScroll`](widgets::VirtualScroll).
+///
+/// Virtual scrolling is a technique to improve performance when scrolling through long lists, by
+/// only loading (and therefore laying out, drawing, processing for event handling), the items visible to the user.
+///
+/// The implementation has some caveats, which are discussed in the documentation of the [underlying widget](widgets::VirtualScroll).
+///
+/// Whenever this view is rebuilt, all of the loaded children are rebuild.
+/// The child creation function is a "component" context, (alike to the usual `app_logic` functions), which means
+/// that changing the app's state in this function will *not* cause a rebuild or rerunning of the app
+/// logic (this avoids infinite loops).
+/// It is correct for `func` to capture, if necessary.
+/// However, it also has access to the app's state, so this is unlikely to be needed.
+pub struct VirtualScroll<State, Action, ChildrenViews, F, Element: ?Sized> {
+    phantom: PhantomData<fn() -> (WidgetPod<Element>, State, Action, ChildrenViews)>,
+    func: F,
+    valid_range: Range<i64>,
+}
+
+/// Component for [`VirtualScroll`].
+///
+/// Arguments:
+/// - `valid_range` is the range of ids which are supported.
+/// - `func` is the component for this element's children.
+///   It is provided with the app's state and the index of the child.
+///
+/// For full details, see the documentation on the [view type](VirtualScroll).
+pub fn virtual_scroll<State, Action, ChildrenViews, F, Element>(
+    valid_range: Range<i64>,
+    func: F,
+) -> VirtualScroll<State, Action, ChildrenViews, F, Element>
+where
+    ChildrenViews: WidgetView<State, Action, Widget = Element>,
+    F: Fn(&mut State, i64) -> ChildrenViews + 'static,
+    Element: Widget + FromDynWidget + ?Sized,
+{
+    VirtualScroll {
+        phantom: PhantomData,
+        func,
+        valid_range,
+    }
+}
+
+/// Component for a [`VirtualScroll`] with unlimited children.
+///
+/// Arguments:
+/// - `func` is the component for this element's children.
+///   It is provided with the app's state and the index of the child.
+///
+/// For full details, see the documentation on the [view type](VirtualScroll).
+pub fn unlimited_virtual_scroll<State, Action, ChildrenViews, F, Element>(
+    func: F,
+) -> VirtualScroll<State, Action, ChildrenViews, F, Element>
+where
+    ChildrenViews: WidgetView<State, Action, Widget = Element>,
+    F: Fn(&mut State, i64) -> ChildrenViews + 'static,
+{
+    VirtualScroll {
+        phantom: PhantomData,
+        func,
+        valid_range: i64::MIN..i64::MAX,
+    }
+}
+
+mod private {
+    use std::{collections::HashMap, sync::Arc};
+
+    use masonry::widgets::VirtualScrollAction;
+    use xilem_core::ViewId;
+
+    #[expect(
+        unnameable_types,
+        reason = "Not meaningful public API; required to be public due to design of View trait"
+    )]
+    pub struct VirtualScrollState<View, State> {
+        pub(super) pending_action: Option<VirtualScrollAction>,
+        pub(super) previous_views: HashMap<i64, View>,
+        pub(super) current_updated: bool,
+        pub(super) pending_children_update: bool,
+        pub(super) current_views: HashMap<i64, View>,
+        pub(super) view_states: HashMap<i64, ChildState<State>>,
+        pub(super) my_path: Arc<[ViewId]>,
+        pub(super) cleanup_queue: Vec<i64>,
+    }
+
+    pub(super) struct ChildState<State> {
+        pub(super) state: State,
+        pub(super) requested_rebuild: bool,
+    }
+}
+
+/// Create the view id used for child views.
+///
+/// This is a minimal function around [`i64::cast_unsigned`] (which is unstable, so polyfilled).
+const fn view_id_for_index(idx: i64) -> ViewId {
+    /* i64::cast_unsigned is unstable */
+    ViewId::new(idx as u64)
+}
+
+/// Get the index stored in the view id.
+///
+/// This is a minimal function around [`u64::cast_signed`] (which is unstable, so polyfilled).
+const fn index_for_view_id(id: ViewId) -> i64 {
+    /* u64::cast_unsigned is unstable */
+    id.routing_id() as i64
+}
+
+#[derive(Debug)]
+struct UpdateVirtualChildren;
+
+impl<State, Action, ChildrenViews, F, Element: Widget + FromDynWidget + ?Sized> ViewMarker
+    for VirtualScroll<State, Action, ChildrenViews, F, Element>
+{
+}
+impl<State, Action, Element, ChildrenViews, F> View<State, Action, ViewCtx>
+    for VirtualScroll<State, Action, ChildrenViews, F, Element>
+where
+    State: 'static,
+    Action: 'static,
+    ChildrenViews: WidgetView<State, Action, Widget = Element>,
+    F: Fn(&mut State, i64) -> ChildrenViews + 'static,
+    Element: Widget + FromDynWidget + ?Sized,
+{
+    type Element = Pod<widgets::VirtualScroll<Element>>;
+
+    type ViewState = VirtualScrollState<ChildrenViews, ChildrenViews::ViewState>;
+
+    fn build(&self, ctx: &mut ViewCtx) -> (Self::Element, Self::ViewState) {
+        // TODO: How does the anchor interact with Xilem?
+        // Setting that seems like an imperative action?
+        let widget = Pod::new(
+            widgets::VirtualScroll::<Element>::new(0).with_valid_range(self.valid_range.clone()),
+        );
+        ctx.record_action(widget.id);
+        (
+            widget,
+            private::VirtualScrollState {
+                pending_action: None,
+                previous_views: HashMap::default(),
+                current_updated: false,
+                pending_children_update: false,
+                current_views: HashMap::default(),
+                view_states: HashMap::default(),
+                my_path: ctx.view_path().into(),
+                cleanup_queue: Vec::default(),
+            },
+        )
+    }
+
+    // This implementation is ugly. This is needed because the `rebuild` function
+    // doesn't have access to the app's state. The way we handle this is:
+    // 1) If the app's state has changed since the last rebuild (i.e. the `app_logic` function has been rerun),
+    //    we send a message to ourselves to recreate all of our loaded children.
+    // 2) If there are new versions of our children, we rebuild/build those.
+    // 3) We make sure to rebuild all children which have requested a rebuild.
+
+    fn rebuild(
+        &self,
+        prev: &Self,
+        view_state: &mut Self::ViewState,
+        ctx: &mut ViewCtx,
+        mut element: xilem_core::Mut<'_, Self::Element>,
+    ) {
+        if self.valid_range != prev.valid_range {
+            widgets::VirtualScroll::set_valid_range(&mut element, self.valid_range.clone());
+        }
+        if ctx.state_changed() && !view_state.pending_children_update {
+            let proxy = ctx.proxy();
+            proxy
+                .send_message(
+                    view_state.my_path.clone(),
+                    DynMessage::new(UpdateVirtualChildren),
+                )
+                .unwrap();
+            view_state.pending_children_update = true;
+            // We think it would be fine to not actually rebuild here (and wait for the message to be handled)
+            // but rebuilding here does still work
+        }
+        let used_action = view_state.pending_action.is_some();
+        if let Some(pending_action) = view_state.pending_action.take() {
+            debug_assert!(view_state.current_updated);
+            widgets::VirtualScroll::will_handle_action(&mut element, &pending_action);
+        }
+        if !view_state.current_updated {
+            // Our state hasn't changed, but we need to rebuild any children which have requested it.
+            for (idx, view) in &view_state.previous_views {
+                let child_state = view_state
+                    .view_states
+                    .get_mut(idx)
+                    .expect("`view_states` is always in sync with `previous_views`");
+                if child_state.requested_rebuild {
+                    ctx.with_id(view_id_for_index(*idx), |ctx| {
+                        // Note that we rebuild the view with itself, because we're only actioning a requested rebuild.
+                        // This should be a no-op other than whatever caused the requested rebuild.
+                        view.rebuild(
+                            view,
+                            &mut child_state.state,
+                            ctx,
+                            widgets::VirtualScroll::child_mut(&mut element, *idx),
+                        );
+                    });
+                    child_state.requested_rebuild = false;
+                }
+            }
+        } else {
+            // Otherwise, our set of loaded children has changed, and/or our loaded children all have a new version.
+            debug_assert!(view_state.cleanup_queue.is_empty());
+            // Remove any children which have been unloaded.
+            for (&idx, child) in &view_state.previous_views {
+                if view_state.current_views.contains_key(&idx) {
+                    // We will handle this in the second loop.
+                    continue;
+                }
+                debug_assert!(
+                    used_action,
+                    "Xilem VirtualScroll: Would remove an item even though we weren't handling an action."
+                );
+                ctx.with_id(view_id_for_index(idx), |ctx| {
+                    let child_state = view_state
+                        .view_states
+                        .get_mut(&idx)
+                        .expect("`view_states` is always in sync with `previous_views`");
+                    child.teardown(
+                        &mut child_state.state,
+                        ctx,
+                        widgets::VirtualScroll::child_mut(&mut element, idx),
+                    );
+                    widgets::VirtualScroll::remove_child(&mut element, idx);
+                    view_state.cleanup_queue.push(idx);
+                });
+            }
+            for to_cleanup in view_state.cleanup_queue.drain(..) {
+                view_state
+                    .previous_views
+                    .remove(&to_cleanup)
+                    .expect("Cleanup index is real item in list");
+                view_state
+                    .view_states
+                    .remove(&to_cleanup)
+                    .expect("`view_states` is always in sync with `previous_views`");
+            }
+            for (idx, child) in view_state.current_views.drain() {
+                ctx.with_id(view_id_for_index(idx), |ctx| {
+                    if let Some(child_prev) = view_state.previous_views.get(&idx) {
+                        // If there was previously a version of this view (i.e. we only updated it)
+                        // then perform only a rebuild.
+                        let child_state = view_state
+                            .view_states
+                            .get_mut(&idx)
+                            .expect("`view_states` is always in sync with `previous_views`");
+                        child.rebuild(
+                            child_prev,
+                            &mut child_state.state,
+                            ctx,
+                            widgets::VirtualScroll::child_mut(&mut element, idx),
+                        );
+                        child_state.requested_rebuild = false;
+                        view_state.previous_views.insert(idx, child);
+                    } else {
+                        debug_assert!(used_action);
+                        // Otherwise, build the first version of this view.
+                        let (new_child, child_state) = child.build(ctx);
+                        widgets::VirtualScroll::add_child(
+                            &mut element,
+                            idx,
+                            new_child.into_widget_pod(),
+                        );
+
+                        view_state.previous_views.insert(idx, child);
+                        view_state.view_states.insert(
+                            idx,
+                            private::ChildState {
+                                state: child_state,
+                                requested_rebuild: false,
+                            },
+                        );
+                    }
+                });
+            }
+            // We have just "used" up the current states, so mark them as inactive.
+            view_state.current_updated = false;
+        }
+        debug_assert_eq!(
+            view_state.previous_views.len(),
+            view_state.view_states.len()
+        );
+    }
+
+    fn teardown(
+        &self,
+        view_state: &mut Self::ViewState,
+        ctx: &mut ViewCtx,
+        mut element: xilem_core::Mut<'_, Self::Element>,
+    ) {
+        for (&idx, child) in &view_state.previous_views {
+            ctx.with_id(view_id_for_index(idx), |ctx| {
+                let view_state = view_state.view_states.get_mut(&idx).unwrap();
+                child.teardown(
+                    &mut view_state.state,
+                    ctx,
+                    widgets::VirtualScroll::child_mut(&mut element, idx),
+                );
+            });
+        }
+        ctx.teardown_leaf(element);
+    }
+
+    fn message(
+        &self,
+        view_state: &mut Self::ViewState,
+        id_path: &[xilem_core::ViewId],
+        message: DynMessage,
+        app_state: &mut State,
+    ) -> xilem_core::MessageResult<Action> {
+        if let [first, tail @ ..] = id_path {
+            let child_idx = index_for_view_id(*first);
+            let target = view_state.previous_views.get(&child_idx);
+            if let Some(target) = target {
+                let state = view_state.view_states.get_mut(&child_idx).unwrap();
+                let result = target.message(&mut state.state, tail, message, app_state);
+                if matches!(result, MessageResult::RequestRebuild) {
+                    state.requested_rebuild = true;
+                }
+                return result;
+            } else {
+                tracing::error!("Message sent type in VirtualScroll::message: {message:?}");
+                return MessageResult::Stale(message);
+            }
+        }
+        if message.is::<masonry::core::Action>() {
+            let action = message.downcast::<masonry::core::Action>().unwrap();
+            if let masonry::core::Action::Other(action) = *action {
+                if !action.is::<VirtualScrollAction>() {
+                    tracing::error!("Wrong action type in VirtualScroll::message: {action:?}");
+                    // Ideally we'd avoid this extra box, but it's not easy to write this kind of code in a clean way
+                    return MessageResult::Stale(DynMessage::new(masonry::core::Action::Other(
+                        action,
+                    )));
+                }
+                // We check then unwrap to avoid unwrapping a box (also, it makes the check path an early-exit)
+                let action = action.downcast::<VirtualScrollAction>().unwrap();
+
+                view_state.current_updated = true;
+                // We know that the `current_views` have not been applied, so we can just brute force overwrite them.
+                view_state.current_views.clear();
+                for new_targets in action.target.clone() {
+                    // TODO: Ideally, we'd avoid updating the already existing items
+                    // Doing so however dramatically increases the complexity in `rebuild`
+                    view_state
+                        .current_views
+                        .insert(new_targets, (self.func)(app_state, new_targets));
+                }
+                view_state.pending_action = Some(*action);
+                MessageResult::RequestRebuild
+            } else {
+                tracing::error!("Wrong action type in VirtualScroll::message: {action:?}");
+                MessageResult::Stale(DynMessage(action))
+            }
+        } else if message.is::<UpdateVirtualChildren>() {
+            view_state.current_updated = true;
+            view_state.current_views.clear();
+            view_state.pending_children_update = false;
+            for &key in view_state.previous_views.keys() {
+                view_state
+                    .current_views
+                    .insert(key, (self.func)(app_state, key));
+            }
+            MessageResult::RequestRebuild
+        } else {
+            tracing::error!("Wrong message type in VirtualScroll::message: {message:?}");
+            MessageResult::Stale(message)
+        }
+    }
+}
