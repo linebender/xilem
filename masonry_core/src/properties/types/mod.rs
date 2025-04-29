8cafc50affd9263635782e28eadc7e13f47f0b7c
Olivier FAURE <couteaubleu@gmail.com>
2025-04-29 12:46:53 +0200

    Better document `properties::types`  module.

--- masonry_core/src/properties/types/mod.rs
+++ masonry_core/src/properties/types/mod.rs
@@ -2,6 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Value types used in properties.
+//!
+//! The types in this modules aren't properties themselves: they're types that the fields
+//! and variants of properties can have.
+//!
+//! So for instance, you can't set a button's [`Gradient`], but you can set a button's
+//! [`Background`] to [`Background::Gradient`], which takes a `Gradient` value.
+//!
+//! [`Background`]: crate::properties::Background
+//! [`Background::Gradient`]: crate::properties::Background::Gradient
 
 mod gradient;
 
