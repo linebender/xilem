6ecfebb49b762680f6fd892ebd131a1d2f7b579b
Olivier FAURE <couteaubleu@gmail.com>
2025-04-30 16:11:15 +0200

    Move example-test module to `tests/` folder.

--- masonry/tests/examples.rs
+++ masonry/tests/examples.rs
@@ -1,19 +1,15 @@
 // Copyright 2025 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-//! This module imports widget code from other examples.
+//! This module imports widget code from most of the examples.
 //!
 //! When running unit tests, it will compare the rendered snapshots of the widgets
 //! with the expected snapshots.
 
-fn main() {
-    println!("This example is only used to compile other examples.");
-}
-
 #[cfg(test)]
 #[allow(dead_code, reason = "We don't need to run the main functions.")]
 #[allow(missing_docs, reason = "Example code doesn't need docs.")]
-#[path = "."]
+#[path = "../examples"]
 pub mod others {
     pub mod calc_masonry;
     pub mod custom_widget;
