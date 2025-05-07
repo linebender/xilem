42f2bf6829e180f168c92ad8b9fba98ea88556e2
Olivier FAURE <couteaubleu@gmail.com>
2025-05-07 16:20:03 +0200

    Fix unused variable

--- masonry_core/src/core/properties.rs
+++ masonry_core/src/core/properties.rs
@@ -1,7 +1,7 @@
 // Copyright 2025 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-use std::any::{Any, TypeId};
+use std::any::TypeId;
 use std::collections::HashMap;
 use std::default::Default;
 
