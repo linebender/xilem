bc3b39fef7b09c8b38d95f90b2c2f3c503245076
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-05-30 11:02:38 +0100

    Ensure that IME events interact with focus as expected; remove bubbling for IME events (#1004)
    Bubbling of IME events interacted extremely poorly with focus,
    especially around widget removal, but also potentially re-parenting in
    future.
    
    This PR is as a result of #999.
    
    The new test passes on main, but it does output the error added in that
    PR.

--- masonry/src/passes/update.rs
+++ masonry/src/passes/update.rs
@@ -545,7 +545,17 @@ pub(crate) fn run_update_focus_pass(root: &mut RenderRoot) {
         // IME was active, but the next focused widget is going to receive the Ime::Disabled event
         // sent by the platform. Synthesize an `Ime::Disabled` event here and send it to the widget
         // about to be unfocused.
-        run_on_text_event_pass(root, &TextEvent::Ime(Ime::Disabled));
+
+        // HACK: It's not valid to send an event to a non-existent widget, so we check that the "previously"
+        // focused widget hasn't just been deleted.
+        // This means that if a parent widget was handling IME events, it won't get this event.
+        // We know that IME events bubbling is not the correct behaviour, but have chosen to keep it for consistency,
+        // as we also are planning to refactor how IME is delivered as we update to use Android View.
+        if let Some(prev_focused) = prev_focused {
+            if root.get_widget(prev_focused).is_some() {
+                run_on_text_event_pass(root, &TextEvent::Ime(Ime::Disabled));
+            }
+        }
 
         // Disable the IME, which was enabled specifically for this widget. Note that if the newly
         // focused widget also requires IME, we will request it again - this resets the platform's
---
+++ masonry/src/widgets/tests/ime_focused.rs
@@ -0,0 +1,37 @@
+// Copyright 2025 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    core::WidgetPod,
+    testing::{TestHarness, widget_ids},
+    widgets::{Flex, TextArea, Textbox},
+};
+
+/// Tests that IME's interactions with focus are sensible.
+
+#[test]
+fn ime_on_remove() {
+    let [text_area] = widget_ids();
+    let widget = Flex::column().with_child(Textbox::from_text_area_pod(WidgetPod::new_with_id(
+        TextArea::new_editable("Simple input test"),
+        text_area,
+    )));
+
+    let mut harness = TestHarness::create(widget);
+    harness.focus_on(Some(text_area));
+    harness.process_text_event(crate::core::TextEvent::Ime(crate::core::Ime::Commit(
+        "New Text".to_string(),
+    )));
+    let text_area = harness
+        .get_widget(text_area)
+        .downcast::<TextArea<true>>()
+        .unwrap();
+    // TODO: Ideally the cursor would start at the logical end of the text.
+    assert_eq!(text_area.widget.text(), "New TextSimple input test");
+    let ime_area_size = harness.ime_rect().1;
+    assert!(ime_area_size.width > 0. && ime_area_size.height > 0.);
+    harness.edit_root_widget(|mut widget| {
+        let mut widget = widget.downcast::<Flex>();
+        Flex::remove_child(&mut widget, 0);
+    });
+}
