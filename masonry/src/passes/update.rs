a58e55e350a6a266bd62454079438c69dcf8b331
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-05-30 08:45:51 +0100

    Update comment to reflect blocking of more principled solution

--- masonry/src/passes/update.rs
+++ masonry/src/passes/update.rs
@@ -546,8 +546,11 @@ pub(crate) fn run_update_focus_pass(root: &mut RenderRoot) {
         // sent by the platform. Synthesize an `Ime::Disabled` event here and send it to the widget
         // about to be unfocused.
 
-        // It's not valid to send an event to a non-existent widget, so we check that the "previously"
+        // HACK: It's not valid to send an event to a non-existent widget, so we check that the "previously"
         // focused widget hasn't just been deleted.
+        // This means that if a parent widget was handling IME events, it won't get this event.
+        // We know that IME events bubbling is not the correct behaviour, but have chosen to keep it for consistency,
+        // as we also are planning to refactor how IME is delivered as we update to use Android View.
         if let Some(prev_focused) = prev_focused {
             if root.get_widget(prev_focused).is_some() {
                 run_on_text_event_pass(root, &TextEvent::Ime(Ime::Disabled));
