18ed1aba8c38b471695ba53d37996d7af2fd28d5
Olivier FAURE <couteaubleu@gmail.com>
2025-06-03 18:15:23 +0200

    Fix clippy lint

--- masonry/src/lib.rs
+++ masonry/src/lib.rs
@@ -36,7 +36,6 @@
     unnameable_types,
     reason = "Requires lint_reasons rustc feature for exceptions"
 )]
-#![expect(clippy::single_match, reason = "General policy not decided")]
 
 // TODO - Add logo
 
