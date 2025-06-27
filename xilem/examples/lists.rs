3c115f41ac5bbefa150d80c155d27a14d63673a8
Nils Martel <nilsmartel@yahoo.de>
2025-06-27 10:41:02 +0200

    Create example to showcase variable length lists (#1097)
    Lists are frequently a pain point in ui toolkits.
    In Xilem it's so trivial, that it can easily be overlooked as something
    worth communicating explicitly. This example is really simple, yet I
    feel like it provides a lot of value to someone who wants to check out
    the capabilities of xilem. source: I myself wondered about lists :)
    
    
    <img width="802" alt="image"
    src="https://github.com/user-attachments/assets/2d653f21-5c66-4de3-a255-b3fb5a286503"
    />
    
    
    (the "more" button was included to illustrate the dynamic nature of a
    list compared to static length uis)
    
    ---------
    
    Co-authored-by: Daniel McNab <36049421+DJMcNab@users.noreply.github.com>

---
+++ xilem/examples/lists.rs
@@ -0,0 +1,36 @@
+// Copyright 2024 the Xilem Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//! Displaying a variable length list is achieved using a [Vec] `FlexSequence`.
+
+use winit::error::EventLoopError;
+use xilem::view::{Axis, MainAxisAlignment, button, flex, prose};
+use xilem::{EventLoop, WidgetView, WindowOptions, Xilem};
+
+#[derive(Default)]
+struct AppState {
+    count: usize,
+}
+
+fn app_logic(state: &mut AppState) -> impl WidgetView<AppState> + use<> {
+    // A vector (Vec) of views can be used as part of a `flex`'s children, allowing collections of dynamic length to be displayed.
+    let list = (0..state.count)
+        .map(|n| prose(format!("item #{}", n)))
+        .collect::<Vec<_>>();
+
+    flex((
+        // Even when a `Vec` is used for the children, other widgets can be included by putting them in a
+        // tuple of children alongside the vector.
+        button("more", |appstate: &mut AppState| appstate.count += 1),
+        list,
+    ))
+    .direction(Axis::Vertical) // Top to Bottom
+    // We can control alignment of the elements in the flexbox
+    .main_axis_alignment(MainAxisAlignment::Start) // Aligned to the left
+}
+
+fn main() -> Result<(), EventLoopError> {
+    let app = Xilem::new_simple(AppState::default(), app_logic, WindowOptions::new("Lists"));
+    app.run_in(EventLoop::with_user_event())?;
+    Ok(())
+}
