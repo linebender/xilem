57c6be11ed2972f9203b53b55808e2952fa276bf
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-07-11 14:45:59 +0100

    Validate the context support in Mason

--- xilem/examples/mason.rs
+++ xilem/examples/mason.rs
@@ -20,9 +20,22 @@ use xilem::{
     EventLoop, EventLoopBuilder, FontWeight, InsertNewline, TextAlign, WidgetView, WindowOptions,
     Xilem, palette,
 };
+use xilem_core::environment::{Resource, provides, with_context};
 const LOREM: &str = r"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus mi sed euismod euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam placerat efficitur tellus at semper. Morbi ac risus magna. Donec ut cursus ex. Etiam quis posuere tellus. Mauris posuere dui et turpis mollis, vitae luctus tellus consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu facilisis nisl.
 
 Phasellus in viverra dolor, vitae facilisis est. Maecenas malesuada massa vel ultricies feugiat. Vivamus venenatis et nibh nec pharetra. Phasellus vestibulum elit enim, nec scelerisque orci faucibus id. Vivamus consequat purus sit amet orci egestas, non iaculis massa porttitor. Vestibulum ut eros leo. In fermentum convallis magna in finibus. Donec justo leo, maximus ac laoreet id, volutpat ut elit. Mauris sed leo non neque laoreet faucibus. Aliquam orci arcu, faucibus in molestie eget, ornare non dui. Donec volutpat nulla in fringilla elementum. Aliquam vitae ante egestas ligula tempus vestibulum sit amet sed ante. ";
+
+#[derive(Debug)]
+struct SomeContext(u32);
+impl Resource for SomeContext {}
+
+fn env_using() -> impl WidgetView<AppData> + use<> {
+    with_context(|context: &mut SomeContext, _: &mut AppData| {
+        button(format!("Context: {}", context.0), |_: &mut AppData| {
+            tracing::warn!("Does nothing");
+        })
+    })
+}
 
 fn app_logic(data: &mut AppData) -> impl WidgetView<AppData> + use<> {
     // here's some logic, deriving state for the view from our state
@@ -65,80 +78,89 @@ fn app_logic(data: &mut AppData) -> impl WidgetView<AppData> + use<> {
         }
     });
 
-    fork(
-        flex((
-            flex_row((
-                label("Label").brush(palette::css::REBECCA_PURPLE),
-                label("Bold Label").weight(FontWeight::BOLD),
-                // TODO masonry doesn't allow setting disabled manually anymore?
-                // label("Disabled label").disabled(),
-            )),
-            flex(
-                text_input(
-                    data.text_input_contents.clone(),
-                    |data: &mut AppData, new_value| {
-                        data.text_input_contents = new_value;
+    provides(
+        |_: &mut AppData| SomeContext(120),
+        fork(
+            flex((
+                env_using(),
+                flex_row((
+                    label("Label").brush(palette::css::REBECCA_PURPLE),
+                    label("Bold Label").weight(FontWeight::BOLD),
+                    // TODO masonry doesn't allow setting disabled manually anymore?
+                    // label("Disabled label").disabled(),
+                )),
+                flex(
+                    text_input(
+                        data.text_input_contents.clone(),
+                        |data: &mut AppData, new_value| {
+                            data.text_input_contents = new_value;
+                        },
+                    )
+                    .insert_newline(InsertNewline::OnEnter),
+                )
+                // Manually adding a direction is equivalent to using flex_row
+                .direction(Axis::Horizontal),
+                prose(LOREM)
+                    .text_alignment(TextAlign::Middle)
+                    .text_size(18.),
+                button_any_pointer(button_label, |data: &mut AppData, button| match button {
+                    None => {
+                        // Usually this is a touch.
+                    }
+                    Some(PointerButton::Primary) => data.count += 1,
+                    Some(PointerButton::Secondary) => data.count -= 1,
+                    Some(PointerButton::Auxiliary) => data.count *= 2,
+                    _ => (),
+                }),
+                checkbox("Check me", data.active, |data: &mut AppData, checked| {
+                    data.active = checked;
+                }),
+                toggleable(data),
+                env_using(),
+                button("Decrement", |data: &mut AppData| data.count -= 1),
+                button("Reset", |data: &mut AppData| data.count = 0),
+                flex((fizz_buzz_flex_sequence, flex_sequence)).direction(axis),
+            ))
+            .padding(8.0),
+            // The following `task` view only exists whilst the example is in the "active" state, so
+            // the updates it performs will only be running whilst we are in that state.
+            data.active.then(|| {
+                task(
+                    |proxy| async move {
+                        let mut interval = time::interval(Duration::from_secs(1));
+                        loop {
+                            interval.tick().await;
+                            let Ok(()) = proxy.message(()) else {
+                                break;
+                            };
+                        }
+                    },
+                    |data: &mut AppData, ()| {
+                        data.count += 1;
                     },
                 )
-                .insert_newline(InsertNewline::OnEnter),
-            )
-            // Manually adding a direction is equivalent to using flex_row
-            .direction(Axis::Horizontal),
-            prose(LOREM)
-                .text_alignment(TextAlign::Middle)
-                .text_size(18.),
-            button_any_pointer(button_label, |data: &mut AppData, button| match button {
-                None => {
-                    // Usually this is a touch.
-                }
-                Some(PointerButton::Primary) => data.count += 1,
-                Some(PointerButton::Secondary) => data.count -= 1,
-                Some(PointerButton::Auxiliary) => data.count *= 2,
-                _ => (),
             }),
-            checkbox("Check me", data.active, |data: &mut AppData, checked| {
-                data.active = checked;
-            }),
-            toggleable(data),
-            button("Decrement", |data: &mut AppData| data.count -= 1),
-            button("Reset", |data: &mut AppData| data.count = 0),
-            flex((fizz_buzz_flex_sequence, flex_sequence)).direction(axis),
-        ))
-        .padding(8.0),
-        // The following `task` view only exists whilst the example is in the "active" state, so
-        // the updates it performs will only be running whilst we are in that state.
-        data.active.then(|| {
-            task(
-                |proxy| async move {
-                    let mut interval = time::interval(Duration::from_secs(1));
-                    loop {
-                        interval.tick().await;
-                        let Ok(()) = proxy.message(()) else {
-                            break;
-                        };
-                    }
-                },
-                |data: &mut AppData, ()| {
-                    data.count += 1;
-                },
-            )
-        }),
+        ),
     )
 }
 
 fn toggleable(data: &mut AppData) -> impl WidgetView<AppData> + use<> {
     if data.active {
-        flex_row((
-            button("Deactivate", |data: &mut AppData| {
-                data.active = false;
-            }),
-            button("Unlimited Power", |data: &mut AppData| {
-                data.count = -1_000_000;
-            }),
-            without_elements(run_once(|| {
-                tracing::warn!("The pathway to unlimited power has been revealed");
-            })),
-        ))
+        provides(
+            |_| SomeContext(777),
+            flex_row((
+                button("Deactivate", |data: &mut AppData| {
+                    data.active = false;
+                }),
+                button("Unlimited Power", |data: &mut AppData| {
+                    data.count = -1_000_000;
+                }),
+                without_elements(run_once(|| {
+                    tracing::warn!("The pathway to unlimited power has been revealed");
+                })),
+                env_using(),
+            )),
+        )
         .boxed()
     } else {
         button("Activate", |data: &mut AppData| data.active = true).boxed()
