68dff90ead012c90237a39c9339ca52695e86d25
Matt Campbell <mattcampbell@pobox.com>
2025-07-17 15:47:24 +0200

    Update Parley and AccessKit (#1172)

--- xilem/examples/emoji_picker.rs
+++ xilem/examples/emoji_picker.rs
@@ -63,7 +63,7 @@ fn picker(data: &mut EmojiPagination) -> impl WidgetView<EmojiPagination> + use<
                     },
                 ))
                 .expand_width(),
-                sized_box(prose(emoji.name).text_alignment(TextAlign::Middle).brush(
+                sized_box(prose(emoji.name).text_alignment(TextAlign::Center).brush(
                     if data.last_selected.is_some_and(|it| it == idx) {
                         // TODO: Ensure this selection indicator color is accessible
                         // TODO: Expose selected state to accessibility tree
