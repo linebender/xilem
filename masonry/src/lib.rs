7a6b0195294cb2fd71d5765d9d9aad419e4a6fd0
Olivier FAURE <couteaubleu@gmail.com>
2025-07-19 21:47:13 +0200

    Remove `#![expect(clippy::missing_assert_message]`

--- masonry/src/lib.rs
+++ masonry/src/lib.rs
@@ -27,7 +27,6 @@
 // TODO: Remove any items listed as "Deferred"
 #![expect(missing_debug_implementations, reason = "Deferred: Noisy")]
 #![expect(clippy::cast_possible_truncation, reason = "Deferred: Noisy")]
-#![expect(clippy::missing_assert_message, reason = "Deferred: Noisy")]
 #![expect(unreachable_pub, reason = "Potentially controversial code style")]
 #![expect(clippy::single_match, reason = "General policy not decided")]
 
