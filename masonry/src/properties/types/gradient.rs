0e24f873ddef2d8c75b280e3a93fa8a835eb48fc
Olivier FAURE <couteaubleu@gmail.com>
2025-07-21 19:49:48 +0200

    Add new gradient types (#1206)
    Add `GradientShape::Radial` variant.
    Add `GradientShape::Sweep` variant.
    
    Add new `Gradient` constructors.
    Add screenshot tests for new gradients.
    
    This PR adds a lot of TODOs, but implementing gradients in a way that
    closely matches the CSS spec is just hard.
    It still covers many common use-cases, and as such it's worth merging
    this before figuring out the full solution.
    
    ---------
    
    Co-authored-by: Daniel McNab <36049421+DJMcNab@users.noreply.github.com>

--- masonry/src/properties/types/gradient.rs
+++ masonry/src/properties/types/gradient.rs
@@ -1,10 +1,13 @@
 // Copyright 2025 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
+use std::f64::consts::TAU;
+
 use vello::kurbo::{Point, Rect};
 
 use crate::peniko::color::{ColorSpaceTag, HueDirection};
 use crate::peniko::{ColorStops, ColorStopsSource, Extend};
+use crate::properties::types::UnitPoint;
 
 /// Properties for the supported [`Gradient`] types.
 ///
@@ -13,6 +16,7 @@ use crate::peniko::{ColorStops, ColorStopsSource, Extend};
 /// "The gradient goes from point A to point B", we declare things like
 /// "The gradient has angle X", and A and B are computed dynamically from widget layout.
 #[derive(Clone, Debug, PartialEq)]
+#[non_exhaustive]
 pub enum GradientShape {
     /// Gradient that transitions between two or more colors along a line.
     ///
@@ -23,6 +27,34 @@ pub enum GradientShape {
         /// The angle defining the gradient line's direction, in radians.
         /// Zero points upwards and positive angles represent clockwise rotation.
         angle: f64,
+    },
+    /// Gradient that transitions between two or more colors that radiate from an origin.
+    ///
+    /// This is interpreted like [`radial-gradient()`] in CSS.
+    ///
+    /// [`radial-gradient()`]: https://drafts.csswg.org/css-images-3/#radial-gradient-syntax
+    Radial {
+        /// The center of the gradient, relative to the widget's bounding box.
+        center: UnitPoint,
+        /// The shape and size of the gradient.
+        shape: RadialGradientShape,
+    },
+    /// Gradient that transitions between two or more colors that rotate around a center
+    /// point.
+    ///
+    /// This is similar to [`conic-gradient()`] in CSS **but the values are interpreted differently**.
+    /// (Specifically, angle zero points to the right and positive angles rotate counter-clockwise.)
+    ///
+    /// [`conic-gradient()`]: https://drafts.csswg.org/css-images-4/#conic-gradient-syntax
+    Sweep {
+        /// The center of the gradient, relative to the widget's bounding box.
+        center: UnitPoint,
+        /// Start angle of the sweep, in radians.
+        /// Zero points to the right and positive angles represent counter-clockwise rotation.
+        start_angle: f64,
+        /// End angle of the sweep, in radians.
+        /// Zero points to the right and positive angles represent counter-clockwise rotation.
+        end_angle: f64,
     },
 }
 
@@ -54,7 +86,60 @@ pub struct Gradient {
     pub stops: ColorStops,
 }
 
+/// An enum for different ways a radial gradient can be sized and shaped.
+///
+/// Matches the `radial-shape` and `radial-extent` parameters
+/// of the [`radial-gradient()`] syntax.
+///
+/// Defaults to `CircleTo(FarthestCorner)`.
+///
+/// [`radial-gradient()`]: https://drafts.csswg.org/css-images-3/#radial-gradient-syntax
+#[non_exhaustive]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum RadialGradientShape {
+    /// A circle defined based on the box size.
+    CircleTo(RadialGradientExtent),
+    /// A circle with a fixed radius in logical pixels.
+    FixedCircle(f64),
+    // TODO - Add following and remove #[non_exhaustive]:
+    // EllipseTo(RadialGradientExtent),
+    // FixedEllipse(f64),
+    // EllipsePercentage(f64),
+}
+
+/// An enum for different ways a radial gradient can be sized based on the surrounding box.
+///
+/// Matches the `radial-extent` parameter of the [`radial-gradient()`] syntax.
+///
+/// [`radial-gradient()`]: https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/radial-gradient#size
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum RadialGradientExtent {
+    /// Interpreted like CSS `closest-corner` size.
+    ClosestCorner,
+    /// Interpreted like CSS `closest-side` size.
+    ClosestSide,
+    /// Interpreted like CSS `farthest-side` size.
+    FarthestSide,
+    /// Interpreted like CSS `farthest-corner` size.
+    FarthestCorner,
+}
+
+// ---
+
 impl Gradient {
+    /// Creates a gradient with the given shape.
+    ///
+    /// See also [`Self::new_linear`], [`Self::new_radial`], [`Self::new_radial_with`].
+    pub fn new(shape: GradientShape) -> Self {
+        Self {
+            shape,
+            extend: Extend::default(),
+            interpolation_cs: ColorSpaceTag::Srgb,
+            hue_direction: HueDirection::default(),
+            stops: ColorStops::default(),
+        }
+    }
+
     /// Creates a [`Linear`](GradientShape::Linear) gradient.
     ///
     /// `angle` is in radians, with zero pointing upwards, and higher values rotating the gradient clockwise.
@@ -64,13 +149,61 @@ impl Gradient {
     /// for an `angle` of [`π/2`](core::f32::consts::FRAC_PI_2) (90°), the first
     /// stop will be aligned with the left edge.
     pub fn new_linear(angle: f64) -> Self {
-        Self {
-            shape: GradientShape::Linear { angle },
-            extend: Extend::default(),
-            interpolation_cs: ColorSpaceTag::Srgb,
-            hue_direction: HueDirection::default(),
-            stops: ColorStops::default(),
-        }
+        let shape = GradientShape::Linear { angle };
+        Self::new(shape)
+    }
+
+    /// Creates a circular [`Radial`](GradientShape::Radial) gradient extending to the farthest corner.
+    ///
+    /// See also [`Self::new_radial_with`].
+    pub fn new_radial(center: UnitPoint) -> Self {
+        let shape = RadialGradientShape::CircleTo(RadialGradientExtent::FarthestCorner);
+        let shape = GradientShape::Radial { center, shape };
+        Self::new(shape)
+    }
+
+    /// Creates a [`Radial`](GradientShape::Radial) gradient.
+    pub fn new_radial_with(center: UnitPoint, shape: RadialGradientShape) -> Self {
+        let shape = GradientShape::Radial { center, shape };
+        Self::new(shape)
+    }
+
+    /// Creates a [`Sweep`](GradientShape::Sweep) gradient.
+    ///
+    /// `start_angle` and `end_angle` are in radians, with zero pointing to the right,
+    /// and higher values rotating the gradient counter-clockwise.
+    ///
+    /// **Warning:** This doesn't match the values CSS uses for
+    /// [conic gradients](https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/conic-gradient#composition_of_a_conic_gradient).
+    /// We may change how these values are interpreted in the future.
+    ///
+    /// See also [`Self::new_full_sweep`].
+    pub fn new_sweep(center: UnitPoint, start_angle: f64, end_angle: f64) -> Self {
+        let shape = GradientShape::Sweep {
+            center,
+            start_angle,
+            end_angle,
+        };
+        Self::new(shape)
+    }
+
+    /// Creates a [`Sweep`](GradientShape::Sweep) gradient doing a full circle.
+    ///
+    /// `angle` is in radians, with zero pointing to the right,
+    /// and higher values rotating the gradient counter-clockwise.
+    ///
+    /// **Warning:** This doesn't match the values CSS uses for
+    /// [conic gradients](https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/conic-gradient#composition_of_a_conic_gradient).
+    /// We may change how these values are interpreted in the future.
+    pub fn new_full_sweep(center: UnitPoint, angle: f64) -> Self {
+        let start_angle = angle;
+        let end_angle = angle + TAU;
+        let shape = GradientShape::Sweep {
+            center,
+            start_angle,
+            end_angle,
+        };
+        Self::new(shape)
     }
 
     /// Builder method to set color stops on the gradient.
@@ -94,6 +227,10 @@ impl Gradient {
     }
 }
 
+#[expect(
+    clippy::cast_possible_truncation,
+    reason = "no other way to go from f64 to f32"
+)]
 impl GradientShape {
     /// Returns gradient coordinates for a gradient covering the given Rect.
     ///
@@ -101,6 +238,14 @@ impl GradientShape {
     pub fn get_peniko_kind_for_rect(&self, rect: Rect) -> crate::peniko::GradientKind {
         match self {
             Self::Linear { angle } => Self::get_peniko_linear_for_rect(*angle, rect),
+            Self::Radial { center, shape } => {
+                Self::get_peniko_radial_for_rect(*center, *shape, rect)
+            }
+            Self::Sweep {
+                center,
+                start_angle,
+                end_angle,
+            } => Self::get_peniko_sweep_for_rect(*center, *start_angle, *end_angle, rect),
         }
     }
 
@@ -122,5 +267,86 @@ impl GradientShape {
         let end = Point::new(center.x + x, center.y + y);
 
         crate::peniko::GradientKind::Linear { start, end }
+    }
+
+    fn get_peniko_radial_for_rect(
+        center: UnitPoint,
+        shape: RadialGradientShape,
+        rect: Rect,
+    ) -> crate::peniko::GradientKind {
+        let center = center.resolve(rect);
+        let radius = Self::get_gradient_radius(rect, center, shape);
+
+        crate::peniko::GradientKind::Radial {
+            start_center: center,
+            start_radius: 0.,
+            end_center: center,
+            end_radius: radius as f32,
+        }
+    }
+
+    fn get_gradient_radius(rect: Rect, center: Point, shape: RadialGradientShape) -> f64 {
+        let center_offset = center - rect.origin();
+
+        let dist_to_sides = [
+            f64::abs(center_offset.x),
+            f64::abs(center_offset.y),
+            f64::abs(rect.size().width - center_offset.x),
+            f64::abs(rect.size().height - center_offset.y),
+        ];
+        let dist_to_corners = [
+            center.distance(Point::new(rect.x0, rect.y0)),
+            center.distance(Point::new(rect.x1, rect.y0)),
+            center.distance(Point::new(rect.x0, rect.y1)),
+            center.distance(Point::new(rect.x1, rect.y1)),
+        ];
+
+        match shape {
+            RadialGradientShape::CircleTo(RadialGradientExtent::ClosestCorner) => {
+                dist_to_corners.into_iter().reduce(f64::min).unwrap()
+            }
+            RadialGradientShape::CircleTo(RadialGradientExtent::ClosestSide) => {
+                dist_to_sides.into_iter().reduce(f64::min).unwrap()
+            }
+            RadialGradientShape::CircleTo(RadialGradientExtent::FarthestSide) => {
+                dist_to_sides.into_iter().reduce(f64::max).unwrap()
+            }
+            RadialGradientShape::CircleTo(RadialGradientExtent::FarthestCorner) => {
+                dist_to_corners.into_iter().reduce(f64::max).unwrap()
+            }
+            RadialGradientShape::FixedCircle(radius) => radius,
+        }
+    }
+
+    fn get_peniko_sweep_for_rect(
+        center: UnitPoint,
+        start_angle: f64,
+        end_angle: f64,
+        rect: Rect,
+    ) -> crate::peniko::GradientKind {
+        let center = center.resolve(rect);
+
+        // TODO: We'd like to write:
+        #[cfg(false)]
+        crate::peniko::GradientKind::Sweep {
+            center,
+            start_angle: (-start_angle - FRAC_PI_2) as f32,
+            end_angle: (-end_angle - FRAC_PI_2) as f32,
+        };
+        // but Vello currently doesn't let us do this kind of gradient
+        // offsetting.
+        // Giving Vello angles outside the 0..2*PI range just culls the displayed gradient.
+
+        crate::peniko::GradientKind::Sweep {
+            center,
+            start_angle: start_angle as f32,
+            end_angle: end_angle as f32,
+        }
+    }
+}
+
+impl Default for RadialGradientShape {
+    fn default() -> Self {
+        Self::CircleTo(RadialGradientExtent::FarthestCorner)
     }
 }
