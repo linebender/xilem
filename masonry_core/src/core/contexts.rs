1d00c8401d86d74c2a7012867a9968d9f93b821c
Olivier FAURE <couteaubleu@gmail.com>
2025-07-30 12:03:05 +0200

    Add `set_animated_child_scroll_translation` method.

--- masonry_core/src/core/contexts.rs
+++ masonry_core/src/core/contexts.rs
@@ -742,6 +742,9 @@ impl ComposeCtx<'_> {
     /// Set the scroll translation for the child widget.
     ///
     /// The translation is applied on top of the position from [`LayoutCtx::place_child`].
+    ///
+    /// The given translation may be quantized so the child's final position
+    /// stays pixel-perfect.
     pub fn set_child_scroll_translation(
         &mut self,
         child: &mut WidgetPod<impl Widget + ?Sized>,
@@ -760,6 +763,41 @@ impl ComposeCtx<'_> {
                 translation,
             );
         }
+
+        let translation = translation.round();
+
+        let child = self.get_child_state_mut(child);
+        if translation != child.scroll_translation {
+            child.scroll_translation = translation;
+            child.transform_changed = true;
+        }
+    }
+
+    /// Set the scroll translation for the child widget.
+    ///
+    /// The translation is applied on top of the position from [`LayoutCtx::place_child`].
+    ///
+    /// Unlike [`Self::set_child_scroll_translation`], doesn't perform pixel-snapping.
+    /// This method should be used for intermediary scroll values during scroll animations.
+    pub fn set_animated_child_scroll_translation(
+        &mut self,
+        child: &mut WidgetPod<impl Widget + ?Sized>,
+        translation: Vec2,
+    ) {
+        if translation.x.is_nan()
+            || translation.x.is_infinite()
+            || translation.y.is_nan()
+            || translation.y.is_infinite()
+        {
+            debug_panic!(
+                "Error in {}: trying to call 'set_animated_child_scroll_translation' with child '{}' {} with invalid translation {:?}",
+                self.widget_id(),
+                self.get_child_dyn(child).short_type_name(),
+                child.id(),
+                translation,
+            );
+        }
+
         let child = self.get_child_state_mut(child);
         if translation != child.scroll_translation {
             child.scroll_translation = translation;
