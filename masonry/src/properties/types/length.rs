40ab5d823a288bd0982010d09b1098de1202ca35
Olivier FAURE <couteaubleu@gmail.com>
2025-08-04 13:35:40 +0200

    Flesh out AsUnit trait.

--- masonry/src/properties/types/length.rs
+++ masonry/src/properties/types/length.rs
@@ -63,26 +63,3 @@ impl Length {
         }
     }
 }
-
-// TODO - Split off, add more impls
-/// Utility trait for wrapping numbers in logical units.
-pub trait AsUnit {
-    /// Create a length, in logical pixels.
-    ///
-    /// # Panics
-    ///
-    /// Panics if value is negative or infinite.
-    fn px(self) -> Length;
-}
-
-impl AsUnit for f64 {
-    fn px(self) -> Length {
-        Length::px(self)
-    }
-}
-
-impl AsUnit for u64 {
-    fn px(self) -> Length {
-        Length::px(self as f64)
-    }
-}
