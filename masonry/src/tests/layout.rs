8197bcf28d9bc8878b94b0dc2979b2d60e395bf7
Olivier FAURE <couteaubleu@gmail.com>
2025-08-05 23:00:28 +0200

    Add tests for layout pass (#1286)
    Add `assert_debug_panics!` macro.
    Remove a ton of outdated safety_rails tests.
    Rewrite some safety_rails tests and move them to new module.
    Add test for pixel snapping.

--- masonry/src/tests/layout.rs
+++ masonry/src/tests/layout.rs
@@ -1,4 +1,144 @@
 // Copyright 2025 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO - Add unit tests
+use masonry_core::core::{NewWidget, WidgetTag};
+use masonry_testing::assert_debug_panics;
+use vello::kurbo::{Point, Size};
+
+use crate::core::Widget;
+use crate::testing::{ModularWidget, TestHarness};
+use crate::theme::default_property_set;
+use crate::widgets::{Flex, SizedBox};
+
+#[test]
+fn forget_to_recurse_layout() {
+    let widget = ModularWidget::new_parent(Flex::row().with_auto_id())
+        .layout_fn(|_child, _ctx, _, _| {
+            // We forget to call ctx.run_layout();
+            Size::ZERO
+        })
+        .with_auto_id();
+
+    assert_debug_panics!(
+        TestHarness::create(default_property_set(), widget),
+        "LayoutCtx::run_layout() was not called"
+    );
+}
+
+#[test]
+fn forget_to_call_place_child() {
+    let widget = ModularWidget::new_parent(Flex::row().with_auto_id())
+        .layout_fn(|child, ctx, _, bc| {
+            // We call ctx.run_layout(), but forget place_child
+            ctx.run_layout(child, bc)
+        })
+        .with_auto_id();
+
+    assert_debug_panics!(
+        TestHarness::create(default_property_set(), widget),
+        "LayoutCtx::place_child() was not called"
+    );
+}
+
+#[test]
+fn call_place_child_before_layout() {
+    let widget = ModularWidget::new_parent(Flex::row().with_auto_id())
+        .layout_fn(|child, ctx, _, _| {
+            // We call ctx.place_child(), but forget run_layout
+            ctx.place_child(child, Point::ORIGIN);
+            Size::ZERO
+        })
+        .with_auto_id();
+
+    assert_debug_panics!(
+        TestHarness::create(default_property_set(), widget),
+        "trying to call 'place_child'"
+    );
+}
+
+#[test]
+fn run_layout_on_stashed() {
+    let parent_tag = WidgetTag::new("parent");
+    let widget =
+        ModularWidget::new_parent(Flex::row().with_auto_id()).layout_fn(|child, ctx, _, bc| {
+            let size = ctx.run_layout(child, bc);
+            ctx.place_child(child, Point::ZERO);
+            size
+        });
+    let widget = NewWidget::new_with_tag(widget, parent_tag);
+
+    let mut harness = TestHarness::create(default_property_set(), widget);
+
+    assert_debug_panics!(
+        harness.edit_widget_with_tag(parent_tag, |mut parent| {
+            parent.ctx.set_stashed(&mut parent.widget.state, true);
+            parent.ctx.request_layout();
+        }),
+        "trying to compute layout of stashed widget"
+    );
+}
+
+#[test]
+fn stash_then_run_layout() {
+    let parent_tag = WidgetTag::new("parent");
+    let widget =
+        ModularWidget::new_parent(Flex::row().with_auto_id()).layout_fn(|child, ctx, _, bc| {
+            // We check that stashing a widget is effective "immediately"
+            // and triggers an error.
+            ctx.set_stashed(child, true);
+            let size = ctx.run_layout(child, bc);
+            ctx.place_child(child, Point::ZERO);
+            size
+        });
+    let widget = NewWidget::new_with_tag(widget, parent_tag);
+
+    assert_debug_panics!(
+        TestHarness::create(default_property_set(), widget),
+        "trying to compute layout of stashed widget"
+    );
+}
+
+#[test]
+fn unstash_then_run_layout() {
+    let parent_tag = WidgetTag::new("parent");
+    let widget =
+        ModularWidget::new_parent(Flex::row().with_auto_id()).layout_fn(|child, ctx, _, bc| {
+            // We check that unstashing a widget is effective "immediately"
+            // and avoids an error.
+            ctx.set_stashed(child, false);
+            let size = ctx.run_layout(child, bc);
+            ctx.place_child(child, Point::ZERO);
+            size
+        });
+    let widget = NewWidget::new_with_tag(widget, parent_tag);
+
+    let mut harness = TestHarness::create(default_property_set(), widget);
+
+    harness.edit_widget_with_tag(parent_tag, |mut parent| {
+        parent.ctx.set_stashed(&mut parent.widget.state, true);
+        parent.ctx.request_layout();
+    });
+}
+
+// TODO - Test that `Widget::layout()` isn't called when layout is cached.
+
+#[test]
+fn pixel_snapping() {
+    let child_tag = WidgetTag::new("child");
+    let child = NewWidget::new_with_tag(SizedBox::empty().width(10.3).height(10.3), child_tag);
+    let pos = Point::new(5.1, 5.3);
+    let parent = ModularWidget::new_parent(child).layout_fn(move |child, ctx, _, bc| {
+        let size = ctx.run_layout(child, &bc.loosen());
+        ctx.place_child(child, pos);
+        size
+    });
+    let parent = NewWidget::new(parent);
+
+    let harness = TestHarness::create(default_property_set(), parent);
+
+    let child_pos = harness.get_widget_with_tag(child_tag).ctx().window_origin();
+    let child_size = harness.get_widget_with_tag(child_tag).ctx().size();
+
+    assert_eq!(child_pos, Point::new(5.0, 5.0));
+    assert_eq!(child_size, Size::new(10., 11.));
+}
