1af4e35aa4daf390cf9b232048855b4132c66438
Olivier FAURE <couteaubleu@gmail.com>
2025-08-07 22:54:04 +0200

    Move safety rail tests for pointer capture

--- masonry/src/widgets/tests/safety_rails.rs
+++ masonry/src/widgets/tests/safety_rails.rs
@@ -1,7 +1,7 @@
 // Copyright 2022 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::core::{PointerButton, Widget, WidgetId, WidgetPod};
+use crate::core::{Widget, WidgetPod};
 use crate::testing::{ModularWidget, TestHarness};
 use crate::theme::default_property_set;
 use crate::widgets::Flex;
@@ -41,42 +41,4 @@ fn check_register_invalid_child() {
         .with_auto_id();
 
     let _harness = TestHarness::create(default_property_set(), widget);
-}
-
-#[should_panic(expected = "event does not allow pointer capture")]
-#[test]
-#[cfg_attr(
-    not(debug_assertions),
-    ignore = "This test doesn't work without debug assertions (i.e. in release mode). See https://github.com/linebender/xilem/issues/477"
-)]
-fn check_pointer_capture_outside_pointer_down() {
-    let widget = ModularWidget::new(())
-        .pointer_event_fn(|_, ctx, _, _event| {
-            ctx.capture_pointer();
-        })
-        .with_auto_id();
-
-    let mut harness = TestHarness::create(default_property_set(), widget);
-    harness.mouse_move((10.0, 10.0));
-    harness.mouse_button_release(PointerButton::Primary);
-}
-
-#[should_panic(expected = "event does not allow pointer capture")]
-#[test]
-#[cfg_attr(
-    not(debug_assertions),
-    ignore = "This test doesn't work without debug assertions (i.e. in release mode). See https://github.com/linebender/xilem/issues/477"
-)]
-fn check_pointer_capture_text_event() {
-    let id = WidgetId::next();
-    let widget = ModularWidget::new(())
-        .accepts_focus(true)
-        .text_event_fn(|_, ctx, _, _event| {
-            ctx.capture_pointer();
-        })
-        .with_id(id);
-
-    let mut harness = TestHarness::create(default_property_set(), widget);
-    harness.focus_on(Some(id));
-    harness.keyboard_type_chars("a");
 }
