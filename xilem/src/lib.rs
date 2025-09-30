bfb7715db1738006f822511a4c9698298a6e7d93
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-09-30 10:13:17 +0100

    Replace outdated `doc_auto_cfg` (#1407)
    Part of [#linebender > Cargo doc failures in linebender
    repos](https://xi.zulipchat.com/#narrow/channel/419691-linebender/topic/Cargo.20doc.20failures.20in.20linebender.20repos/with/542106544).
    
    cc @cyncrovee, this should fix the docs failure you're seeing in #1406
    (once we land it and you rebase past it)

--- xilem/src/lib.rs
+++ xilem/src/lib.rs
@@ -131,7 +131,7 @@
 // Targeting e.g. 32-bit means structs containing usize can give false positives for 64-bit.
 #![cfg_attr(target_pointer_width = "64", warn(clippy::trivially_copy_pass_by_ref))]
 // END LINEBENDER LINT SET
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(
     test,
     expect(
