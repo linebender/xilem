3b1df623107a5280fbadefc75c45f27763868fc8
yu <yu2344@yuli.co>
2025-10-01 12:40:50 +0800

    fix lint

--- masonry/src/widgets/portal.rs
+++ masonry/src/widgets/portal.rs
@@ -6,7 +6,6 @@ use std::ops::Range;
 use accesskit::{Node, Role};
 use dpi::PhysicalPosition;
 use tracing::{Span, trace_span};
-use ui_events::pointer::PointerState;
 use vello::Scene;
 use vello::kurbo::{Point, Rect, Size, Vec2};
 
@@ -271,11 +270,7 @@ impl<W: Widget + FromDynWidget + ?Sized> Widget for Portal<W> {
         let content_size = self.content_size;
 
         match *event {
-            PointerEvent::Scroll {
-                delta,
-                state: PointerState { modifiers, .. },
-                ..
-            } => {
+            PointerEvent::Scroll { delta, .. } => {
                 // TODO - Remove reference to scale factor.
                 // See https://github.com/linebender/xilem/issues/1264
                 let delta = match delta {
