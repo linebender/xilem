734935589286dd592246465912f1c3da083c72d9
Stefan Tammer <st.tammer@gmail.com>
2025-10-23 17:44:34 +0200

    Improve tracing backend
    - Introduce a `verify_subscriber_has_not_been_set` check before initializing the default tracing subscriber
      in the `try_init_tracing` and `try_init_test_tracing` functions to possibly skip subscriber setup.
    
    - Export a `default_subscriber` constructor for users who which to set the default tracing subscriber manually.

--- masonry_core/src/app/mod.rs
+++ masonry_core/src/app/mod.rs
@@ -7,6 +7,8 @@ mod render_root;
 mod tracing_backend;
 
 pub use render_root::{RenderRoot, RenderRootOptions, RenderRootSignal, WindowSizePolicy};
-pub use tracing_backend::{try_init_test_tracing, try_init_tracing};
+pub use tracing_backend::{
+    SubscriberHasBeenSetError, default_subscriber, try_init_test_tracing, try_init_tracing,
+};
 
 pub(crate) use render_root::{MutateCallback, RenderRootState};
