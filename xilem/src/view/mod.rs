dbdf01d63f8dbf5a01229131c0d128b128c3feef
Olivier FAURE <couteaubleu@gmail.com>
2025-11-07 14:05:59 +0100

    Reorder reexports in xilem crates (#1459)
    This is shorter, plays better with rustfmt, and is less likely to lead
    to merge conflicts if two people add a module in parallel.

--- xilem/src/view/mod.rs
+++ xilem/src/view/mod.rs
@@ -8,68 +8,48 @@
 // See https://github.com/linebender/xilem/issues/1254
 pub use masonry::core::Axis;
 
-mod task;
-pub use task::*;
-
-mod worker;
-pub use worker::*;
-
 mod button;
-pub use button::*;
-
 mod checkbox;
-pub use checkbox::*;
-
 mod flex;
-pub use flex::*;
-
 mod grid;
-pub use grid::*;
-
-mod sized_box;
-pub use sized_box::*;
-
-mod slider;
-pub use slider::*;
-
-mod spinner;
-pub use spinner::*;
-
 mod image;
-pub use image::*;
-
 mod indexed_stack;
-pub use indexed_stack::*;
-
 mod label;
-pub use label::*;
-
-mod variable_label;
-pub use variable_label::*;
-
-mod progress_bar;
-pub use progress_bar::*;
-
-mod prop;
-pub use prop::*;
-
-mod prose;
-pub use prose::*;
-
-mod text_input;
-pub use text_input::*;
-
-mod virtual_scroll;
-pub use virtual_scroll::*;
-
 mod portal;
-pub use portal::*;
-
-mod zstack;
-pub use zstack::*;
-
-mod transform;
-pub use transform::*;
-
+mod progress_bar;
+mod prop;
+mod prose;
+mod sized_box;
+mod slider;
+mod spinner;
 mod split;
-pub use split::*;
+mod task;
+mod text_input;
+mod transform;
+mod variable_label;
+mod virtual_scroll;
+mod worker;
+mod zstack;
+
+pub use self::button::*;
+pub use self::checkbox::*;
+pub use self::flex::*;
+pub use self::grid::*;
+pub use self::image::*;
+pub use self::indexed_stack::*;
+pub use self::label::*;
+pub use self::portal::*;
+pub use self::progress_bar::*;
+pub use self::prop::*;
+pub use self::prose::*;
+pub use self::sized_box::*;
+pub use self::slider::*;
+pub use self::spinner::*;
+pub use self::split::*;
+pub use self::task::*;
+pub use self::text_input::*;
+pub use self::transform::*;
+pub use self::variable_label::*;
+pub use self::virtual_scroll::*;
+pub use self::worker::*;
+pub use self::zstack::*;
