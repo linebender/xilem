6924bf31dbfbf4c7d78dec444cf35d7dec2e9eb6
Olivier FAURE <couteaubleu@gmail.com>
2025-11-21 13:32:47 +0100

    Better organize and document stress tests

--- docs/build_perf_script.sh
+++ docs/build_perf_script.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "== INITIAL BUILDS =="
 
-echo "-- Building stress_test --"
-cargo build --quiet --test stress_test
+echo "-- Building long_elem_seq --"
+cargo build --quiet --test long_elem_seq --features compile-stress-test
 
 echo "-- Building calc --"
 cargo build --quiet --example calc
@@ -16,8 +16,8 @@ cargo build --quiet --package placehero
 
 echo "== REMOVING CACHES =="
 
-echo "-- Removing cache for stress_test --"
-rm -rf target/debug/incremental/stress_test-*
+echo "-- Removing cache for long_elem_seq --"
+rm -rf target/debug/incremental/long_elem_seq-*
 
 echo "-- Removing cache for calc --"
 rm -rf target/debug/incremental/calc-*
@@ -28,12 +28,12 @@ rm -rf target/debug/incremental/placehero-*
 
 echo "== INCREMENTAL BUILDS =="
 
-touch xilem/tests/stress_test.rs
+touch xilem/stress_tests/long_elem_seq.rs
 touch xilem/examples/calc.rs
 touch placehero/src/main.rs
 
-echo "-- Building stress_test --"
-command time -f "Built stress_test in %es" cargo build --quiet --test stress_test
+echo "-- Building long_elem_seq --"
+command time -f "Built long_elem_seq in %es" cargo build --quiet --test long_elem_seq --features compile-stress-test
 
 echo "-- Building calc --"
 command time -f "Built calc in %es" cargo build --quiet --example calc
