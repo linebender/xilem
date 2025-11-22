e40fd0f10f2196992ac849ed6a973cf1f6edf584
Olivier FAURE <couteaubleu@gmail.com>
2025-11-22 13:01:16 +0100

    Rename map_message to map_message_result and add some documentation.

--- xilem_core/src/view.rs
+++ xilem_core/src/view.rs
@@ -138,7 +138,7 @@ pub trait View<State: ViewArgument, Action, Context: ViewPathTracker>:
         Self: Sized,
         F: Fn(Arg<'_, State>, MessageResult<Action>) -> MessageResult<ParentAction> + 'static,
     {
-        crate::map_message(self, f)
+        crate::map_message_result(self, f)
     }
 
     /// A view that maps a child [`View<State,ChildAction,_>`] to [`View<State,ParentAction,_>`] while providing mutable access to `State` in the map function.
