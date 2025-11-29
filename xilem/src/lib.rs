e967276ae24c30cc5151552a2f2bb8eaaf4f4232
Olivier FAURE <couteaubleu@gmail.com>
2025-11-29 01:38:41 +0100

    Fix doc errors

--- xilem/src/lib.rs
+++ xilem/src/lib.rs
@@ -30,7 +30,8 @@
 //!
 //! ```rust,no_run
 //! use winit::error::EventLoopError;
-//! use xilem::view::{Axis, text_button, flex, label};
+//! use xilem::masonry::core::Axis;
+//! use xilem::view::{text_button, flex, label};
 //! use xilem::{EventLoop, WindowOptions, WidgetView, Xilem};
 //! use xilem::core::Edit;
 //!
--- xilem/src/view/resize_observer.rs
+++ xilem/src/view/resize_observer.rs
@@ -32,7 +32,7 @@ use crate::{Pod, ViewCtx, WidgetView};
 /// To create a responsive version of a page, you can use `resize_observer`.
 ///
 /// ```rust,no_run
-/// # use xilem::{WidgetView, core::Edit, view::{resize_observer, flex, sized_box, Axis}, masonry::kurbo::Size};
+/// # use xilem::{WidgetView, core::Edit, view::{resize_observer, flex, sized_box}, masonry::kurbo::Size, masonry::core::Axis};
 ///
 /// struct State {
 ///     available_size: Size,
