8177d6a8749102db7ed38cdf43f38e78b9a14eed
Olivier FAURE <couteaubleu@gmail.com>
2025-12-08 12:58:51 +0100

    Add build-perf profile

--- docs/build_perf_script.sh
+++ docs/build_perf_script.sh
@@ -11,25 +11,25 @@ set -e
 echo "== INITIAL BUILDS =="
 
 echo "-- Building long_elem_seq --"
-cargo rustc --quiet --package xilem --test long_elem_seq
+cargo rustc --profile build-perf --quiet --package xilem --test long_elem_seq
 
 echo "-- Building calc --"
-cargo build --quiet --example calc
+cargo build --profile build-perf --quiet --example calc
 
 echo "-- Building placehero --"
-cargo build --quiet --package placehero
+cargo build --profile build-perf --quiet --package placehero
 
 
 echo "== REMOVING CACHES =="
 
 echo "-- Removing cache for long_elem_seq --"
-rm -rf target/debug/incremental/long_elem_seq-*
+rm -rf target/build-perf/incremental/long_elem_seq-*
 
 echo "-- Removing cache for calc --"
-rm -rf target/debug/incremental/calc-*
+rm -rf target/build-perf/incremental/calc-*
 
 echo "-- Removing cache for placehero --"
-rm -rf target/debug/incremental/placehero-*
+rm -rf target/build-perf/incremental/placehero-*
 
 
 echo "== INCREMENTAL BUILDS =="
@@ -40,12 +40,14 @@ touch placehero/src/main.rs
 
 echo "-- Building long_elem_seq --"
 command time -f "Built long_elem_seq in %es" \
-    cargo rustc --quiet --package xilem --test long_elem_seq -- --cfg compile_stress_test
+    cargo rustc --profile build-perf --quiet --package xilem --test long_elem_seq -- --cfg compile_stress_test
 
 echo "-- Building calc --"
-command time -f "Built calc in %es" cargo build --quiet --example calc
+command time -f "Built calc in %es" \
+    cargo build --profile build-perf --quiet --example calc
 
 echo "-- Building placehero --"
-command time -f "Built placehero in %es" cargo build --quiet --package placehero
+command time -f "Built placehero in %es" \
+    cargo build --profile build-perf --quiet --package placehero
 
 echo "== DONE =="
