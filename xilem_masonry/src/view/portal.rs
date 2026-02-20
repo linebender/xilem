9b0b977adb6d5f0f38166017862caee86b7d67cd
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2026-02-20 23:27:03 +0600

    Expose Portal widget options form Portal view (#1672)

--- xilem_masonry/src/view/portal.rs
+++ xilem_masonry/src/view/portal.rs
@@ -18,6 +18,9 @@ where
 {
     Portal {
         child,
+        constrain_horizontal: false,
+        constrain_vertical: false,
+        must_fill: false,
         phantom: PhantomData,
     }
 }
@@ -26,7 +29,46 @@ where
 #[must_use = "View values do nothing unless provided to Xilem."]
 pub struct Portal<V, State, Action> {
     child: V,
+    constrain_horizontal: bool,
+    constrain_vertical: bool,
+    must_fill: bool,
     phantom: PhantomData<fn(State) -> Action>,
+}
+
+impl<V, State, Action> Portal<V, State, Action> {
+    /// Set horizontal constraining of the child.
+    ///
+    /// - When it is `false` (the default), the child does not receive any upper
+    ///   bound on its width. The child can be as wide as it wants,
+    ///   and the viewport gets moved around to see all of it.
+    /// - When it is `true`, the [`Portal`]'s width will be passed down as an upper bound
+    ///   on the width of the child. There will be no horizontal scrollbar and
+    ///   the mouse wheel can't be used to horizontally scroll either.
+    pub fn constrain_horizontal(mut self, constrain_horizontal: bool) -> Self {
+        self.constrain_horizontal = constrain_horizontal;
+        self
+    }
+
+    /// Sets vertical constraining of the child.
+    ///
+    /// - When it is `false` (the default), the child does not receive any upper
+    ///   bound on its height. The child can be as tall as it wants,
+    ///   and the viewport gets moved around to see all of it.
+    /// - When it is `true`, the [`Portal`]'s height will be passed down as an upper bound
+    ///   on the height of the child. There will be no vertical scrollbar and
+    ///   the mouse wheel can't be used to vertically scroll either.
+    pub fn constrain_vertical(mut self, constrain_vertical: bool) -> Self {
+        self.constrain_vertical = constrain_vertical;
+        self
+    }
+
+    /// Sets whether the child must fill the view.
+    ///
+    /// If `true`, the child size is guaranteed to be at least the size of the portal.
+    pub fn must_fill(mut self, must_fill: bool) -> Self {
+        self.must_fill = must_fill;
+        self
+    }
 }
 
 impl<V, State, Action> ViewMarker for Portal<V, State, Action> {}
@@ -43,7 +85,12 @@ where
         // The Portal `View` doesn't get any messages directly (yet - scroll events?), so doesn't need to
         // use ctx.with_id.
         let (child, child_state) = self.child.build(ctx, app_state);
-        let widget_pod = ctx.create_pod(widgets::Portal::new(child.new_widget));
+        let widget_pod = ctx.create_pod(
+            widgets::Portal::new(child.new_widget)
+                .constrain_horizontal(self.constrain_horizontal)
+                .constrain_vertical(self.constrain_vertical)
+                .content_must_fill(self.must_fill),
+        );
         (widget_pod, child_state)
     }
 
@@ -55,6 +102,16 @@ where
         mut element: Mut<'_, Self::Element>,
         app_state: &mut State,
     ) {
+        if self.constrain_horizontal != prev.constrain_horizontal {
+            widgets::Portal::set_constrain_horizontal(&mut element, self.constrain_horizontal);
+        }
+        if self.constrain_vertical != prev.constrain_vertical {
+            widgets::Portal::set_constrain_vertical(&mut element, self.constrain_vertical);
+        }
+        if self.must_fill != prev.must_fill {
+            widgets::Portal::set_content_must_fill(&mut element, self.must_fill);
+        }
+
         let child_element = widgets::Portal::child_mut(&mut element);
         self.child
             .rebuild(&prev.child, view_state, ctx, child_element, app_state);
