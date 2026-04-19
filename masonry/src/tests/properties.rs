ed2911e153f84327e8cf544a5d044ea2d23fa6e8
Olivier FAURE <couteaubleu@gmail.com>
2026-04-19 18:14:57 +0200

    Fix compilation errors

--- masonry/src/tests/properties.rs
+++ masonry/src/tests/properties.rs
@@ -1,32 +1,32 @@
 // Copyright 2026 the Xilem Authors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::core::palette::css::ORANGE;
-use crate::core::{NewWidget, PropertySet, layout::AsUnit as _};
+use crate::core::Widget as _;
+use crate::layout::AsUnit as _;
+use crate::palette::css::BLUE;
 use crate::properties::{ContentColor, Dimensions, Gap};
+use crate::widgets::Button;
 
 #[test]
 fn widget_new_properties() {
-    let harness = TestHarness::new();
-
-    let widget = Button::new("").prepare();
+    let widget = Button::with_text("").prepare();
 
     // Set Dimensions, Gap, ContentColor to some values.
     let widget = widget
         .with_props(Dimensions::STRETCH)
         .with_props(Gap::new(10.px()))
-        .with_props(ContentColor::new(ORANGE));
+        .with_props(ContentColor::new(BLUE));
 
-    let props = &new_widget.properties;
-    assert_eq!(props.get::<Dimensions>(), Dimensions::STRETCH);
-    assert_eq!(props.get::<Gap>(), Gap::new(10.px()));
-    assert_eq!(props.get::<ContentColor>(), ContentColor::new(ORANGE));
+    let props = &widget.properties;
+    assert_eq!(props.get::<Dimensions>(), Some(&Dimensions::STRETCH));
+    assert_eq!(props.get::<Gap>(), Some(&Gap::new(10.px())));
+    assert_eq!(props.get::<ContentColor>(), Some(&ContentColor::new(BLUE)));
 
     // Override Dimensions and Gap but not ContentColor.
     let widget = widget.with_props((Dimensions::MIN, Gap::ZERO));
 
-    let props = &new_widget.properties;
-    assert_eq!(props.get::<Dimensions>(), Dimensions::MIN);
-    assert_eq!(props.get::<Gap>(), Gap::ZERO);
-    assert_eq!(props.get::<ContentColor>(), ContentColor::new(ORANGE));
+    let props = &widget.properties;
+    assert_eq!(props.get::<Dimensions>(), Some(&Dimensions::MIN));
+    assert_eq!(props.get::<Gap>(), Some(&Gap::ZERO));
+    assert_eq!(props.get::<ContentColor>(), Some(&ContentColor::new(BLUE)));
 }
