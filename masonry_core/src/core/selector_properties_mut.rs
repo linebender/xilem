dbcdffd896f12a3bc48025565ab5d4bfa6ba1c91
Dirk Tony <tonyramanantsalama@gmail.com>
2026-08-18 17:00:51 +0300

    `SelectorPropertiesMut` docs

--- masonry_core/src/core/selector_properties_mut.rs
+++ masonry_core/src/core/selector_properties_mut.rs
@@ -1,5 +1,6 @@
 use crate::core::{Property, PropertySet};
 
+/// A mutable reference to a [`PropertySet`].
 #[derive(Debug)]
 pub struct SelectorPropertiesMut<'a> {
     pub(crate) stack: &'a mut PropertySet,
@@ -7,14 +8,17 @@ pub struct SelectorPropertiesMut<'a> {
 }
 
 impl<'a> SelectorPropertiesMut<'a> {
+    /// Returns value of property `P`.
     pub fn get<P: Property>(&self) -> Option<&P> {
         self.stack.get()
     }
+    /// Sets property `P` to given value. Returns the previous value if `P` was already set.
     pub fn insert<P: Property>(&mut self, value: P) -> Option<P> {
         let old_value = self.stack.insert(value);
         self.has_property_changed = true;
         old_value
     }
+    /// Removes property `P`. Returns the previous value if `P` was set.
     pub fn remove<P: Property>(&mut self) -> Option<P> {
         let old_value = self.stack.remove();
         self.has_property_changed = true;
